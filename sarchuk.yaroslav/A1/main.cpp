798f0ff99e544c1f2cb16c4048f4e8c8069918ed
Yaroslav Sarchuk <ysarchuk@gmail.com>
2019-03-14 10:55:20 +0300

    Code Format Fix

--- sarchuk.yaroslav/A1/main.cpp
+++ sarchuk.yaroslav/A1/main.cpp
@@ -4,48 +4,49 @@
 #include "triangle.hpp"
 
 using namespace std;
+
 int main() {
-    Circle circle({0,0},3);
-    Shape *shape = &circle;
-    cout<<"New circle:\n";
-    shape -> writeInfo();
+  Circle circle({0, 0}, 3);
+  Shape *shape = &circle;
+  cout << "New circle:\n";
+  shape->writeInfo();
 
-    Rectangle rect(shape -> getFrameRect());
-    cout<<"Rectangle around circle\n";
-    rect.writeInfo();
+  Rectangle rect(shape->getFrameRect());
+  cout << "Rectangle around circle\n";
+  rect.writeInfo();
 
-    cout<<"Move to point 8,8\n";
-    shape -> move({8,8});
-    shape -> writeInfo();
-    cout<<"Move to 2,2\n";
-    shape -> move (2,2);
-    shape -> writeInfo();
+  cout << "Move to point 8,8\n";
+  shape->move({8, 8});
+  shape->writeInfo();
+  cout << "Move to 2,2\n";
+  shape->move(2, 2);
+  shape->writeInfo();
 
-    cout<<"New Rectangle\n";
-    shape = new Rectangle({2,5},4,4);
-    shape -> writeInfo();
+  cout << "New Rectangle\n";
+  shape = new Rectangle({2, 5}, 4, 4);
+  shape->writeInfo();
 
-    cout<<"Move to point 8,8\n";
-    shape -> move({8,8});
-    shape -> writeInfo();
-    cout<<"Move to 2,2\n";
-    shape -> move (2,2);
-    shape -> writeInfo();
+  cout << "Move to point 8,8\n";
+  shape->move({8, 8});
+  shape->writeInfo();
+  cout << "Move to 2,2\n";
+  shape->move(2, 2);
+  shape->writeInfo();
 
-    cout<<"New Triangle\n";
-    shape = new Triangle({0,0},{0,3},{9,0});
-    shape -> writeInfo();
+  cout << "New Triangle\n";
+  shape = new Triangle({0, 0}, {0, 3}, {9, 0});
+  shape->writeInfo();
 
-    cout<<"Move to point 8,8\n";
-    shape -> move({8,8});
-    shape -> writeInfo();
-    cout<<"Move to 2,2\n";
-    shape -> move (2,2);
-    shape -> writeInfo();
+  cout << "Move to point 8,8\n";
+  shape->move({8, 8});
+  shape->writeInfo();
+  cout << "Move to 2,2\n";
+  shape->move(2, 2);
+  shape->writeInfo();
 
-    shape = new Rectangle(shape->getFrameRect());
-    cout<<"Rectangle around triangle\n";
-    shape -> writeInfo();
+  shape = new Rectangle(shape->getFrameRect());
+  cout << "Rectangle around triangle\n";
+  shape->writeInfo();
 
-    return 0;
+  return 0;
 }
