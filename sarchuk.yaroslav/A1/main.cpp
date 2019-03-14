319bc354019716424c0f86a4afcde2d36506283c
Yaroslav Sarchuk <ysarchuk@gmail.com>
2019-03-14 12:29:46 +0300

    Code construction changes

--- sarchuk.yaroslav/A1/main.cpp
+++ sarchuk.yaroslav/A1/main.cpp
@@ -6,48 +6,48 @@
 using namespace std;
 
 int main() {
-  Circle circle({0, 0}, 3);
-  Shape *shape = &circle;
-  cout << "New circle:\n";
-  shape->writeInfo();
 
-  Rectangle rect(shape->getFrameRect());
+  Shape* shape[5];
+
+  shape[0] = new Circle({0, 0}, 3);
+  cout << "New circle:\n";
+  shape[0]->writeInfo();
+
+  Rectangle rect(shape[0]->getFrameRect());
   cout << "Rectangle around circle\n";
   rect.writeInfo();
-
   cout << "Move to point 8,8\n";
-  shape->move({8, 8});
-  shape->writeInfo();
+  shape[0]->move({8, 8});
+  shape[0]->writeInfo();
   cout << "Move to 2,2\n";
-  shape->move(2, 2);
-  shape->writeInfo();
+  shape[0]->move(2, 2);
+  shape[0]->writeInfo();
 
   cout << "New Rectangle\n";
-  shape = new Rectangle({2, 5}, 4, 4);
-  shape->writeInfo();
-
+  shape[1] = new Rectangle({2, 5}, 4, 4);
+  shape[1]->writeInfo();
   cout << "Move to point 8,8\n";
-  shape->move({8, 8});
-  shape->writeInfo();
+  shape[1]->move({8, 8});
+  shape[1]->writeInfo();
   cout << "Move to 2,2\n";
-  shape->move(2, 2);
-  shape->writeInfo();
+  shape[1]->move(2, 2);
+  shape[1]->writeInfo();
 
-  delete[] shape;
   cout << "New Triangle\n";
-  shape = new Triangle({0, 0}, {0, 3}, {9, 0});
-  shape->writeInfo();
+  shape[2] = new Triangle({0, 0}, {0, 3}, {9, 0});
+  shape[2]->writeInfo();
 
   cout << "Move to point 8,8\n";
-  shape->move({8, 8});
-  shape->writeInfo();
+  shape[2]->move({8, 8});
+  shape[2]->writeInfo();
   cout << "Move to 2,2\n";
-  shape->move(2, 2);
-  shape->writeInfo();
-  delete[] shape;
-  shape = new Rectangle(shape->getFrameRect());
+  shape[2]->move(2, 2);
+  shape[2]->writeInfo();
+
+  shape[3] = new Rectangle(shape[2]->getFrameRect());
   cout << "Rectangle around triangle\n";
-  shape->writeInfo();
+  shape[3]->writeInfo();
+
 
   delete[] shape;
 
