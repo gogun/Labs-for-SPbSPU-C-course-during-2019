604216537520d9f37244aacd10e914cc0c164263
Yaroslav Sarchuk <ysarchuk@gmail.com>
2019-03-14 21:19:17 +0300

    Sad no to using namespace

--- sarchuk.yaroslav/A1/main.cpp
+++ sarchuk.yaroslav/A1/main.cpp
@@ -3,49 +3,48 @@
 #include "rectangle.hpp"
 #include "triangle.hpp"
 
-using namespace std;
 
 int main() {
 
   Shape* shape[4];
 
   shape[0] = new Circle({0, 0}, 3);
-  cout << "New circle:\n";
+  std::cout<< "New circle:\n";
   shape[0]->writeInfo();
 
   Rectangle rect(shape[0]->getFrameRect());
-  cout << "Rectangle around circle\n";
+  std::cout<< "Rectangle around circle\n";
   rect.writeInfo();
-  cout << "Move to point 8,8\n";
+  std::cout<< "Move to point 8,8\n";
   shape[0]->move({8, 8});
   shape[0]->writeInfo();
-  cout << "Move to 2,2\n";
+  std::cout<< "Move to 2,2\n";
   shape[0]->move(2, 2);
   shape[0]->writeInfo();
 
-  cout << "New Rectangle\n";
+  std::cout<< "New Rectangle\n";
   shape[1] = new Rectangle({2, 5}, 4, 4);
   shape[1]->writeInfo();
-  cout << "Move to point 8,8\n";
+  std::cout<< "Move to point 8,8\n";
   shape[1]->move({8, 8});
   shape[1]->writeInfo();
-  cout << "Move to 2,2\n";
+  std::cout<< "Move to 2,2\n";
   shape[1]->move(2, 2);
   shape[1]->writeInfo();
 
-  cout << "New Triangle\n";
+  std::cout<< "New Triangle\n";
   shape[2] = new Triangle({0, 0}, {0, 3}, {9, 0});
   shape[2]->writeInfo();
 
-  cout << "Move to point 8,8\n";
+  std::cout<< "Move to point 8,8\n";
   shape[2]->move({8, 8});
   shape[2]->writeInfo();
-  cout << "Move to 2,2\n";
+  std::cout<< "Move to 2,2\n";
   shape[2]->move(2, 2);
   shape[2]->writeInfo();
 
   shape[3] = new Rectangle(shape[2]->getFrameRect());
-  cout << "Rectangle around triangle\n";
+  std::cout<< "Rectangle around triangle\n";
   shape[3]->writeInfo();
 
 
