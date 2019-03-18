c286aecda86346d755f0e06bd3e5b0bff402d712
Daniil_Besedin <besedin.dd@edu.spbstu.ru>
2019-03-18 23:51:49 +0300

    Fix duplicate code

--- besedin.daniil/A1/main.cpp
+++ besedin.daniil/A1/main.cpp
@@ -2,34 +2,44 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
+void printFrameRect(const Shape &shape, const std::string &name)
+{
+  rectangle_t frameRect = shape.getFrameRect();
+  std::cout << name << std::endl;
+  std::cout << "Width: " << frameRect.width << "  ";
+  std::cout << "Height: " << frameRect.height << "  ";
+  std::cout << "Center: ( " << frameRect.pos.x << " , " << frameRect.pos.y << " )";
+  std::cout << std::endl;
+}
+
+void printArea(const Shape &shape)
+{
+  std::cout << "Area: " << shape.getArea() << std::endl;
+}
+
+void printCenter(const Shape &shape)
+{
+  std::cout << "Center: ( " << shape.getFrameRect().pos.x << " , " << shape.getFrameRect().pos.y << " )";
+}
+
 int main()
 {
-
   point_t point1 = {5, 5};
   Rectangle rectangle(3, 3, point1);
   Circle circle(7, point1);
-
-  std::cout << "Rectangle" << std::endl;
-  std::cout << "Width: " << rectangle.getFrameRect().width << '\t';
-  std::cout << "Height: " << rectangle.getFrameRect().height << '\t';
-  std::cout << "Center: ( " << rectangle.getFrameRect().pos.x << " , " << rectangle.getFrameRect().pos.y << " )";
-  std::cout << std::endl;
-  std::cout << "Area: " << rectangle.getArea() << std::endl;
-  rectangle.move(3, 3);
-  std::cout << "Center: ( " << rectangle.getFrameRect().pos.x << " , " << rectangle.getFrameRect().pos.y << " )";
-  std::cout << '\n' << '\n';
-
-  std::cout << "Circle" << std::endl;
-  std::cout << "Width: " << circle.getFrameRect().width << '\t';
-  std::cout << "Height: " << circle.getFrameRect().height << '\t';
-  std::cout << "Center: ( " << circle.getFrameRect().pos.x << " , " << circle.getFrameRect().pos.y << " )";
-  std::cout << std::endl;
-  std::cout << "Area: " << circle.getArea() << std::endl;
   point_t point2 = {15, 15};
+
+  printFrameRect(rectangle, "Rectangle");
+  printArea(rectangle);
+  rectangle.move(3, 3);
+  printCenter(rectangle);
+  std::cout << std::endl << std::endl;
+
+  printFrameRect(circle, "Circle");
+  printArea(circle);
   circle.move(point2);
-  std::cout << "Center: ( " << circle.getFrameRect().pos.x << " , " << circle.getFrameRect().pos.y << " )";
+  printCenter(circle);
   std::cout << std::endl;
 
   return 0;
-
 }
