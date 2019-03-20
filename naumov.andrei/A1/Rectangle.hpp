a8e3973f873ec216af192cd37a58ad2ad76bc55d
Andrei Naumov <naumov.dron@gmail.com>
2019-03-20 16:39:24 +0000

    Upload Rectangle.hpp to A1 and add end of line character

---
+++ naumov.andrei/A1/Rectangle.hpp
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "Shape.hpp"
+
+class Rectangle: public Shape
+{
+public:
+  float getArea() const;
+  rectangle_t getFrameRect() const;
+  void move(const point_t & newCentre);
+  void move(float newX, float newY);
+
+  void setWidth(float newWidth);
+  void setHeight(float newHeight);
+protected:
+  float width_;
+  float height_;
+private:
+};
+
+#endif // !RECTANGLE_HPP
