80164df64ad66729e20ceea279d2e60a2918dbf7
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-20 11:49:55 +0300

    Modifying main

--- petrov.artyom/A1/main.cpp
+++ petrov.artyom/A1/main.cpp
@@ -8,17 +8,19 @@ int main()
 {
   point_t centre = point_t { 20, 40 };
 
+  // Смысл polymorphicRectangle раскрывается на строку ниже, когда мы приводим Rectangle к Shape
   Rectangle polymorphicRectangle = Rectangle(centre, 100, 100);
   Shape * shape = &polymorphicRectangle;
   std::cout << "Area of rectangle 100*100 is " << shape->getArea() << std::endl;
 
+  // Аналогично, Circle приводится к Shape
   Circle polymorphicCircle(centre, 10);
   shape = &polymorphicCircle;
   std::cout << "Area of circle of raidus 10 is " << shape->getArea() << std::endl;
 
-  auto startingFrame = shape->getFrameRect();
+  auto /* rectangle_t */ startingFrame = shape->getFrameRect();
   shape->move(50, 40);
-  auto finishFrame = shape->getFrameRect();
+  auto /* rectangle_t */ finishFrame = shape->getFrameRect();
   std::cout << "Shape frame moved for ("
             << finishFrame.pos.x - startingFrame.pos.x << ";"
             << finishFrame.pos.y - startingFrame.pos.y  << ") unit points" << std::endl;
@@ -37,6 +39,14 @@ int main()
   std::cout << "Rectangle frame moved for ("
             << finishFrame.pos.x - startingFrame.pos.x << ";"
             << finishFrame.pos.y - startingFrame.pos.y  << ") unit points" << std::endl;
+
+  try {
+    Circle invalidCirce(centre, -5);
+  }
+  catch (std::invalid_argument&) {
+    std::cerr << "Circle radius is less than 0";
+    return  -2;
+  }
 
   return 0;
 }
