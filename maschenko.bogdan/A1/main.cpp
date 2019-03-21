296f28f537d8b1f2990608be9e73997e04b69122
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-03-21 21:32:34 +0300

    fix mistakes in main.cpp

--- maschenko.bogdan/A1/main.cpp
+++ maschenko.bogdan/A1/main.cpp
@@ -6,11 +6,10 @@
 
 int main()
 {
-  Shape * shape;
   std::cout << "new Circle with center: (14;56) and radius = 5" << std::endl;
 
   Circle circle({14, 56}, 5);
-  shape = &circle;
+  Shape * shape = &circle;
   shape->writeInfo();
   std::cout << "move Circle in point with center: (-34,32)" << std::endl;
   shape->move({-34, 32});
@@ -33,6 +32,7 @@ int main()
   std::cout << "new Rectangle with center (-5;32) and width 2, height = 7" << std::endl;
 
   Rectangle rectangle({-5, 32}, 2, 7);
+  shape = &rectangle;
   shape->writeInfo();
   std::cout << "move Rectangle in point with center (-37,-37)" << std::endl;
   shape->move({-37, -37});
