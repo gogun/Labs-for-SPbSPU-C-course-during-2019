c4bfc9b15e5ddde349e9529061701e3917da8d28
Kornilov Daniil Fedorovich <daniil-kornilov@yandex.ru>
2019-03-22 23:03:14 +0000

    Update kornilov.daniil/A1/shape.hpp

--- kornilov.daniil/A1/shape.hpp
+++ kornilov.daniil/A1/shape.hpp
@@ -6,11 +6,11 @@
 class Shape
 {
   public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t &point) = 0;
-    virtual void move(const double dx, const double dy) = 0;
-    virtual ~Shape() = default;
+    virtual void move(const double dx, const double dy) = 0;   
 };
 
 #endif
