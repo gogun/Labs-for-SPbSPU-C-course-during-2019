f5d1344e41f104970bf5f4be34e57e7ccc282f55
KWSilence <kamil.95.05@mail.ru>
2019-03-25 22:55:53 +0300

    Добавлено полиморфное применение

--- khismatullin.kamil/A1/main.cpp
+++ khismatullin.kamil/A1/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "rectangle.hpp"
 #include "circle.hpp"
+#include "shape.hpp"
 
 int main() {
-  Rectangle rectangle ({2, 4}, 7, 10);  //создание прямоугольника
+  Rectangle rectangle({2, 4}, 7, 10);  //создание прямоугольника
   std::cout << "[Rectangle]" << std::endl;  //вывод параметров прямоугольника(на следующей строке)
   std::cout << "Center : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl
       << "Height : " << rectangle.getFrameRect().height << std::endl
@@ -13,7 +14,7 @@ int main() {
   std::cout << std::endl << "{Moving}" << std::endl;
   rectangle.move({-3, -3});  //перемещение по координатам и дальнейший вывод текущего положения
   std::cout << "-- to point : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl;
-  rectangle.move(3, 3);  //перемещение по смещению и дальнейший вывод текущего положения
+  rectangle.move(5, 2);  //перемещение по смещению и дальнейший вывод текущего положения
   std::cout << "-- by offset : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl;
 
   std::cout << "___________________________" << std::endl;
@@ -33,6 +34,19 @@ int main() {
   std::cout << "-- by offset : (" << circle.getCenter().x << " , " << circle.getCenter().y << ")" << std::endl;
 
   std::cout << "___________________________" << std::endl;
+
+  Shape *shape[] = {&rectangle, &circle}; //полиморфное применение
+  shape[1]->move(5, 10); //перемещение круга сдвигом
+  shape[0]->move(6, 7); //перемещение прямоугольника сдвигом
+  for (Shape *s:shape) {
+    std::cout << "Center : (" << s->getCenter().x << " , " << s->getCenter().y << ")" << std::endl
+        << "Height of frame rectangle : " << s->getFrameRect().height << std::endl
+        << "Width of frame rectangle  : " << s->getFrameRect().width << std::endl
+        << "Area   : " << s->getArea() << std::endl;
+
+    std::cout << "___________________________" << std::endl;
+  }
+
 
   return 0;
 }
