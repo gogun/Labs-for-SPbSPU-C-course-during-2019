2f2cd2e0eb2599909d8c67c8529b546ae3ebdd3d
KWSilence <kamil.95.05@mail.ru>
2019-03-25 17:16:32 +0300

    Удалены динамические элементы (new)

--- khismatullin.kamil/A1/circle.cpp
+++ khismatullin.kamil/A1/circle.cpp
@@ -10,35 +10,35 @@ Circle::Circle(const point_t &pos, const double radius) :
   if (radius <= 0) {
     std::cerr << "Radius less or equal zero" << std::endl;
   }
-};
+}
 
 double Circle::getArea() const
 {
   return pow(radius_, 2) * M_PI;
-};
+}
 
 rectangle_t Circle::getFrameRect() const
 {
   return {center_, radius_ * 2, radius_ * 2};
-};
+}
 
 void Circle::move(const double dx, const double dy)
 {
   center_.x += dx;
   center_.y += dy;
-};
+}
 
 void Circle::move(const point_t &point)
 {
   center_ = point;
-};
+}
 
 point_t Circle::getCenter() const
 {
   return center_;
-};
+}
 
 double Circle::getRadius() const
 {
   return radius_;
-};
+}
--- khismatullin.kamil/A1/main.cpp
+++ khismatullin.kamil/A1/main.cpp
@@ -3,36 +3,34 @@
 #include "circle.hpp"
 
 int main() {
-  Rectangle *rectangle = new Rectangle{{2, 4}, 7, 10};  //создание прямоугольника
+  Rectangle rectangle ({2, 4}, 7, 10);  //создание прямоугольника
   std::cout << "[Rectangle]" << std::endl;  //вывод параметров прямоугольника(на следующей строке)
-  std::cout << "Center : (" << rectangle->getCenter().x << " , " << rectangle->getCenter().y << ")" << std::endl
-      << "Height : " << rectangle->getFrameRect().height << std::endl
-      << "Width  : " << rectangle->getFrameRect().width << std::endl
-      << "Area   : " << rectangle->getArea() << std::endl;
+  std::cout << "Center : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl
+      << "Height : " << rectangle.getFrameRect().height << std::endl
+      << "Width  : " << rectangle.getFrameRect().width << std::endl
+      << "Area   : " << rectangle.getArea() << std::endl;
 
   std::cout << std::endl << "{Moving}" << std::endl;
-  rectangle->move({-3, -3});  //перемещение по координатам и дальнейший вывод текущего положения
-  std::cout << "-- to point : (" << rectangle->getCenter().x << " , " << rectangle->getCenter().y << ")" << std::endl;
-  rectangle->move(3, 3);  //перемещение по смещению и дальнейший вывод текущего положения
-  std::cout << "-- by offset : (" << rectangle->getCenter().x << " , " << rectangle->getCenter().y << ")" << std::endl;
-  delete (rectangle);
+  rectangle.move({-3, -3});  //перемещение по координатам и дальнейший вывод текущего положения
+  std::cout << "-- to point : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl;
+  rectangle.move(3, 3);  //перемещение по смещению и дальнейший вывод текущего положения
+  std::cout << "-- by offset : (" << rectangle.getCenter().x << " , " << rectangle.getCenter().y << ")" << std::endl;
 
   std::cout << "___________________________" << std::endl;
 
-  Circle *circle = new Circle{{2, 4}, 5}; //создание круга
+  Circle circle({2, 4}, 5); //создание круга
   std::cout << "[Circle]" << std::endl; //вывод параметров круга(на следующей строке)
-  std::cout << "Center : (" << circle->getCenter().x << " , " << circle->getCenter().y << ")" << std::endl
-      << "Radius : " << circle->getRadius() << std::endl
-      << "Height of frame rectangle : " << circle->getFrameRect().height << std::endl
-      << "Width of frame rectangle  : " << circle->getFrameRect().width << std::endl
-      << "Area   : " << circle->getArea() << std::endl;
+  std::cout << "Center : (" << circle.getCenter().x << " , " << circle.getCenter().y << ")" << std::endl
+      << "Radius : " << circle.getRadius() << std::endl
+      << "Height of frame rectangle : " << circle.getFrameRect().height << std::endl
+      << "Width of frame rectangle  : " << circle.getFrameRect().width << std::endl
+      << "Area   : " << circle.getArea() << std::endl;
 
   std::cout << std::endl << "{Moving}" << std::endl;
-  circle->move({3, 3}); //перемещение по координатам и дальнейший вывод текущего положения
-  std::cout << "-- to point : (" << circle->getCenter().x << " , " << circle->getCenter().y << ")" << std::endl;
-  circle->move(3, 3); //перемещение по смещению и дальнейший вывод текущего положения
-  std::cout << "-- by offset : (" << circle->getCenter().x << " , " << circle->getCenter().y << ")" << std::endl;
-  delete (circle);
+  circle.move({3, 3}); //перемещение по координатам и дальнейший вывод текущего положения
+  std::cout << "-- to point : (" << circle.getCenter().x << " , " << circle.getCenter().y << ")" << std::endl;
+  circle.move(3, 3); //перемещение по смещению и дальнейший вывод текущего положения
+  std::cout << "-- by offset : (" << circle.getCenter().x << " , " << circle.getCenter().y << ")" << std::endl;
 
   std::cout << "___________________________" << std::endl;
 
