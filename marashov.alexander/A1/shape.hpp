cb652b55b6f8396ff7580f2ec6d7c43542f21771
Marashov Alexander <SanYAMarashov@yandexru>
2019-03-25 16:03:09 +0300

    A1 edited the order of methods in class shape

--- marashov.alexander/A1/shape.hpp
+++ marashov.alexander/A1/shape.hpp
@@ -6,12 +6,12 @@
 class Shape
 {
   public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual point_t getCenter() const = 0;
     virtual void move(const point_t& pos) = 0;
     virtual void move(double dltX, double dltY) = 0;
-    virtual ~Shape() = default;
 };
 
 #endif //SHAPE_HPP
