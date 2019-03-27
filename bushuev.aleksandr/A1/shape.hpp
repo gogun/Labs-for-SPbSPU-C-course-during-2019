7d435f99471151f22b2023775a395970ba598185
AleksandrBushuev <aleksandr.bushuev2000@gmail.com>
2019-03-27 14:36:10 +0300

    Added destructor into Shape

--- bushuev.aleksandr/A1/shape.hpp
+++ bushuev.aleksandr/A1/shape.hpp
@@ -6,6 +6,8 @@
 class Shape
 {
   public:
+    virtual ~Shape() = default;
+
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void coutInfo() const = 0;
