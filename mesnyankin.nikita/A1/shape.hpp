2624cc85b0f0826bea2ac9b9b65b0adce06c43b0
Nikita Mesnyankin <NikitaMesnyankin@gmail.com>
2019-03-27 03:36:36 +0300

    Added abstract class file

---
+++ mesnyankin.nikita/A1/shape.hpp
@@ -0,0 +1,15 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "base-types.hpp"
+
+class Shape {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(double, double) = 0;
+    virtual void move(point_t) = 0;
+};
+
+#endif
