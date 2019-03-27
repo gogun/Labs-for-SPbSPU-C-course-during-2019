00153383015dd17f775e27d6e722481a301719d1
philipp.trofimov <philipp.trofimov2000@mail.ru>
2019-03-28 00:33:10 +0300

    Philipp.trofimov

---
+++ trofimov.philipp/A1/circle.cpp
@@ -0,0 +1,48 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include "circle.h"
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+Circle::Circle(const point_t &pos, double r) :
+  center_(pos),
+  radius_(r)
+{
+  if (radius_ <= 0) 
+  {
+    std::cerr << "Invalid radius" << std::endl;
+  }
+}
+
+double Circle::getArea() const
+{
+  return M_PI * radius_ *radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  return { center_,radius_,radius_ };
+}
+
+void  Circle::move(double dx, double dy)
+{
+  center_.x += dx;
+  center_.y += dy;
+
+}
+
+void Circle::move(const point_t &point)
+{
+  center_ = point;
+}
+
+void Circle::getInfo() const
+{
+  rectangle_t rectangle = getFrameRect();
+  std::cout << "Rectangle height = " << rectangle.height << std::endl;
+  std::cout << "Rectangle width = " << rectangle.width << std::endl;
+  std::cout << "Point x = " << rectangle.pos.x << std::endl;
+  std::cout << "Point y = " << rectangle.pos.y << std::endl;
+}
+#endif
---
+++ trofimov.philipp/A1/rectangle.cpp
@@ -0,0 +1,44 @@
+#ifndef RECTANGLE_CPP
+#define RECTANGLE_CPP
+#include "rectangle.h"
+#include <iostream>
+
+Rectangle::Rectangle(const point_t &pos, double w, double h) :
+  rectangle_({ pos, w, h })
+{
+  if ((rectangle_.height <= 0.0) || (rectangle_.width <= 0.0)) 
+  {
+    std::cerr << "Invalid data" << std::endl;
+  }
+}
+
+double Rectangle::getArea() const
+{
+  return rectangle_.height * rectangle_.width;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rectangle_;
+}
+
+void Rectangle::move(const point_t &point)
+{
+  rectangle_.pos = point;;
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  rectangle_.pos.x += dx;
+  rectangle_.pos.y += dy;
+}
+
+void Rectangle::getInfo() const
+{
+  rectangle_t rec_ = getFrameRect();
+  std::cout << "Rectangle height = " << rec_.height << std::endl;
+  std::cout << "Rectangle width = " << rec_.width << std::endl;
+  std::cout << "Point x = " << rec_.pos.x << std::endl;
+  std::cout << "Point y = " << rec_.pos.y << std::endl;
+}
+#endif
