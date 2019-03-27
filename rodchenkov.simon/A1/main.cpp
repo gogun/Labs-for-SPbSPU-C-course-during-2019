11f0afff42f5ea81f054b5be7741f2a753e5c0a1
rodchenkov.sn <rodchenkov.sn@edu.spbstu.ru>
2019-03-27 20:27:36 +0300

    A1 fix
    raw pointers were replaced with smart ones

--- rodchenkov.simon/A1/main.cpp
+++ rodchenkov.simon/A1/main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
+#include <memory>
 
 #include "rectangle.hpp"
 #include "circle.hpp"
 
-void testShape(Shape* s, const point_t& p);
+void testShape(std::unique_ptr<Shape>& s, const point_t& p);
 
 int main()
 {
-  Shape* shapes[2];
   try {
-    shapes[0] = new Rectangle{{10, 10}, 20, 40};
-    shapes[1] = new Circle{{100, 100}, 10};
+    std::unique_ptr<Shape> shapes[2];
+    shapes[0] = std::make_unique<Rectangle>(point_t{10, 10}, 20, 40);
+    shapes[1] = std::make_unique<Circle>(point_t{100, 100}, 10);
     for (auto& sh : shapes) {
       testShape(sh, {5, 5});
     }
-    delete shapes[0];
-    delete shapes[1];
   }
   catch (std::invalid_argument& exp) {
     std::cerr << exp.what();
-    delete shapes[0];
-    delete shapes[1];
     return 1;
   }
   catch (...) {
     std::cerr << "Internal error.\n";
-    delete shapes[0];
-    delete shapes[1];
     return 2;
   }
   return 0;
 }
 
-void testShape(Shape* s, const point_t& p)
+void testShape(std::unique_ptr<Shape>& s, const point_t& p)
 {
   std::cout << "Before moving :\n";
   s->printData(std::cout);
