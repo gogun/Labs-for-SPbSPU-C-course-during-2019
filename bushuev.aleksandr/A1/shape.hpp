c1175b0c57417182928ce004266610f4542fdab5
AleksandrBushuev <aleksandr.bushuev2000@gmail.com>
2019-03-27 14:06:06 +0300

    Fixed mistakes

--- bushuev.aleksandr/A1/shape.hpp
+++ bushuev.aleksandr/A1/shape.hpp
@@ -5,13 +5,13 @@
 
 class Shape
 {
-    public:
-        virtual double getArea() const = 0;
-        virtual rectangle_t getFrameRect() const = 0;
-        virtual void coutInfo() const = 0;
+  public:
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void coutInfo() const = 0;
 
-        virtual void move(const point_t&) = 0;
-        virtual void move(double,double) = 0;
-} ;
+    virtual void move(const point_t&) = 0;
+    virtual void move(double,double) = 0;
+};
 
 #endif // SHAPE
