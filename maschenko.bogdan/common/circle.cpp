9705a2ab7853eff84f3a7cf5dd8ae9251b3db0e6
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-03-27 09:13:23 +0300

    in classes of implementation remove namespace

--- maschenko.bogdan/common/circle.cpp
+++ maschenko.bogdan/common/circle.cpp
@@ -4,73 +4,70 @@
 #include <iostream>
 #include <stdexcept>
 
-namespace maschenko
-{
-  Circle::Circle(const point_t &center, double radius) :
+maschenko::Circle::Circle(const point_t &center, double radius) :
   center_(center),
   radius_(radius)
-  {
-    if (radius_ <= 0) {
-      throw std::invalid_argument ("Invalid radius of circle.");
-    }
+{
+  if (radius_ <= 0) {
+    throw std::invalid_argument ("Invalid radius of circle.");
   }
+}
 
-  double Circle::getArea() const
-  {
-    return M_PI * radius_ * radius_;
-  }
+double maschenko::Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
 
-  rectangle_t Circle::getFrameRect() const
-  {
-    return rectangle_t{
-      center_,
-      2 * radius_,
-      2 * radius_
-    };
-  }
+maschenko::rectangle_t maschenko::Circle::getFrameRect() const
+{
+  return rectangle_t{
+    center_,
+    2 * radius_,
+    2 * radius_
+  };
+}
 
-  void Circle::move(double dx, double dy)
-  {
-    center_.x += dx;
-    center_.y += dy;
-  }
+void maschenko::Circle::move(double dx, double dy)
+{
+  center_.x += dx;
+  center_.y += dy;
+}
 
-  void Circle::move(const point_t &center)
-  {
-    center_ = center;
-  }
+void maschenko::Circle::move(const point_t &center)
+{
+  center_ = center;
+}
 
-  void Circle::scale(double coefficient)
+void maschenko::Circle::scale(double coefficient)
+{
+  if (coefficient > 0)
   {
-    if (coefficient > 0)
-    {
-      radius_ *= coefficient;
-    }
-    else
-    {
-      throw std::invalid_argument ("Invalid coefficient for circle");
-    }
+    radius_ *= coefficient;
   }
+  else
+  {
+    throw std::invalid_argument ("Invalid coefficient for circle");
+  }
+}
 
-  void Circle::writeInfo() const
-  {
-    rectangle_t rectangle = getFrameRect();
-    std::cout << std::endl
-              << "Circle with center: (" << center_.x
-              << ";" << center_.y << ")" << std::endl
-              << "radius = " << radius_ << std::endl
-              << "Frame rectangle width = " << rectangle.width
-              << ", height = " << rectangle.height << std::endl
-              << "Area = " << getArea() << std::endl << std::endl;
-  }
+void maschenko::Circle::writeInfo() const
+{
+  rectangle_t rectangle = getFrameRect();
+  std::cout << std::endl
+            << "Circle with center: (" << center_.x
+            << ";" << center_.y << ")" << std::endl
+            << "radius = " << radius_ << std::endl
+            << "Frame rectangle width = " << rectangle.width
+            << ", height = " << rectangle.height << std::endl
+            << "Area = " << getArea() << std::endl << std::endl;
+}
 
-  point_t Circle::getCenter() const
-  {
-    return center_;
-  }
+maschenko::point_t maschenko::Circle::getCenter() const
+{
+  return center_;
+}
 
-  double Circle::getRadius() const
-  {
-    return radius_;
-  }
+double maschenko::Circle::getRadius() const
+{
+  return radius_;
 }
