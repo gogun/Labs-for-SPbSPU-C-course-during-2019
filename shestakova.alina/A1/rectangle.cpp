5abd837646a8c17dd2b5415ebd4d8ec5913ebebb
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2019-03-28 11:00:27 +0000

    Upload New File

---
+++ shestakova.alina/A1/rectangle.cpp
@@ -0,0 +1,32 @@
+#include "rectangle.hpp"
+#include <iostream>
+#include <cassert>
+
+Rectangle::Rectangle(point_t pos, double w, double h) :
+    pos_(pos),
+    width_(w),
+    height_(h)
+{
+    assert((width_ > 0) && (height_ > 0));
+}
+
+double Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return {width_, height_, pos_};
+}
+
+void Rectangle::move(const point_t &point)
+{
+  pos_ = point;
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  pos_.x += dx;
+  pos_.y +=dy;
+}
