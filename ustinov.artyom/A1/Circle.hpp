8c8dbd40005556b9aa596dd2ac39fe76d1287eac
Ustinov Artyom <art.ustinov@mail.ru>
2019-03-28 21:15:04 +0000

    Удалил комментарий и виртуальные методы

--- ustinov.artyom/A1/Circle.hpp
+++ ustinov.artyom/A1/Circle.hpp
@@ -8,14 +8,11 @@ public:
   Circle();
   Circle(double xx, double yy, double radius);
   Circle(point_t center, double radius);
-<<<<<<< HEAD
-=======
-  //~Circle();
->>>>>>> 21efff2bf96f287fe3008cc78e0d089cb8fec88f
-  virtual double getArea() const override;
-  virtual rectangle_t getFrameRect() const override;
-  virtual void move(point_t goal) override;
-  virtual void move(double dx, double dy) override;
+
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t goal) override;
+  void move(double dx, double dy) override;
   void show() const;
 private:
   point_t center_;
