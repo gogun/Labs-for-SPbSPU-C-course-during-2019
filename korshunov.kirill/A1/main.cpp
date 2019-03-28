3b5967e8b3727922ebab572c51dd520f61f1e209
kir0108 <kirill.corschunow@yandex.ru>
2019-03-28 18:48:27 +0300

    A1

--- korshunov.kirill/A1/main.cpp
+++ korshunov.kirill/A1/main.cpp
@@ -10,7 +10,7 @@ int main()
   Circle circle({10,5}, 13);
   circle.move({31,13});
 
-  Shape * shapes[2] = {&rectangle, &circle};
+  Shape * shapes[2] = {&rectangle, &circle,};
 
   int i = 0;
   for (Shape * shape : shapes)
