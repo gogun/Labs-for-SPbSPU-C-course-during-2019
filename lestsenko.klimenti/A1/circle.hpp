b775f6ed24bd33d6c9ab2941e41959db766a4092
Klim Lestsenko <klim.lestsenko@gmail.com>
2019-03-28 15:48:29 +0000

    Upload New File

---
+++ lestsenko.klimenti/A1/circle.hpp
@@ -0,0 +1,23 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "base-types.h"
+#include "shape.h"
+
+class Circle : public Shape
+{
+public:
+  Circle(double, const point_t &);
+  
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t &newPos) override;
+  void move(double dx, double dy) override;
+  point_t getPos() const override;
+
+private:
+  double radius_;
+  point_t center_;
+};
+
+#endif // CIRCLE_HPP
