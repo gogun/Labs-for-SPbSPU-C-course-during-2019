7d8ff4e562ce5cc786bb336ac1a38809d14b90f4
Artyom Ustinov <art.ustinov@mail.ru>
2019-03-28 19:27:32 +0300

    A1

---
+++ ustinov.artyom/A1/Circle.cpp
@@ -0,0 +1,59 @@
+#include "Circle.hpp"
+#include <iostream>
+#include <cmath>
+
+Circle::Circle()
+{
+}
+
+Circle::Circle(double xx, double yy, double radius) :
+//center_{ xx,yy },
+radius_(radius)
+{
+  center_.x = xx;
+  center_.y = yy;
+  //radius_ = radius;
+}
+
+Circle::Circle(point_t goal, double rad) :
+center_(goal),
+radius_(rad)
+{
+}
+
+/*Circle::~Circle()
+{
+}*/
+
+double Circle::getArea() const
+{
+  return M_PI*radius_*radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  
+  rectangle_t rectmp;
+  rectmp.width=2*radius_;
+  rectmp.height=2*radius_;
+  rectmp.pos=center_;
+  
+  return rectmp;
+}
+
+void Circle::move(point_t goal)
+{
+  center_ = goal;
+}
+
+void Circle::move(double dx, double dy)
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+void Circle::show() const
+{
+  std::cout << "\nCircle center: " << center_.x << " , " << center_.y << std::endl;
+  std::cout << "Circle radius: " << radius_ << std::endl;
+}
