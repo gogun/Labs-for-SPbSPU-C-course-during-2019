529b9cad70d79b49c1aa8754e33e941ad8e9b0b0
Klim Lestsenko <klim.lestsenko@gmail.com>
2019-03-28 15:49:56 +0000

    Upload New File

---
+++ lestsenko.klimenti/A1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "circle.h"
+#include "rectangle.h"
+
+int main()
+{
+  Circle circle1(5, { 1, 2 });
+
+  std::cout << "Circle1 first pos: " << circle1.getPos().x << "," << circle1.getPos().y
+      << "\nCircle1 area: " << circle1.getArea()
+      << "\nRectangle surrounding Circle1: h: " << circle1.getFrameRect().height
+      << ", w: " << circle1.getFrameRect().width
+      << ", x: " << circle1.getFrameRect().pos.x
+      << ", y: " << circle1.getFrameRect().pos.y;
+
+  circle1.move({ 3,4 });
+  
+  std::cout << "\nCircle1 second pos: " << circle1.getPos().x << ", " << circle1.getPos().y;
+
+  circle1.move(5, 6);
+  
+  std::cout << "\nCircle1 third pos: " << circle1.getPos().x << ", " << circle1.getPos().y;
+
+  Rectangle rectangle1(3, 4, { 5,6 });
+
+  std::cout << "\n\nRectangle1 first pos: " << rectangle1.getPos().x << ", " << rectangle1.getPos().y
+      << "\nRectangle1 area: " << rectangle1.getArea();
+
+  rectangle1.move({ 3,4 });
+  
+  std::cout << "\nRectangle1 second pos: " << rectangle1.getPos().x << ", " << rectangle1.getPos().y;
+
+  rectangle1.move(5, 6);
+  
+  std::cout << "\nRectangle1 third pos: " << rectangle1.getPos().x << ", " << rectangle1.getPos().y << std::endl;
+  
+  return 0;
+}
