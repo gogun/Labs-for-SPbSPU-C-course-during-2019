d43f4813362cdda808ea9505a88423bdb9417c1c
Ustinov Artyom <art.ustinov@mail.ru>
2019-03-29 15:55:50 +0000

    trying fix all bugs

---
+++ ustinov.artyom/A1/base-types.hpp
@@ -0,0 +1,15 @@
+#ifndef base_types_h
+#define base_types_h
+
+struct point_t
+{
+  double x, y;
+};
+
+struct rectangle_t
+{
+  point_t pos;
+  double width, height;
+};
+
+#endif /* base_types_h */
---
+++ ustinov.artyom/A1/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Shape.hpp"
+#include "Circle.hpp"
+#include "Rectangle.hpp"
+
+int main()
+{
+  double dx = 10;
+  double dy = 20;
+  point_t xpoint;
+  xpoint.x = 333;
+  xpoint.y = 555;
+  Rectangle MyRectangle(100, 200, 123, 57);
+  Circle MyCircle(111, 222, 45);
+  Rectangle OgRectangle;
+  MyRectangle.show();
+  MyCircle.show();
+  std::cout << "Rectangle area = " << MyRectangle.getArea() << std::endl;
+  std::cout << "Circle area =  " << MyCircle.getArea() << std::endl;
+  
+  OgRectangle = MyRectangle.getFrameRect();
+  std::cout << "Rectangle border:" << std::endl;
+  OgRectangle.show();
+  
+  OgRectangle = MyCircle.getFrameRect();
+  std::cout << "Circle border:\n" << std::endl;
+  OgRectangle.show();
+  
+  MyRectangle.move(dx, dy);
+  std::cout << "Move" << std::endl;
+  MyRectangle.show();
+  
+  MyCircle.move(dx, dy);
+  std::cout << "Move\n" << std::endl;
+  MyCircle.show();
+  
+  MyRectangle.move(xpoint);
+  std::cout << "Move\n" << std::endl;
+  MyRectangle.show();
+  
+  MyCircle.move(xpoint);
+  std::cout << "Move\n" << std::endl;
+  MyCircle.show();
+  
+  return 0;
+}
+
