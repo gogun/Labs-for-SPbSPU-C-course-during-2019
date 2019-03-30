f635c290f18ca2a80e6a0ed8e725faf58f33ef49
Ekaterina Zaitceva <zaitceva_ek@mail.ru>
2019-03-30 18:12:52 +0000

    Upload New File

---
+++ zaitceva.ekaterina/A1/circle.cpp
@@ -0,0 +1,27 @@
+#include "circle.hpp"
+#include <cmath>
+#include <cassert>
+
+Circle::Circle(point_t centre, double rad)
+{
+  pos_ = centre;
+  radius_ = rad;
+  assert (radius_ > 0.0);
+}
+double Circle::getArea () const
+{
+  return M_PI*radius_*radius_;
+}
+void Circle::move(point_t pnt)
+{
+  pos_ = pnt;
+}
+void Circle::move(double abs, double ord)
+{
+  pos_.x = pos_.x + abs;
+  pos_.y = pos_.y + ord;
+}
+rectangle_t Circle::getFrameRect() const
+{
+  return {2*radius_, 2*radius_, pos_};
+}
