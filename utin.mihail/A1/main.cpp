94605660215317eb606966ad05ae9fa03a6d7e4a
Utin Mihail <utinspb3@gmail.com>
2019-04-01 01:33:20 +0300

    Added spases

--- utin.mihail/A1/main.cpp
+++ utin.mihail/A1/main.cpp
@@ -4,20 +4,20 @@
 
 int main()
 {
-  Rectangle rect({4,8},6,7);
+  Rectangle rect({4, 8}, 6, 7);
   Shape *shape = &rect;
   shape->printInfo();
-  shape->move(4,4);
+  shape->move(4, 4);
   shape->printInfo();
-  shape->move({1,1});
+  shape->move({1, 1});
   shape->printInfo();
   std::cout << "Area of rectangle is " << shape->getArea() << '\n';
-  Circle circ({4,8},6);
+  Circle circ({4, 8}, 6);
   shape = &circ;
   shape->printInfo();
-  shape->move(5,7);
+  shape->move(5, 7);
   shape->printInfo();
-  shape->move(2,6);
+  shape->move(2, 6);
   shape->printInfo();
   std::cout << "Area of Circle is " << shape->getArea() << '\n';
   return 0;
