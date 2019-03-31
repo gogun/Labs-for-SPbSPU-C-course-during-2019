a93290b2b9967f6d071c1e2c760acf0d408c1550
Sachuk Ilya <ilya-sachuk@mail.ru>
2019-03-31 14:14:27 +0300

    add circle.hpp

---
+++ sachuk.ilya/A1/circle.hpp
@@ -0,0 +1,20 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+class Circle:public Shape{
+public:
+  Circle(point_t point, double rad);
+
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t point) override;
+  void move(double xx, double yy) override;
+  void printInfo() const override;
+private:
+  point_t center_;
+  double radius_;
+};
+
+#endif // CIRCLE_HPP
