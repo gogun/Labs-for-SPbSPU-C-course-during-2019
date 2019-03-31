67dd09448f2d93228e914dc5a4b03706ede77873
atagiev <andrej-tagiev@yandex.ru>
2019-03-31 12:43:39 +0300

    fifth commit

--- tagiev.andrey/A1/main.cpp
+++ tagiev.andrey/A1/main.cpp
@@ -5,11 +5,18 @@
 #include "rectangle.hpp"
 
 int main() {
+
   Rectangle rec({123,100},5,3);
-  Shape* w = &rec;
-  std::cout << w->getArea() << '\n';
+  Shape* shape1 = &rec;
+  shape1->move(10,20);
+  std::cout << shape1->getArea() << '\n';
+
   Circle cir({11,12}, 4);
-  Shape* e = &cir;
-  std::cout << e->getArea() << '\n';
+  Shape* shape2 = &cir;
+  rectangle_t frame = shape2->getFrameRect();
+  point_t tomove = {10,20};
+  shape2->move(tomove);
+  std::cout << "Width of rectangle: " <<frame.width << "\nHeight of rectangle: " << frame.height <<'\n';
+  
   return 0;
 }
