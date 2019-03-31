a83c52ca6e2a138c1f9bcdd1b2f2fa5ca2bc310e
Sachuk Ilya <ilya-sachuk@mail.ru>
2019-03-31 14:15:20 +0300

    add rectangle

---
+++ sachuk.ilya/A1/rectangle.cpp
@@ -0,0 +1,41 @@
+#include "rectangle.hpp"
+
+#include <iostream>
+#include <cassert>
+
+Rectangle::Rectangle(point_t point, double w, double h):
+center_(point),width_(w),height_(h)
+{
+  assert((width_ > 0) && (height_ > 0));
+}
+
+double Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+ void Rectangle::move(point_t point)
+ {
+   center_ = point;
+ }
+
+void Rectangle::move(double xx, double yy)
+{
+  center_.x += xx;
+  center_.y += yy;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return {width_, height_, center_};
+}
+
+void Rectangle::printInfo() const
+{
+  std::cout << "Rectangle:";
+  std::cout << " Width=" << getFrameRect().width << ";";
+  std::cout << " Height=" << getFrameRect().height << std::endl;
+  std::cout << "X=" << getFrameRect().pos.x << ";";
+  std::cout << "Y=" << getFrameRect().pos.y << std::endl;
+  std::cout << "Area=" << getArea() << std::endl << std::endl;
+}
