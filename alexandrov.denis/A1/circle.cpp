5a5fa58367a5a77bdef23a8bdbadb0747013700d
Denis <denisalexandrov99@yandex.ru>
2019-04-01 21:09:19 +0300

    Added A1 implementation

---
+++ alexandrov.denis/A1/circle.cpp
@@ -0,0 +1,32 @@
+#include "circle.hpp"
+#include <iostream>
+#include <math.h>
+#include <cassert>
+
+Circle::Circle(const point_t & center, const double radius):
+  center_(center),
+  radius_(radius)
+{
+  assert(radius_ > 0.0);
+}
+
+void Circle::move(const point_t & newPoint)
+{
+  center_ = newPoint;
+}
+
+void Circle::move(const double dx, const double dy)
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+double Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  return {radius_ * 2, radius_ * 2, center_};
+}
