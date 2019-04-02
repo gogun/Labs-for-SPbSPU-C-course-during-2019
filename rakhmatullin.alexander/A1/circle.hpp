47dbc3ab4b824dcabb1328349d0cd3376f1bf28f
Alexander Rakhmatullin <rar130600@mail.ru>
2019-04-02 14:34:36 +0300

    Исправлено оформление.

--- rakhmatullin.alexander/A1/circle.hpp
+++ rakhmatullin.alexander/A1/circle.hpp
@@ -12,7 +12,7 @@ public:
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(const point_t &) override;
-  void move(const double & ,const double & ) override;
+  void move(const double & ,const double &) override;
 
 private:
   point_t center_;
