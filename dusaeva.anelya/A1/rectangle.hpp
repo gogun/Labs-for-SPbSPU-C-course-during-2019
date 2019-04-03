fc5a7326aeaf6c518e341d4bbd11136617642fdc
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-04 00:54:31 +0300

    поправлены отступы

--- dusaeva.anelya/A1/rectangle.hpp
+++ dusaeva.anelya/A1/rectangle.hpp
@@ -1,21 +1,21 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
-#include "Shape.hpp"
+#include "shape.hpp"
 #include "base-types.hpp"
 
 class Rectangle : public Shape
 {
 public:
-	Rectangle(const point_t &, double, double);
-	double getArea() const override;
-	rectangle_t getFrameRect() const override;
-	void move(double, double) override;
-	void move(const point_t &) override;
+  Rectangle(const point_t &, double, double);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(double, double) override;
+  void move(const point_t &) override;
   void printInfo() const override;
 private:
   point_t position_;
-	double width_;
-	double height_;
+  double width_;
+  double height_;
 };
 
 #endif
