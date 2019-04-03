cea4b23a23203dd9ab71653b49a249b411d0e0e3
Никита Пурнов <nikitapurnov@MacBook-Pro-Nikita.local>
2019-04-03 17:38:29 +0300

    A1

--- purnov.nikita/A1/rectangle.cpp
+++ purnov.nikita/A1/rectangle.cpp
@@ -5,7 +5,7 @@
 Rectangle::Rectangle(const double width, const double height, const point_t & pos) :
   rect_ (rectangle_t {.width = width, .height = height, .pos = pos})
 {
-  assert((width > 0) && (height > 0))
+  assert((width > 0) && (height > 0));
 }
 
 double Rectangle::getArea() const
