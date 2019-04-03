dd7d13b1282383b7c47ec2e1aca814b8eedbbeab
Никита Пурнов <nikitapurnov@MacBook-Pro-Nikita.local>
2019-04-03 21:23:51 +0300

    Add A1

--- purnov.nikita/A1/main.cpp
+++ purnov.nikita/A1/main.cpp
@@ -5,26 +5,26 @@
 
 void showRectangle(rectangle_t rect)
 {
-  std::cout << "width: " << rect.width << ", height: " << rect.height;
-  std::cout << ", x: " << rect.pos.x << ", y: " << rect.pos.y;
-  std::cout << std::endl;
+    std::cout << "width: " << rect.width << ", height: " << rect.height;
+    std::cout << ", x: " << rect.pos.x << ", y: " << rect.pos.y;
+    std::cout << std::endl;
 }
 
 void show(const Shape & shape)
 {
-  std::cout << __LINE__ << ": " << shape.getArea() << std::endl;
-  showRectangle(shape.getFrameRect());
+    std::cout << __LINE__ << ": " << shape.getArea() << std::endl;
+    showRectangle(shape.getFrameRect());
 }
 
 int main()
 {
-  Rectangle rect (10.0, 5.0, {.x = 15.0, .y = 15.0});
-  show(rect);
-  rect.move({.x = -10, .y =-5});
-  show(rect);
+    Rectangle rect (10.0, 5.0, {.x = 15.0, .y = 15.0});
+    show(rect);
+    rect.move({.x = -10, .y =-5});
+    show(rect);
     
-  Circle circle (5.0, {.x = 1.0, .y = 2.0 });
-  show(circle);
+    Circle circle (5.0, {.x = 1.0, .y = 2.0 });
+    show(circle);
     
-  return 0;
+    return 0;
 }
--- purnov.nikita/A1/rectangle.hpp
+++ purnov.nikita/A1/rectangle.hpp
@@ -6,15 +6,15 @@
 class Rectangle : public Shape
 {
 public:
-  Rectangle(const double width, const double height, const point_t & pos);
-  virtual double getArea() const;
-  virtual rectangle_t getFrameRect() const;
-  virtual void move(const point_t & pos);
-  virtual void move(double x, double y);
+    Rectangle(const double width, const double height, const point_t & pos);
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(const point_t & pos);
+    virtual void move(double x, double y);
     
 private:
-  rectangle_t rect_;
+    rectangle_t rect_;
 };
 
-#endif 
+#endif
 
