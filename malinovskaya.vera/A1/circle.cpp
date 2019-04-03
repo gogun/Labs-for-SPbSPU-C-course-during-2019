8c1ab1d115a6714bd59f5bfeb49c7140c228989a
vera.mal <vera8malinovskaya@gmail.com>
2019-04-03 18:21:31 +0300

    A1

---
+++ malinovskaya.vera/A1/circle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include "circle.hpp"
+
+Circle::Circle(const point_t &center, double radius) :
+	center_(center),
+	radius_(radius)
+{
+	assert(radius > 0.0);
+}
+
+double Circle::getArea() const
+{
+	return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+	return { 2 * radius_, 2 * radius_, center_ };
+}
+
+void Circle::move(double dx, double dy)
+{
+	center_.x += dx;
+	center_.y += dy;
+}
+
+void Circle::move(const point_t &point)
+{
+	center_ = point;
+}
+
+void Circle::print() const
+{
+	std::cout << "Center: (" << getFrameRect().pos.x << "; " << getFrameRect().pos.y << ")" << std::endl;
+}
