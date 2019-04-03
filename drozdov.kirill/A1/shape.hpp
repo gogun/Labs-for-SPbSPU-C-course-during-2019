9fb2077c56242b3158b1e243c19c207c098737f0
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-03 21:32:33 +0300

    Добавил методы выполняющие общую работу и их тела.

--- drozdov.kirill/A1/shape.hpp
+++ drozdov.kirill/A1/shape.hpp
@@ -7,6 +7,8 @@ class Shape
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void displayData() const = 0;
+    void move (double dx, double dy);
+    void move (point_t dot);
   private:
     point_t pos_;
 };
