3fd2c0c7eb78785deb6bfe50bb5be34e31f7b751
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-03 21:32:33 +0300

    Добавил методы выполняющие общую работу и их тела.

--- drozdov.kirill/A1/shape.cpp
+++ drozdov.kirill/A1/shape.cpp
@@ -1,5 +1,22 @@
 #include "shape.hpp"
+#include <iostream>
 
 Shape::Shape(point_t pos):
   pos_(pos)
 {}
+
+void Shape::displayData() const
+{
+  std::cout << "\nPosition: (" << pos_.x << ';' << pos_.y << ')';
+}
+
+void Shape::move(double dx, double dy)
+{
+  pos_.x += dx;
+  pos_.y += dy;
+}
+
+void Shape::move(point_t dot)
+{
+  pos_ = dot;
+}
