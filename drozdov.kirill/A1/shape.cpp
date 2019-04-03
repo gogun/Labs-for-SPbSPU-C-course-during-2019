5981430c7a3f2957bd758567ab49862d645c2489
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-03 21:29:38 +0300

    Добавил конструктор для класса

---
+++ drozdov.kirill/A1/shape.cpp
@@ -0,0 +1,5 @@
+#include "shape.hpp"
+
+Shape::Shape(point_t pos):
+  pos_(pos)
+{}
--- drozdov.kirill/A1/shape.hpp
+++ drozdov.kirill/A1/shape.hpp
@@ -3,6 +3,7 @@
 class Shape
 {
   public:
+    Shape(point_t pos);
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void displayData() const = 0;
