bf596efbc52d53e555833fd1384db04d4d243afc
Denis <denisalexandrov99@yandex.ru>
2019-04-03 21:18:43 +0300

    correction A1
      modified: shape.hpp.
    
      shape.hpp:
        4 spaces replaced by 2 in lines 8-12.

--- alexandrov.denis/A1/shape.hpp
+++ alexandrov.denis/A1/shape.hpp
@@ -5,11 +5,11 @@
 class Shape
 {
 public:
-    virtual ~Shape() = default;
-    virtual void move(const point_t & newPoint) = 0;
-    virtual void move(const double dx, const double dy) = 0;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
+  virtual ~Shape() = default;
+  virtual void move(const point_t & newPoint) = 0;
+  virtual void move(const double dx, const double dy) = 0;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
 };
 
 #endif // SHAPE_HPP
