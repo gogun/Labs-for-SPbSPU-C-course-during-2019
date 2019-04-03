aa32dde5be05dfe5c5bcaad6f0b2b31e460c8421
MaLebDen <lebedev.dm00@mail.ru>
2019-04-03 21:18:33 +0300

    push implimentation of lab1

---
+++ lebedev.denis/A1/circle.hpp
@@ -0,0 +1,20 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+#include "shape.hpp"
+
+class Circle : public Shape 
+{
+public:	
+  Circle(point_t pos, double radius);
+  
+  double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  void move(double x, double y) override;
+  void move(point_t poit) override;
+  
+private:
+  double  m_radius;
+  point_t m_position;
+};
+
+#endif
