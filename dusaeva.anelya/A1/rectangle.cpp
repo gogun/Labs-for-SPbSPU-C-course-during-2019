539f438315dc916c11f3b07683871ca0a3a19a58
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-04 00:25:32 +0300

    rectangle

---
+++ dusaeva.anelya/A1/rectangle.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cassert>
+#include <iostream>
+#include "base-types.hpp"
+#include "rectangle.hpp"
+
+Rectangle::Rectangle(const point_t &centre, double width, double height) :
+  position_(centre),
+  width_(width),
+  height_(height)
+{
+  assert(width >= 0 || height >= 0);
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  position_.x += dx;
+  position_.y += dy;
+}
+
+void Rectangle::move(const point_t &param)
+{
+  position_ = param;
+}
+
+double Rectangle::getArea() const
+{
+  return height_ * width_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rectangle_t {width_, height_,{position_.x, position_.y}};
+}
+
+void Rectangle::printInfo() const
+{
+  rectangle_t info = getFrameRect();
+  std::cout << "Width = " << width_ << std::endl;
+  std::cout << "Height = " << height_ << std::endl;
+  std::cout << "Area = " << getArea() << std::endl;
+  std::cout << "Central coordinates (" << position_.x << ", " << position_.y << ")" << std::endl;
+  std::cout << "Central coordinates of frame rectangle: (" << info.pos.x << ", " << info.pos.y << ")" << std::endl;
+  std::cout << "Frame width = " << info.width <<", frame height = " << info.height << std::endl;
+  std::cout << std::endl;
+}
