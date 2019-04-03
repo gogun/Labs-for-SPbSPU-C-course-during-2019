cd09f611d5b2af5ac73f398ccd945caa4b8f3b25
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-03 21:40:54 +0300

    Добавлена проверка методов класса Окружность

--- drozdov.kirill/A1/main.cpp
+++ drozdov.kirill/A1/main.cpp
@@ -6,7 +6,6 @@ void showFrame(const rectangle_t frame);
 
 int main()
 {
-
   /*test on example Rectangle:*/
   Rectangle rect1(7.2, 5.7, {5, -2.4});
   Shape * r1 = & rect1;
@@ -20,6 +19,21 @@ int main()
   r1->move(2.5, -4.1);
   std::cout << "\n==============================After move -> +2.5, -4.1==============================" << std::endl;
   r1->displayData();
+
+  /*test on example Circle:*/
+  Circle circ1(5);
+  Shape *c1 = &circ1;
+
+  std::cout << "\nCircle: ";
+  c1->displayData();
+  std::cout << "\nArea: " << c1->getArea() << std::endl;
+
+  frameRect = c1->getFrameRect();
+  showFrame(frameRect);
+  point_t position = {-1.8, 2.4};
+  c1->move(position);
+  std::cout << "\n==============================After move to (-1.8, 2.4)==============================";
+  c1->displayData();
 
   std::cout << std::endl;
   return 0;
