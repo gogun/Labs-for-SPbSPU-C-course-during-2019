600125088e6f6b06ccd75d7c12cc62b4abd3da1d
IvlevaAnna <annaiva19@mail.ru>
2019-04-03 22:38:39 +0300

    Added lab1 implementation

--- ivleva.anna/A1/rectangle.cpp
+++ ivleva.anna/A1/rectangle.cpp
@@ -5,33 +5,34 @@
 Rectangle::Rectangle(const double width, const double height, const point_t & pos) :
 	rect_ (rectangle_t {.width = width, .height = height, .pos = pos})
 {
-
-	if (width <= 0.0) {
-		std::cerr << "Invalid rectangle width, shall be greater than 0.0, is: " << width << std::endl;
-	}
-	if (height <= 0.0) {
-		std::cerr << "Invalid rectangle height, shall be greater than 0.0, is: " << height << std::endl;
-	}
+  if (width <= 0.0) 
+  {
+  std::cerr << "Invalid rectangle width, shall be greater than 0.0, is: " << width << std::endl;
+  }
+  if (height <= 0.0) 
+  {
+  std::cerr << "Invalid rectangle height, shall be greater than 0.0, is: " << height << std::endl;
+  }
 }
 
 double Rectangle::getArea() const
 {
-	return (rect_.width * rect_.height);
+  return (rect_.width * rect_.height);
 }
 
 rectangle_t Rectangle::getFrameRect() const 
 {
-	return rect_;
+  return rect_;
 }
 
 void Rectangle::move(const point_t & pos) 
 {
-	rect_.pos.x = pos.x;
-	rect_.pos.y = pos.y;
+  rect_.pos.x = pos.x;
+  rect_.pos.y = pos.y;
 }
 
 void Rectangle::move(double x, double y) 
 {
-	rect_.pos.x += x;
-	rect_.pos.y += y;
+  rect_.pos.x += x;
+  rect_.pos.y += y;
 }
