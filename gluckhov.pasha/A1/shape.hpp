de39cb34e7f9b40366e59d1a6e26a1e0177a271d
reqojkonzi <fazekonzi@gmail.com>
2019-04-04 12:52:23 +0300

    Добавлен для всех классов метод отображения данных.

--- gluckhov.pasha/A1/shape.hpp
+++ gluckhov.pasha/A1/shape.hpp
@@ -9,6 +9,7 @@ public:
   virtual ~Shape() = default;
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
+  virtual void displayData() const = 0;
   virtual void move(double dx, double dy) = 0;
   virtual void move(point_t newPos) = 0;
 };
