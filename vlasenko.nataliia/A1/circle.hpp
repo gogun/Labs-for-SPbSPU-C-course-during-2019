621047a42dc808832efea940464e32a6c330a609
Somn117 <somn117@yandex.ru>
2019-04-04 23:24:57 +0300

    changed design

--- vlasenko.nataliia/A1/circle.hpp
+++ vlasenko.nataliia/A1/circle.hpp
@@ -6,17 +6,17 @@
 class Circle : public Shape
 {
 public:
-	Circle(const point_t &, const double);
+  Circle(const point_t &, const double);
 
-	double getArea() const;
-	rectangle_t getFrameRect() const;
-	void printInfo() const;
-	void move(const point_t & position);
-	void move(const double &, const double &);
+  double getArea() const;
+  rectangle_t getFrameRect() const;
+  void printInfo() const;
+  void move(const point_t & position);
+  void move(const double &, const double &);
 
 private:
-	double radius_;
-	point_t center_;
+  double radius_;
+  point_t center_;
 };
 
 #endif
