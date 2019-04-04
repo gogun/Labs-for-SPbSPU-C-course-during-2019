ee0c8bcb1d721ebf6fa2c3c1e5868e7d3915f74c
Somn117 <somn117@yandex.ru>
2019-04-04 23:25:19 +0300

    changed design

--- vlasenko.nataliia/A1/circle.cpp
+++ vlasenko.nataliia/A1/circle.cpp
@@ -7,43 +7,43 @@
 #include "shape.hpp"
 
 Circle::Circle(const point_t &position, const double radius) :
-	radius_(radius),
-	center_(position)
+  radius_(radius),
+  center_(position)
 {
-	assert(radius_ > 0);
+  assert(radius_ > 0);
 }
 
 double Circle::getArea() const
 {
-	return (M_PI * radius_ * radius_);
+  return (M_PI * radius_ * radius_);
 }
 
 rectangle_t Circle::getFrameRect() const
 {
-	return { 2 * radius_, 2 * radius_, center_ };
+  return { 2 * radius_, 2 * radius_, center_ };
 }
 
 void Circle::printInfo() const
 {
-	std::cout << "\t Information about circle:\n";
-	std::cout << "\t Radius: " << radius_ << " \n";
-	std::cout << "\t Area of circle: " << getArea() << " \n";
-	std::cout << "\t Center: (" << center_.x << "," << center_.y << "). \n";
-	std::cout << "\t Frame rectangle:" << " \n";
-	rectangle_t frameRectangle = getFrameRect();
-	std::cout << "\t Polymorphism: \n";
-	std::cout << "\t Width: " << frameRectangle.width << "\n";
-	std::cout << "\t Height: " << frameRectangle.height << "\n";
-	std::cout << "\t Center: (" << frameRectangle.pos.x << "," << frameRectangle.pos.y << ").\n\n";
+  std::cout << "\t Information about circle:\n";
+  std::cout << "\t Radius: " << radius_ << " \n";
+  std::cout << "\t Area of circle: " << getArea() << " \n";
+  std::cout << "\t Center: (" << center_.x << "," << center_.y << "). \n";
+  std::cout << "\t Frame rectangle:" << " \n";
+  rectangle_t frameRectangle = getFrameRect();
+  std::cout << "\t Polymorphism: \n";
+  std::cout << "\t Width: " << frameRectangle.width << "\n";
+  std::cout << "\t Height: " << frameRectangle.height << "\n";
+  std::cout << "\t Center: (" << frameRectangle.pos.x << "," << frameRectangle.pos.y << ").\n\n";
 }
 
 void Circle::move(const point_t &position)
 {
-	center_ = position;
+  center_ = position;
 }
 
 void Circle::move(const double &shift_x, const double &shift_y)
 {
-	center_.x += shift_x;
-	center_.y += shift_y;
+  center_.x += shift_x;
+  center_.y += shift_y;
 }
