a6b4b35d4411ac2716af7801d2e0c80e548ee00f
MaLebDen <lebedev.dm00@mail.ru>
2019-04-04 19:21:10 +0300

    fix Error

--- lebedev.denis/A1/circle.hpp
+++ lebedev.denis/A1/circle.hpp
@@ -3,19 +3,21 @@
 
 #include "shape.hpp"
 
-class Circle: public Shape 
+class Circle : public Shape
 {
-  public:	
-    Circle(point_t pos, double radius);
-  
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(double x, double y) override;
-    void move(point_t poit) override;
+public:
+	Ciecle(point_t pos, double radius);
 
-  private:
-    double  m_radius;
-    point_t m_position;
+	double getArea() const override;
+	rectangle_t getFrameRect() const override;
+	void move(double x, double y) override;
+	void move(point_t poit) override;
+
+private:
+	double m_radius;
+	point_t m_pos;
 };
 
 #endif
+
+
