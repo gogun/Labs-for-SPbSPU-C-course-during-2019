86bf1ca754a92f59e8d0d19e27b61a81d4292fd1
Somn117 <somn117@yandex.ru>
2019-04-04 22:12:48 +0300

    Added rectangle.hpp

---
+++ vlasenko.nataliia/A1/rectangle.hpp
@@ -0,0 +1,24 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+#include "base-types.hpp"
+
+class Rectangle : public Shape
+{
+public:
+	Rectangle(const point_t &, const double, const double);//убрать конст
+
+	double getArea() const;
+	rectangle_t getFrameRect() const;
+	void printInfo() const;
+	void move(const point_t &);
+	void move(const double &, const double &);
+
+private:
+	double height_;
+	double width_;
+	point_t center_;
+};
+
+#endif
