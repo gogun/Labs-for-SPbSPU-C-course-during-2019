502a4a1f2c739ddef830a0b1094dda8433d6cd51
Somn117 <somn117@yandex.ru>
2019-04-04 23:23:26 +0300

    changed design

--- vlasenko.nataliia/A1/main.cpp
+++ vlasenko.nataliia/A1/main.cpp
@@ -4,27 +4,27 @@
 
 int main()
 {
-	std::cout << "\t  //////////Rectangle//////////\n";
-	Rectangle rectangle({3.5, 1.2}, 7, 9);
-	Shape * shape = &rectangle;
-	shape->printInfo();
-	std::cout << "\t Transfer center of rectangle to (3.5+6.3, 1.2+7.3)\n";
-	shape->move(6.3, 7.3);
-	shape->printInfo();
-	std::cout << "\t Move rectangle to (15.0, 21.9)\n";
-	shape->move({15.0, 21.9});
-	shape->printInfo();
+  std::cout << "\t  //////////Rectangle//////////\n";
+  Rectangle rectangle({3.5, 1.2}, 7, 9);
+  Shape * shape = &rectangle;
+  shape->printInfo();
+  std::cout << "\t Transfer center of rectangle to (3.5+6.3, 1.2+7.3)\n";
+  shape->move(6.3, 7.3);
+  shape->printInfo();
+  std::cout << "\t Move rectangle to (15.0, 21.9)\n";
+  shape->move({15.0, 21.9});
+  shape->printInfo();
 
-	std::cout << "\t  ///////////Circle///////////\n";
-	Circle circle({2.0, 5.2}, 8.4);
-	shape = &circle;
-	shape->printInfo();
-	std::cout << "\t Transfer center of circle to (2.0+7.0, 5.2+1.0)\n";
-	shape->move(7.0, 1.0);
-	shape->printInfo();
-	std::cout << "\t Move to (-27.1, 9.0)\n";
-	shape->move({-27.1, 9.0});
-	shape->printInfo();
+  std::cout << "\t  ///////////Circle///////////\n";
+  Circle circle({2.0, 5.2}, 8.4);
+  shape = &circle;
+  shape->printInfo();
+  std::cout << "\t Transfer center of circle to (2.0+7.0, 5.2+1.0)\n";
+  shape->move(7.0, 1.0);
+  shape->printInfo();
+  std::cout << "\t Move to (-27.1, 9.0)\n";
+  shape->move({-27.1, 9.0});
+  shape->printInfo();
 
-	return 0;
+  return 0;
 }
