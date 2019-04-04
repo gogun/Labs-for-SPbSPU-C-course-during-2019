418534d8b1c6ec1700b42db9ddb5df18d2b6b1c4
Somn117 <somn117@yandex.ru>
2019-04-04 23:23:57 +0300

    changed design

--- vlasenko.nataliia/A1/shape.hpp
+++ vlasenko.nataliia/A1/shape.hpp
@@ -6,13 +6,13 @@
 class Shape
 {
 public:
-	virtual ~Shape() = default;
+  virtual ~Shape() = default;
 
-	virtual double getArea() const = 0;
-	virtual rectangle_t getFrameRect() const = 0;
-	virtual void printInfo() const = 0;
-	virtual void move(const point_t &) = 0;
-	virtual void move(const double &, const double &) = 0;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void printInfo() const = 0;
+  virtual void move(const point_t &) = 0;
+  virtual void move(const double &, const double &) = 0;
 };
 
 #endif
