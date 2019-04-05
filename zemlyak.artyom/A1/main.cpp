a3ee2f2d191a54e99892a0186a0154eb7ac8b91b
ArtyomZemlyak <zemlyak.aa@edu.spbstu.ru>
2019-04-05 10:12:28 +0000

    Исправлен пример с полиморфизмом.

--- zemlyak.artyom/A1/main.cpp
+++ zemlyak.artyom/A1/main.cpp
@@ -12,18 +12,11 @@ int main()
   std::cout << "Obtaining information about the first object" << std::endl;
   Shape *shape_1 = &circle_1;
   shape_1->printInfo();
-
-  std::cout << "Creating a rectangle by coordinates X = 0 and Y = 5 with width = 5 and height = 15." << std::endl;
-  Rectangle rectangle_1 = Rectangle({0, 5}, 5, 15);
-
-  std::cout << "Obtaining information about the second object" << std::endl;
-  Shape *shape_2 = &rectangle_1;
-  shape_2->printInfo();
   std::cout << std::endl;
 
   std::cout << "Move the first object on X = 5 and Y = 10. Info:" << std::endl;
   shape_1->move(5, 10);
-  shape_1->printInfo();
+  circle_1.printInfo();
   std::cout << std::endl;
 
   std::cout << "Setting the new coordinates X = 1 and Y = 1 for the first object. Info:" << std::endl;
@@ -31,14 +24,22 @@ int main()
   shape_1->printInfo();
   std::cout << std::endl;
 
+  std::cout << "Creating a rectangle by coordinates X = 0 and Y = 5 with width = 5 and height = 15." << std::endl;
+  Rectangle rectangle_1 = Rectangle({0, 5}, 5, 15);
+
+  std::cout << "Obtaining information about the second object" << std::endl;
+  shape_1 = &rectangle_1;
+  shape_1->printInfo();
+  std::cout << std::endl;
+
   std::cout << "Move the second object on X = 5 and Y = 0. Info:" << std::endl;
-  shape_2->move(5, 0);
-  shape_2->printInfo();
+  shape_1->move(5, 0);
+  shape_1->printInfo();
   std::cout << std::endl;
 
   std::cout << "Setting the new coordinates X = 10 and Y = 10  for the second object. Info:" << std::endl;
-  shape_2->move({10, 10});
-  shape_2->printInfo();
+  shape_1->move({10, 10});
+  shape_1->printInfo();
   std::cout << std::endl;
 
   std::cout << "End of program A1." << std::endl;
