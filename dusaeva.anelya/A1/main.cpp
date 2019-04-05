4db17bb59da0214092156946ff38624a1c2d83e3
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-05 17:33:37 +0300

    изменена работа с ссылками и указателями

--- dusaeva.anelya/A1/main.cpp
+++ dusaeva.anelya/A1/main.cpp
@@ -6,12 +6,12 @@
 int main()
 {
   //Rectangle
-  Rectangle objRect({5.0,6.0}, 7.0, 1.0);
+  Rectangle objRect({5.0, 6.0}, 7.0, 1.0);
   Shape *shape = &objRect;
 
   std::cout << "Rectangle:" << std::endl;
   shape->printInfo();
-  shape->move(5.0, 6.0);
+  shape->move(5.1, 6.2);
   std::cout << "Coordinate shift:" << std::endl;
   shape->printInfo();
   shape->move({0.0, 3.0});
@@ -21,16 +21,16 @@ int main()
 
   //Circle
   Circle objCirc({1.0, 5.0}, 6.0);
-  Shape *shapeC = &objCirc;
+  shape = &objCirc;
 
   std::cout << "Circle:" << std::endl;
-  shapeC->printInfo();
-  shapeC->move(1.0, 2.0);
+  shape->printInfo();
+  shape->move(1.0, 2.0);
   std::cout << "Coordinate shift:" << std::endl;
-  shapeC->printInfo();
-  shapeC->move({7.0, -9.0});
+  shape->printInfo();
+  shape->move({7.8, -9.0});
   std::cout << "Point shift:" << std::endl;
-  shapeC->printInfo();
+  shape->printInfo();
   std::cout << std::endl;
 
   return 0;
