b31f8be7ace6daa9c2616c0fc1d58acef6fb8048
alexandragoncharova <alexandra.andronova.99@mail.ru>
2019-04-05 15:03:14 +0300

    A1

--- goncharova.alexandra/A1/Shape.hpp
+++ goncharova.alexandra/A1/Shape.hpp
@@ -1,16 +1,31 @@
 #ifndef Shape_hpp
+
 #define Shape_hpp
+
+
 
 #include "base_types.hpp"
 
+
+
 class Shape
+
 {
+
 public:
+
   virtual ~Shape() = default;
+
   virtual double getArea() const = 0;
+
   virtual rectangle_t getFrameRect() const = 0;
+
   virtual void move(const point_t &newPoint) = 0;
+
   virtual void move(double dx, double dy) = 0;
+
 };
+
+
 
 #endif
