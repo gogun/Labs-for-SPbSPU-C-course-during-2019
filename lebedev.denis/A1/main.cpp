09ae537d2a3ef757685f5b839f3fac88f25a197f
MaLebDen <lebedev.dm00@mail.ru>
2019-04-05 22:34:38 +0300

    Удалил лишние строки.

--- lebedev.denis/A1/main.cpp
+++ lebedev.denis/A1/main.cpp
@@ -20,19 +20,14 @@ int main()
 {
   Rectangle r1({ 5.00, 10.00 }, 3.00, 4.00);
   std::cout<<"Rectangle."<<"\n";
-	
   showAll(r1);
   r1.move({ .x = 7.00, .y = 8.00 });
-  
   std::cout<<"After move"<<"\n";
   showAll(r1);
- 
   Circle c1({ 1.00, 3.00 }, 3.00);
   std::cout<<"Circle."<<"\n";
-  
   showAll(c1);
   c1.move(3.00, -1.5);
-  
   std::cout<<"After move"<<"\n";
   showAll(c1);
   return 0;
