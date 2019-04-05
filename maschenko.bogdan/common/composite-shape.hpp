bd748d9801f803c497fe05ebe83bf39a7fc20c33
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-04-05 20:09:54 +0300

    rename 2 files (previous time it was be incorrect)

---
+++ maschenko.bogdan/common/composite-shape.hpp
@@ -0,0 +1,28 @@
+#ifndef A3_COMPOSITE_SHAPE_HPP
+#define A3_COMPOSITE_SHAPE_HPP
+
+#include "shape.hpp"
+#include <memory>
+
+class CompositeShape : public maschenko::Shape
+{
+public:
+  CompositeShape(maschenko::Shape *shape);
+
+  double getArea() const override;
+  maschenko::rectangle_t getFrameRect() const override;
+  void move(double dx, double dy) override;
+  void move(const maschenko::point_t &center) override;
+  void scale(double coefficient) override;
+  void writeInfo() const override;
+  maschenko::point_t getCenter() const;
+  int getShapeQuantity() const;
+  void addShape(maschenko::Shape *shape);
+
+private:
+  maschenko::point_t pos_;
+  int shape_quantity_;
+  std::unique_ptr<maschenko::Shape*[]> shape_array_;
+};
+
+#endif //A3_COMPOSITE_SHAPE_HPP
