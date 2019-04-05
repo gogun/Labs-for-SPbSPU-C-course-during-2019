1fe151d90d8e299b3ae25cc47b088f658238e216
vera.mal <vera8malinovskaya@gmail.com>
2019-04-05 15:38:31 +0300

    Изменены имена

--- malinovskaya.vera/A1/main.cpp
+++ malinovskaya.vera/A1/main.cpp
@@ -5,41 +5,41 @@
 int main()
 {
   Circle objectCircle({ 4.0, 2.0 }, 3.0);
-  Shape *pointer = &objectCircle;
+  Shape *shapePointer = &objectCircle;
   std::cout << "Circle:" << std::endl;
   std::cout << std::endl;
-  std::cout << "Area: " << pointer->getArea() << std::endl;
-  std::cout << "Frame (width): " << pointer->getFrameRect().width << std::endl;
-  std::cout << "Frame (height): " << pointer->getFrameRect().height << std::endl;
-  pointer->print();
+  std::cout << "Area: " << shapePointer->getArea() << std::endl;
+  std::cout << "Frame (width): " << shapePointer->getFrameRect().width << std::endl;
+  std::cout << "Frame (height): " << shapePointer->getFrameRect().height << std::endl;
+  shapePointer->print();
   std::cout << std::endl;
   std::cout << "Shift: dx = " << -2 << ", dy = " << 4 << std::endl;
-  pointer->move(-2.0, 4.0);
-  pointer->print();
+  shapePointer->move(-2.0, 4.0);
+  shapePointer->print();
   std::cout << std::endl;
   std::cout << "Move to (" << 0 << ", " << -6 << ")" << std::endl;
-  pointer->move({0.0, -6.0});
-  pointer->print();
+  shapePointer->move({0.0, -6.0});
+  shapePointer->print();
 
   std::cout << std::endl;
   std::cout << std::endl;
 
   Rectangle objectRectangle({ 3.0, 4.0, { 5.0, 8.0 } });
-  pointer = &objectRectangle;
+  shapePointer = &objectRectangle;
   std::cout << "Rectangle:" << std::endl;
   std::cout << std::endl;
-  std::cout << "Area: " << pointer->getArea() << std::endl;
-  std::cout << "Frame (width): " << pointer->getFrameRect().width << std::endl;
-  std::cout << "Frame (height): " << pointer->getFrameRect().height << std::endl;
-  pointer->print();
+  std::cout << "Area: " << shapePointer->getArea() << std::endl;
+  std::cout << "Frame (width): " << shapePointer->getFrameRect().width << std::endl;
+  std::cout << "Frame (height): " << shapePointer->getFrameRect().height << std::endl;
+  shapePointer->print();
   std::cout << std::endl;
   std::cout << "Shift: dx = " << 8 << ", dy = " << 2 << std::endl;
-  pointer->move(8.0, 2.0);
-  pointer->print();
+  shapePointer->move(8.0, 2.0);
+  shapePointer->print();
   std::cout << std::endl;
   std::cout << "Move to (" << -5 << ", " << -10 << ")" << std::endl;
-  pointer->move({ -5.0, -10.0 });
-  pointer->print();
+  shapePointer->move({ -5.0, -10.0 });
+  shapePointer->print();
 
   return 0;
 }
