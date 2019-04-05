bde8b0c5d4c1452132c529f35af989299720a91b
BAZOUNE MAHDI FARES <bazounepeter96@gmail.com>
2019-04-05 13:49:38 +0000

    Update bazoune.mahdi/A1/main.cpp

--- bazoune.mahdi/A1/main.cpp
+++ bazoune.mahdi/A1/main.cpp
@@ -11,9 +11,9 @@ int main()
   myCircle->print();
   std::cout << "circle area " << myCircle.getArea() <<std:: endl;
   myCircle->move(1, 2);
-  std::cout <<"\n";
+  std::cout <<'\n';
   std::cout << "move the circle with (1,2)"<<std:: endl;
-  std::cout <<"\n";
+  std::cout <<'\n';
   std::cout << "frame rect.x= "<<myCircle.getFrameRect().pos.x <<std:: endl;
   std::cout << "frame rect.y= "<<myCircle.getFrameRect().pos.y <<std:: endl;
   myCircle->move(myPoint);
@@ -23,7 +23,7 @@ int main()
   myRect->move(3, 4);
   std::cout << '\n';
   std::cout << "move the rectangle with (3,4)"<< std:: endl;
-  std::cout << "\n";
+  std::cout << '\n';
   myRect->print();
   std::cout << "rectangle area = "<<myRect.getArea()<<std::endl;
   std::cout << '\n';
