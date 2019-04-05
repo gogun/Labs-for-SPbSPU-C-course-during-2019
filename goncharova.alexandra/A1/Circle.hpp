41ba324aac1e23e7762d13265eca0d45279b430b
alexandragoncharova <alexandra.andronova.99@mail.ru>
2019-04-05 14:20:45 +0300

    A1

---
+++ goncharova.alexandra/A1/Circle.hpp
@@ -0,0 +1,18 @@
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include "Shape.hpp"
+
+class Circle: public Shape
+{
+public:
+  double radius_;
+  point_t center_;
+  Circle(const point_t &center, double radius);
+  double getArea() const;
+  rectangle_t getFrameRect() const;
+  void move(const point_t &newPoint);
+  void move(double dx, double dy);
+};
+
+#endif
---
+++ goncharova.alexandra/A1/Rectangle.hpp
@@ -0,0 +1,19 @@
+#ifndef Rectangle_hpp
+#define Rectangle_hpp
+
+#include "Shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  double height_;
+  double width_;
+  point_t center_;
+  Rectangle(const point_t &center, double width, double height);
+  double getArea() const;
+  rectangle_t getFrameRect() const;
+  void move(const point_t &newPoint);
+  void move(double dx, double dy);
+};
+
+#endif
