520dc93aad3da3af299d934f701b288948bff8dd
Wiserax.Kar.av <eskercorps@gmail.com>
2019-04-06 00:34:28 +0300

    Merged master fixed conflict

--- karpenko.alexey/A1/rectangle.cpp
+++ karpenko.alexey/A1/rectangle.cpp
@@ -4,40 +4,23 @@
 Rectangle::Rectangle(const rectangle_t &rect) :
   rect_(rect)
 {
-<<<<<<< HEAD
   assert((rect.width > 0.0)&&(rect.height > 0.0));
-=======
-  assert((rect.width >= 0.0)&&(rect.height >= 0.0));
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
 }
 
 double Rectangle::getArea() const
 {
-<<<<<<< HEAD
   return rect_.width * rect_.height;
-=======
-  return (rect_.width * rect_.height);
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
 }
 
 rectangle_t Rectangle::getFrameRect() const
 {
-<<<<<<< HEAD
   return rect_;
-=======
-  return (rect_);
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
 }
 
 void Rectangle::move(const double dx, const double dy)
 {
-<<<<<<< HEAD
   rect_.pos.x += dx;
   rect_.pos.y += dy;
-=======
-  rect_.pos.x = dx;
-  rect_.pos.y = dy;
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
 }
 
 void Rectangle::move(const point_t &newPos)
--- karpenko.alexey/A1/shape.hpp
+++ karpenko.alexey/A1/shape.hpp
@@ -1,19 +1,13 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
-<<<<<<< HEAD
 
-=======
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
 #include "base-types.hpp"
 
 class Shape
 {
 public:
   virtual ~Shape() = default;
-<<<<<<< HEAD
   
-=======
->>>>>>> 88285cbc9e7fb8678fba0deb06bb01127088a644
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t &newPos) = 0;
