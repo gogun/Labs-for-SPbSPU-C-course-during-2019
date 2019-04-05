f8bb592b7a64f39d31ead0334b894b52e9d8bf4e
atagiev <andrej-tagiev@yandex.ru>
2019-04-05 12:20:57 +0300

    seventh commit Добавил вызов всех методотов в main для обоих фигур; Назвал их чуть понятнее, чем фигура1 и фигура2;

--- tagiev.andrey/A1/main.cpp
+++ tagiev.andrey/A1/main.cpp
@@ -7,16 +7,24 @@
 int main() {
 
   Rectangle rec({123,100},5,3);
-  Shape* shape1 = &rec;
-  shape1->move(10,20);
-  std::cout << shape1->getArea() << '\n';
+  Shape* rectangle = &rec;
+  rectangle->move(10,20);
+  point_t tomove = {1,2};
+  rectangle->move(tomove);
+  std::cout << rectangle->getArea() << '\n';
+  rectangle_t frame = rectangle->getFrameRect();
+  std::cout << "Width of rectangle: " <<frame.width << '\n';
+  std::cout << "Height of rectangle: " << frame.height <<'\n';
 
   Circle cir({11,12}, 4);
-  Shape* shape2 = &cir;
-  rectangle_t frame = shape2->getFrameRect();
-  point_t tomove = {10,20};
-  shape2->move(tomove);
-  std::cout << "Width of rectangle: " <<frame.width << "\nHeight of rectangle: " << frame.height <<'\n';
+  Shape* circle = &cir;
+  tomove = {10,20};
+  circle->move(tomove);
+  circle->move(1,2);
+  std::cout << circle->getArea() << '\n';
+  frame = circle->getFrameRect();
+  std::cout << "Width of rectangle: " << frame.width << '\n';
+  std::cout << "Height of rectangle: " << frame.height <<'\n';
 
   return 0;
 }
