01fdb15af5f96e40468f1cc8a6f6f274f3127587
MaLebDen <lebedev.dm00@mail.ru>
2019-04-05 20:03:46 +0300

    Сделал все в соответствии с правилом оформления

--- lebedev.denis/A1/base-types.hpp
+++ lebedev.denis/A1/base-types.hpp
@@ -1,13 +1,14 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
-struct point_t {
+
+struct point_t 
+{
   double x;
   double y;
 };
 
-
-
-struct rectangle_t {
+struct rectangle_t 
+{
   double height;
   double width;
   point_t pos;
