0597ad6530e362c232d95caf4c7502c000586ae7
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-05 16:48:36 +0300

    Объединил указатели на фигуры в одну общую ptrShape

--- drozdov.kirill/A1/main.cpp
+++ drozdov.kirill/A1/main.cpp
@@ -8,32 +8,32 @@ int main()
 {
   /*test on example Rectangle:*/
   Rectangle rect1(7.2, 5.7, {5, -2.4});
-  Shape * ptrRect = & rect1;
+  Shape * ptrShape = & rect1;
 
   std::cout << "Rectangle: ";
-  ptrRect->displayData();
-  std::cout << "\nArea: " << ptrRect->getArea() << std::endl;
+  ptrShape->displayData();
+  std::cout << "\nArea: " << ptrShape->getArea() << std::endl;
 
-  rectangle_t frameRect = ptrRect->getFrameRect();
+  rectangle_t frameRect = ptrShape->getFrameRect();
   showFrame(frameRect);
-  ptrRect->move(2.5, -4.1);
+  ptrShape->move(2.5, -4.1);
   std::cout << "\n==============================After move -> +2.5, -4.1==============================";
-  ptrRect->displayData();
+  ptrShape->displayData();
 
   /*test on example Circle:*/
-  Circle cirptrCirc(5);
-  Shape *ptrCirc = &cirptrCirc;
+  Circle cirptrShape(5);
+  ptrShape = &cirptrShape;
 
   std::cout << std::endl << "\nCircle: ";
-  ptrCirc->displayData();
-  std::cout << "\nArea: " << ptrCirc->getArea() << std::endl;
+  ptrShape->displayData();
+  std::cout << "\nArea: " << ptrShape->getArea() << std::endl;
 
-  frameRect = ptrCirc->getFrameRect();
+  frameRect = ptrShape->getFrameRect();
   showFrame(frameRect);
   point_t position = {-1.8, 2.4};
-  ptrCirc->move(position);
+  ptrShape->move(position);
   std::cout << "\n==============================After move to (-1.8, 2.4)==============================";
-  ptrCirc->displayData();
+  ptrShape->displayData();
 
   std::cout << std::endl;
   return 0;
