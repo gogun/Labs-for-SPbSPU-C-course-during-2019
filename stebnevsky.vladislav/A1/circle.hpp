e675f7e34d47045c8e2f0b984c5731e38824814b
Stebnevsky Vladislav <s.vladislav@list.ru>
2019-04-05 17:48:29 +0300

    deleted unnecessary spaces in header files

--- stebnevsky.vladislav/A1/circle.hpp
+++ stebnevsky.vladislav/A1/circle.hpp
@@ -5,15 +5,15 @@
 
 class Circle : public Shape
 {
-  public:
-    Circle(const point_t & center, double radius);
-    virtual double getArea() const override;
-    virtual rectangle_t getFrameRect() const override;
-    virtual void move(const point_t & point) override;
-    virtual void move(double dx, double dy) override;
-  private:
-    point_t m_center;
-    double m_radius;
+public:
+  Circle(const point_t & center, double radius);
+  virtual double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  virtual void move(const point_t & point) override;
+  virtual void move(double dx, double dy) override;
+private:
+  point_t m_center;
+  double m_radius;
 };
 
 #endif // CIRCLE_HPP
--- stebnevsky.vladislav/A1/rectangle.hpp
+++ stebnevsky.vladislav/A1/rectangle.hpp
@@ -5,14 +5,14 @@
 
 class Rectangle : public Shape
 {
-  public:
-    Rectangle(const rectangle_t & rect_init);
-    virtual double getArea() const override;
-    virtual rectangle_t getFrameRect() const override;
-    virtual void move(const point_t & point) override;
-    virtual void move(double dx, double dy) override;
-  private:
-    rectangle_t m_rect;
+public:
+  Rectangle(const rectangle_t & rect_init);
+  virtual double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  virtual void move(const point_t & point) override;
+  virtual void move(double dx, double dy) override;
+private:
+  rectangle_t m_rect;
 };
 
 #endif // RECTANGLE_HPP
