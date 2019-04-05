be03908ef00801fa2772ca110796eaba8d3109d5
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-05 18:12:41 +0300

    Убран указатель shape в функции main
    От него можно избавиться, при помощи передачи объекта по значению в 
    функции (например viewShape)

--- drozdov.kirill/A1/main.cpp
+++ drozdov.kirill/A1/main.cpp
@@ -8,28 +8,24 @@ int main()
 {
   /*test on example Rectangle:*/
   Rectangle rect1(7.2, 5.7, {5, -2.4});
-  Shape * ptrShape = & rect1;
-
   std::cout << "Rectangle: ";
-  viewShape(ptrShape);
+  viewShape(&rect1);
 
   double dx = 2.5, dy = -4.1;
-  ptrShape->move(dx, dy);
+  rect1.move(dx, dy);
   std::cout << "\n============After move: + (" << dx << ';' << dy << ")============";
-  ptrShape->displayData();
+  rect1.displayData();
 
   std::cout << std::endl;
   /*test on example Circle:*/
-  Circle cirptrShape(5);
-  ptrShape = &cirptrShape;
-
+  Circle circ1(5);
   std::cout << "\nCircle: ";
-  viewShape(ptrShape);
+  viewShape(&circ1);
 
   point_t position = {-1.8, 2.4};
-  ptrShape->move(position);
+  circ1.move(position);
   std::cout << "\n============After move to (-1.8, 2.4)============";
-  ptrShape->displayData();
+  circ1.displayData();
 
   std::cout << std::endl;
   return 0;
