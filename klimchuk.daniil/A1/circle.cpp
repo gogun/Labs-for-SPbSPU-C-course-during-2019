e693c4ddb33cdc638c4d5a7f409f8ff5c79d0633
Daniil Klimchuk <dan.klimchuk@yandex.ru>
2019-04-06 03:02:38 +0300

    Added my folder and lab A1

---
+++ klimchuk.daniil/A1/circle.cpp
@@ -0,0 +1,43 @@
+#include "circle.hpp"
+#include <iostream>
+#include <cmath>
+
+Circle::Circle(const double radius, const point_t &center) :
+        radius_(radius),
+        pos_(center)
+{
+    if (radius <= 0) {
+        std::cerr << "Invalid circle radius. Radius must be above zero.\n";
+    }
+}
+
+double Circle::getArea() const
+{
+    return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+    return {2.0 * radius_, 2.0 * radius_, pos_};
+}
+
+void Circle::move(const point_t &point)
+{
+    pos_ = point;
+}
+
+void Circle::move(const double dx, const double dy)
+{
+    pos_.x += dx;
+    pos_.y += dy;
+}
+
+void Circle::printInfo() const
+{
+    std::cout << "Circle:"
+              << "\n  Radius: " << radius_
+              << "\n  Position:"
+              << "\n    x: " << pos_.x
+              << "\n    y: " << pos_.y
+              << "\n  Area: " << this->getArea() << "\n\n";
+}
---
+++ klimchuk.daniil/A1/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "circle.hpp"
+#include "rectangle.hpp"
+
+int main(int, char *[])
+{
+    Rectangle rectangle = {5.0, 3.5, {-1.0, -2.0}};
+    Circle circle = {2.5, {3.14, 2.71}};
+
+    std::cout << "That's what we have:\n";
+    rectangle.printInfo();
+    circle.printInfo();
+
+    std::cout << "Let's move the shapes 2 units left and 7.77 units up.\n";
+    rectangle.move(-2.0, 7.77);
+    rectangle.printInfo();
+    circle.move(-2.0, 7.77);
+    circle.printInfo();
+
+    std::cout << "Now we will move the shapes to a specific point (3, 4).\n";
+    rectangle.move({3.0, 4.0});
+    rectangle.printInfo();
+    circle.move({3.0, 4.0});
+    circle.printInfo();
+
+
+    rectangle_t rectangleFrame = rectangle.getFrameRect();
+    std::cout << "Frame rectangle for the rectangle:"
+              << "\n  Width: " << rectangleFrame.width
+              << "\n  Height: " << rectangleFrame.height
+              << "\n  Position:"
+              << "\n    x: " << rectangleFrame.pos.x
+              << "\n    y: " << rectangleFrame.pos.y << "\n\n";
+
+    rectangle_t circleFrame = circle.getFrameRect();
+    std::cout << "Frame rectangle for the circle:"
+              << "\n  Width: " << circleFrame.width
+              << "\n  Height: " << circleFrame.height
+              << "\n  Position:"
+              << "\n    x: " << circleFrame.pos.x
+              << "\n    y: " << circleFrame.pos.y << std::endl;
+
+    return 0;
+}
