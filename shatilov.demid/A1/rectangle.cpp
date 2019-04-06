fe669329695dc0f7d2296591095ea478ae95bddd
Demid <shatilov.demid@gmail.com>
2019-04-07 01:13:53 +0300

    Перезапись файлов Для окончания строк Windows

---
+++ shatilov.demid/A1/rectangle.cpp
@@ -0,0 +1,32 @@
+#include "rectangle.hpp"
+#include <iostream>
+#include <cassert>
+
+Rectangle::Rectangle(const double width, const double height, const point_t & pos) :
+        rect_ (rectangle_t {width, height, pos})
+{
+  assert ((height > 0.0) && (width > 0.0));
+}
+
+
+double Rectangle::getArea() const
+{
+  return (rect_.width * rect_.height);
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rect_;
+}
+
+void Rectangle::move(const point_t & pos)
+{
+  rect_.pos.x += pos.x;
+  rect_.pos.y += pos.y;
+}
+
+void Rectangle::move(double x, double y)
+{
+  rect_.pos.x += x;
+  rect_.pos.y += y;
+}
---
+++ shatilov.demid/A1/rectangle.hpp
@@ -0,0 +1,19 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(const double width, const double height, const point_t & pos);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t & pos) override;
+  void move(double x, double y) override;
+
+private:
+  rectangle_t rect_;
+};
+
+#endif
