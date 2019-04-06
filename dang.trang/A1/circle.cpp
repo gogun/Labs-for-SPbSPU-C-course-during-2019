3c780ae3e272b20354d317261eab37eb8f4e4a39
Trang <trangdang6898@gmail.com>
2019-04-06 17:21:02 +0300

    Upload A1

--- dang.trang/A1/circle.cpp
+++ dang.trang/A1/circle.cpp
@@ -11,28 +11,33 @@ Circle::Circle (double Rcircle, point_t cpoint):
   if (cR <= 0)
   throw std::invalid_argument("radius of circle <= 0");
 }
+
 double Circle::getArea() const
 {
   return M_PI*cR*cR;
 }
+
 rectangle_t Circle::getFrameRect() const
 {
   return {cR,cR,pos.x,pos.y};
 }
+
 void Circle::move(const double mX,const double mY)
 {
   pos.x += mX;
   pos.y += mY;
 }
+
 void Circle::move(const point_t point)
 {
   pos.x = point.x;
   pos.y = point.y;
 }
+
 void Circle::dataoutput() const
 {
-  std::cout<<"area_of_circle_="<<getArea()<<std::endl;
-  std::cout<<"framerect_width_height_X_Y_="<<getFrameRect().width<<" "<<getFrameRect().height<<" ";
-  std::cout<<getFrameRect().pos.x<<" "<<getFrameRect().pos.y<<std::endl;
-  std::cout<<"position_X_Y_"<<pos.x<<" "<<pos.y<<std::endl;
+  std::cout << "area_of_circle_=" << getArea() << std::endl;
+  std::cout << "framerect_width_height_X_Y_=" << getFrameRect().width << " " << getFrameRect().height << " ";
+  std::cout << getFrameRect().pos.x << " " << getFrameRect().pos.y << std::endl;
+  std::cout << "position_X_Y_" << pos.x << " " << pos.y << std::endl;
 }
