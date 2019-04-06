eecba809ec28eef444a062664baa04e953b65d7d
folsea <88ched@gmail.com>
2019-04-06 23:59:39 +0300

    A1

--- che.eduard/A1/circle.cpp
+++ che.eduard/A1/circle.cpp
@@ -11,16 +11,6 @@ Circle::Circle(const point_t & center, double radius):
   assert(radius_ >= 0.0);
 }
 
-rectangle_t Circle::getFrameRect() const
-{
-  return { radius_ * 2, radius_ * 2, center_ };
-}
-
-double Circle::getArea() const
-{
-  return M_PI * radius_ * radius_;
-}
-
 void Circle::move(double dx, double dy)
 {
   center_.x += dx;
@@ -30,5 +20,15 @@ void Circle::move(double dx, double dy)
 void Circle::move(const point_t & point)
 {
   center_ = point;
+}
+
+double Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  return { radius_ * 2, radius_ * 2, center_ };
 }
 
