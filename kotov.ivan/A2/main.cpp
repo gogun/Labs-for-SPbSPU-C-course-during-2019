143c9bf80208fe0cd7de938c91428ac5b02df41a
иван котов <i.kotov2000@gmail.com>
2019-04-07 21:13:32 +0300

    change tests and delete new in main.cpp

--- kotov.ivan/A2/main.cpp
+++ kotov.ivan/A2/main.cpp
@@ -6,8 +6,8 @@ int main()
 {
   kotov::Rectangle rect({55, 55}, 10, 10);
   kotov::Circle circle({25, 25}, 5);
-  kotov::Shape ** shapes = new kotov::Shape*[2];
-  shapes[0] = & rect; 
+  kotov::Shape* shapes[2];
+  shapes[0] = & rect;
   shapes[1] = & circle;
   std::cout << "Area of " << shapes[0]->getShapeName() << ": " << shapes[0]->getArea() << std::endl;
   std::cout << "Area of " << shapes[1]->getShapeName() << ": " << shapes[1]->getArea() << std::endl;
@@ -32,6 +32,5 @@ int main()
       << ", " << shapes[0]->getFrameRect().pos.y << std::endl;
   std::cout << "position of " << shapes[1]->getShapeName() << " after scaling: " << shapes[1]->getFrameRect().pos.x
       << ", " << shapes[1]->getFrameRect().pos.y << std::endl;
-  delete[] shapes;
   return 0;
 }
--- kotov.ivan/A2/test-main.cpp
+++ kotov.ivan/A2/test-main.cpp
@@ -10,41 +10,36 @@ BOOST_AUTO_TEST_SUITE(testForRectangle)
 
 BOOST_AUTO_TEST_CASE(scaleTest)
 {
-  for (int i = 1; i < 100; i++) {
-    kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
-    double areaRect = rectangle.getArea() * i * i / 625;
-    rectangle.scale(((double) i) / 25);
-    BOOST_CHECK_CLOSE(areaRect, rectangle.getArea(), 1000 * std::numeric_limits<double>::epsilon());
-  }
+  kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
+  double areaRect = rectangle.getArea() * 6.7 * 6.7;
+  rectangle.scale(6.7);
+  BOOST_CHECK_CLOSE(areaRect, rectangle.getArea(), std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(displacementMovingTest) 
 {
-  for (int i = 0; i < 100; i++) {
-    kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
-    kotov::rectangle_t frameRect = rectangle.getFrameRect();
-    rectangle.move(std::pow(-1, i) * i / 25, std::pow(-1, i + 1) * i / 25);
-    BOOST_CHECK_CLOSE(frameRect.height, rectangle.getFrameRect().height, std::numeric_limits<double>::epsilon());
-    BOOST_CHECK_CLOSE(frameRect.width, rectangle.getFrameRect().width, std::numeric_limits<double>::epsilon());
-  }
+  kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
+  kotov::rectangle_t frameRect = rectangle.getFrameRect();
+  rectangle.move(25, -12);
+  BOOST_CHECK_CLOSE(frameRect.height, rectangle.getFrameRect().height, std::numeric_limits<double>::epsilon());
+  BOOST_CHECK_CLOSE(frameRect.width, rectangle.getFrameRect().width, std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(movingInPositionTest)
 {
-  for (int i = 0; i < 100; i++) {
-    kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
-    kotov::rectangle_t frameRect = rectangle.getFrameRect();
-    rectangle.move({std::pow(-1, i) * i / 2, std::pow(-1, i + 1) * i / 2});
-    BOOST_CHECK_CLOSE(frameRect.height, rectangle.getFrameRect().height, std::numeric_limits<double>::epsilon());
-    BOOST_CHECK_CLOSE(frameRect.width, rectangle.getFrameRect().width, std::numeric_limits<double>::epsilon());
-  }
+  kotov::Rectangle rectangle({0.0, 0.0}, 10.0, 10.0);
+  kotov::rectangle_t frameRect = rectangle.getFrameRect();
+  rectangle.move({65, -7});
+  BOOST_CHECK_CLOSE(frameRect.height, rectangle.getFrameRect().height, std::numeric_limits<double>::epsilon());
+  BOOST_CHECK_CLOSE(frameRect.width, rectangle.getFrameRect().width, std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(invalidArgumentsTest)
 {
-  for (int i = 0; i < 10; i++) {
-    BOOST_CHECK_THROW(kotov::Rectangle({0, 0}, std::pow(-1, i) * i, std::pow(-1, i + 1) * i), std::invalid_argument);
-  }
+  BOOST_CHECK_THROW(kotov::Rectangle({0, 0}, 0, 1), std::invalid_argument);
+  BOOST_CHECK_THROW(kotov::Rectangle({0, 0}, 1, 0), std::invalid_argument);
+  BOOST_CHECK_THROW(kotov::Rectangle({0, 0}, 1, -1), std::invalid_argument);
+  BOOST_CHECK_THROW(kotov::Rectangle({0, 0}, -1, 1), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -53,41 +48,34 @@ BOOST_AUTO_TEST_SUITE(testForCircle)
 
 BOOST_AUTO_TEST_CASE(scaleTest)
 {
-  for (int i = 1; i < 100; i++) {
-    kotov::Circle circle({0.0, 0.0}, 10.0);
-    double areaCircle = circle.getArea() * i * i / 625;
-    circle.scale(((double) i) / 25);
-    BOOST_CHECK_CLOSE(areaCircle, circle.getArea(), 1000 * std::numeric_limits<double>::epsilon());
-  }
+  kotov::Circle circle({0.0, 0.0}, 10.0);
+  double areaCircle = circle.getArea() * 8.9 * 8.9;
+  circle.scale(8.9);
+  BOOST_CHECK_CLOSE(areaCircle, circle.getArea(), std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(displacementMovingTest)
 {
-  for (int i = 0; i < 100; i++) {
-    kotov::Circle circle({0.0, 0.0}, 10.0);
-    kotov::rectangle_t frameCircle = circle.getFrameRect();
-    circle.move(std::pow(-1, i) * i / 25, std::pow(-1, i + 1) * i / 25);
-    BOOST_CHECK_CLOSE(frameCircle.height, circle.getFrameRect().height, std::numeric_limits<double>::epsilon());
-    BOOST_CHECK_CLOSE(frameCircle.width, circle.getFrameRect().width, std::numeric_limits<double>::epsilon());
-  }
+  kotov::Circle circle({0.0, 0.0}, 10.0);
+  kotov::rectangle_t frameCircle = circle.getFrameRect();
+  circle.move(63, -89);
+  BOOST_CHECK_CLOSE(frameCircle.height, circle.getFrameRect().height, std::numeric_limits<double>::epsilon());
+  BOOST_CHECK_CLOSE(frameCircle.width, circle.getFrameRect().width, std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(movingInPositionTest)
 {
-  for (int i = 0; i < 100; i++) {
-    kotov::Circle circle({0.0, 0.0}, 10.0);
-    kotov::rectangle_t frameCircle = circle.getFrameRect();
-    circle.move({std::pow(-1, i) * i / 2, std::pow(-1, i + 1) * i / 2});
-    BOOST_CHECK_CLOSE(frameCircle.height, circle.getFrameRect().height, std::numeric_limits<double>::epsilon());
-    BOOST_CHECK_CLOSE(frameCircle.width, circle.getFrameRect().width, std::numeric_limits<double>::epsilon());
-  }
+  kotov::Circle circle({0.0, 0.0}, 10.0);
+  kotov::rectangle_t frameCircle = circle.getFrameRect();
+  circle.move({-45, 45});
+  BOOST_CHECK_CLOSE(frameCircle.height, circle.getFrameRect().height, std::numeric_limits<double>::epsilon());
+  BOOST_CHECK_CLOSE(frameCircle.width, circle.getFrameRect().width, std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(invalidArgumentsTest)
 {
-  for (int i = 0; i < 5; i++) {
-    BOOST_CHECK_THROW(kotov::Circle({0, 0}, -i / 2), std::invalid_argument);
-  }
+    BOOST_CHECK_THROW(kotov::Circle({0, 0}, 0), std::invalid_argument);
+    BOOST_CHECK_THROW(kotov::Circle({0, 0}, -1), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
