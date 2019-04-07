733de0fe2dc34db28f511ac6781bdcf0941957fb
Artem Kvashnin <123456kvin@mail.ru>
2019-04-07 16:28:02 +0000

    Добавлена более явная демонстрация полиморфизма.

--- kvashnin.artem/A1/main.cpp
+++ kvashnin.artem/A1/main.cpp
@@ -8,39 +8,40 @@ int main()
   Circle cir = Circle({0, 0}, 1);
 
   std::cout << "Using polymorphism and learn data about the object." << std::endl;
-  Shape *shp = &cir;
-  shp->printInfo();
+  Shape *figure = &cir;
+  figure->printInfo();
   std::cout << std::endl;
 
   std::cout << "Move the object and check the data about the object." << std::endl;
-  shp->move(15, 10);
+  figure->move(15, 10);
   cir.printInfo();
   std::cout << std::endl;
 
   std::cout << "Set new coordinates for the object." << std::endl;
-  shp->move({20, 15});
+  figure->move({20, 15});
   cir.printInfo();
   std::cout << std::endl;
 
   std::cout << "Create a rectangle with dimensions {2, 2} in coordinates {10, 20}." << std::endl;
   Rectangle rec = Rectangle({2, 2}, 10, 20);
 
+  figure = &rec;
   std::cout << "Learn data about the object." << std::endl;
-  rec.printInfo();
+  figure->printInfo();
   std::cout << std::endl;
 
   std::cout << "Move the object." << std::endl;
-  rec.move({10, 10});
+  figure->move({10, 10});
 
   std::cout << "Check the new coordinates." << std::endl;
-  rec.printInfo();
+  figure->printInfo();
   std::cout << std::endl;
 
   std::cout << "Move the object." << std::endl;
-  rec.move(2, 4);
+  figure->move(2, 4);
 
   std::cout << "Check the new coordinates." << std::endl;
-  rec.printInfo();
+  figure->printInfo();
   std::cout << std::endl;
 
   return 0;
