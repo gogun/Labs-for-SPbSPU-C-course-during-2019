d37dedda0a881ea2e03f74c194b4c4d6ed5a4ded
npinaev <nikitapinaev1@gmail.com>
2019-04-07 22:35:18 +0300

    add A1

---
+++ pinaev.nikita/A1/circle.cpp
@@ -0,0 +1,35 @@
+#define _USE_MATH_DEFINES
+#include "circle.hpp"
+#include "base-types.hpp"
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+Circle::Circle(const point_t &centr, const double radius) :
+  radius_(radius),
+  centr_(centr)
+{
+  assert(radius >= 0);
+}
+double Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
+rectangle_t Circle::getFrameRect() const
+{
+  return { radius_ * 2, radius_ * 2, centr_ };
+}
+void Circle::move(const double dx, const double dy)
+{
+  centr_.x += dx;
+  centr_.y += dy;
+}
+void Circle::move(const point_t &newPoint)
+{
+  centr_ = newPoint;
+}
+void Circle::showResults() const
+{
+  std::cout << "Radius is " << radius_ << std::endl
+    << "Center at: (" << getFrameRect().pos.x << ";" << getFrameRect().pos.y << ")" << std::endl;
+}
---
+++ pinaev.nikita/A1/circle.hpp
@@ -0,0 +1,20 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+class Circle : public Shape
+{
+public:
+  Circle(const point_t &, const double);
+  rectangle_t getFrameRect() const override;
+  double getArea() const override;
+  void move(const double, const double) override;
+  void move(const point_t &) override;
+  void showResults() const override;
+private:
+  double radius_;
+  point_t centr_;
+};
+
+#endif //CIRCLE_HPP
