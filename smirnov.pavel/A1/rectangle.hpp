6c07e15951ecff6f64c2a80e83f24d71953fb01f
smirnov.pavel.1 <smpashts@gmail.com>
2019-04-07 21:19:48 +0300

    Еще пара замен

---
+++ smirnov.pavel/A1/rectangle.hpp
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(double width, double
+      height, const point_t & center);
+  double getArea() const;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t & new_point) override;
+  void move(double x_delta, double y_delta) override;
+  void getInfo() const override;
+
+private:
+  double width_;
+  double height_;
+  point_t center_;
+};
+
+
+#endif
