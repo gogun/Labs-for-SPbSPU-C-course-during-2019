5342f6c76cd520b6a3e1a74a8475f32208dcf24f
dimnaz <replikant217@gmail.com>
2019-04-07 23:59:47 +0300

    Исправлены мелкие недочеты

--- nazarov.dmitry/A1/base-types.hpp
+++ nazarov.dmitry/A1/base-types.hpp
@@ -1,15 +1,15 @@
 #ifndef BASE_TYPES_HPP_INCLUDED
 #define BASE_TYPES_HPP_INCLUDED
 
-  struct point_t
-  {
-    double x, y;
-  };
+struct point_t
+{
+  double x, y;
+};
 
-  struct rectangle_t
-  {
-    point_t pos;
-    double width, height;
-  };
+struct rectangle_t
+{
+  point_t pos;
+  double width, height;
+};
 
 #endif
