45b45f7d4efa9305de47ea714db71f3c7ff0c3e3
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-07 19:25:12 +0000

    Update main.cpp

--- goncharova.alexandra/A1/main.cpp
+++ goncharova.alexandra/A1/main.cpp
@@ -7,23 +7,23 @@ int main()
   point_t dot = {3, 4};
   Circle circle1(dot, 15);
   Rectangle rectangle1(dot, 10, 10);
-  
+
   std::cout << "Area of circle1: ";
   srd::cout << circle1.getArea() << "\n";
   std::cout << "Height of frame rectangle: ";
   std::cout << circle1.getFrameRect().height << "\n";
   std::cout << "Width of frame rectangle: ";
   std::cout << circle1.getFrameRect().width << "\n";
-  
+
   point_t movePoint = { 5,6 };
   circle1.move(movePoint);
   std::cout << "Circle moved to: x:" << circle1.center_.x;
   std::cout << ", y:" << circle1.center_.y << "\n";
-  
+
   circle1.move(7.0, 8.0);
   std::cout << "Circle moved to: x:" << circle1.center_.x;
   std::cout << ", y:" << circle1.center_.y << "\n";
-  
+
   std::cout << "---" << "\n";
 
   std::cout << "Area of rectangle1: ";
@@ -32,11 +32,11 @@ int main()
   std::cout << rectangle1.getFrameRect().height << "\n";
   std::cout << "Width of frame rectangle: ";
   std::cout << rectangle1.getFrameRect().width << "\n";
-  
+
   rectangle1.move(movePoint);
   std::cout << "Rectangle moved to: x:" << rectangle1.center_.x;
   std::cout << ", y:" << rectangle1.center_.y << "\n";
-  
+
   rectangle1.move(7.0, 8.0);
   std::cout << "Rectangle moved to: x:" << rectangle1.center_.x;
   std::cout << ", y:" << rectangle1.center_.y << "\n";
