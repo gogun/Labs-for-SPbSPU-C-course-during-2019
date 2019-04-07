00005bf14ea4be2e3cc5f895053e46a5b46f52ef
Somn117 <somn117@yandex.ru>
2019-04-07 21:38:19 +0300

    deleted empty line

--- vlasenko.nataliia/A1/base-types.hpp
+++ vlasenko.nataliia/A1/base-types.hpp
@@ -1,17 +1,14 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-struct point_t
-{
+struct point_t {
   double x;
   double y;
 };
 
-struct rectangle_t
-{
+struct rectangle_t {
   double width;
   double height;
   point_t pos;
 };
-
 #endif
