ee933a4b0c6e994ed1a60381dbc77dfefa5e4f7f
Anastasia Moiseeva <whatareyou95@mail.ru>
2019-04-07 15:02:10 +0300

    Adding new files

---
+++ moiseeva.anastasia/A1/rectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef A1_RECTANGLE_HPP
+#define A1_RECTANGLE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(point_t dot, double width, double height);
+
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const double dx, const double dy) override;
+  void move(const point_t point) override;
+
+private:
+  point_t center_;
+  double width_;
+  double height_;
+};
+#endif //A1_RECTANGLE_HPP
