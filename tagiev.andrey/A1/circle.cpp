87b7f287fc46fda61412351fd4830106af74b235
atagiev <andrej-tagiev@yandex.ru>
2019-04-07 19:59:51 +0300

    Поправка для единообразия кода

--- tagiev.andrey/A1/circle.cpp
+++ tagiev.andrey/A1/circle.cpp
@@ -18,11 +18,13 @@ double Circle::getArea() const
 {
   return M_PI * radius_ * radius_;
 }
+
 void Circle::move(double dx, double dy)
 {
   center_.x += dx;
   center_.y += dy;
 }
+
 void Circle::move(const point_t &newPoint)
 {
   center_ = newPoint;
--- tagiev.andrey/A1/rectangle.cpp
+++ tagiev.andrey/A1/rectangle.cpp
@@ -8,19 +8,23 @@ Rectangle::Rectangle(const point_t &center, double width, double height) :
 {
   assert((height > 0) && (width > 0));
 }
+
 double Rectangle::getArea() const
 {
   return width_ * height_;
 }
+
 rectangle_t Rectangle::getFrameRect() const
 {
   return {width_, height_, center_};
 }
+
 void Rectangle::move(double dx, double dy)
 {
   center_.x += dx;
   center_.y += dy;
 }
+
 void Rectangle::move(const point_t &newPoint)
 {
   center_ = newPoint;
