10c7855f5fd02808b7092ec8003cdb63a99ec3ae
sazonov dmitriy maksimovich <omckbird@mail.ru>
2019-04-07 18:12:15 +0000

    A1

---
+++ sazonov.dmitriy/A1/rectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(double, double, const point_t &);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t &newPos) override;
+  void move(double ax, double ay) override;
+  point_t getPos() const override;
+
+private:
+  double height_;
+  double width_;
+  point_t center_;
+};
+
+#endif
