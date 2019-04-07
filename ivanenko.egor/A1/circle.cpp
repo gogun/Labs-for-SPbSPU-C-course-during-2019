690e18b1ce91fc5228d88529ebda6671860d2573
ivanenko.egor <ivanenko_egor@mail.ru>
2019-04-08 00:51:17 +0300

    A1

---
+++ ivanenko.egor/A1/circle.cpp
@@ -0,0 +1,31 @@
+#include "circle.hpp"
+#include <cmath>
+#include <cassert>
+
+Circle::Circle(const point_t &position, double radius) :
+	pos_(position),
+	radius_(radius)
+{
+	assert(radius_ > 0.0);
+}
+
+double Circle::getArea() const
+{
+	return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+	return {2 * radius_, 2 * radius_, pos_};
+}
+
+void Circle::move(const point_t &pos)
+{
+	pos_ = pos;
+}
+
+void Circle::move(double dx, double dy)
+{
+	pos_.x += dx;
+	pos_.y += dy;
+}
---
+++ ivanenko.egor/A1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+void printArea(const Shape &shape)
+{
+	std::cout << "area: " << shape.getArea() << std::endl;
+};
+
+void printFrameRectangle(const Shape &shape)
+{
+	rectangle_t frameRectangle = shape.getFrameRect();
+	std::cout << "Width: " << frameRectangle.width << std::endl;
+	std::cout << "Height: " << frameRectangle.height << std::endl;
+	std::cout << "Center: " << "(" << frameRectangle.pos.x << ", " << frameRectangle.pos.y << ")";
+	std::cout << std::endl;
+}
+
+int main()
+{
+	point_t pointCenter{10,10};
+	Rectangle rectangle{5, 5, pointCenter};
+	Circle circle(pointCenter, 2);
+
+	printArea(rectangle);
+	printFrameRectangle(rectangle);
+	rectangle.move(-2,-2);
+	printFrameRectangle(rectangle);
+	std::cout << std::endl;
+
+	printArea(circle);
+	printFrameRectangle(circle);
+	circle.move(3,3);
+	printFrameRectangle(rectangle);
+	std::cout << std::endl;
+
+	return 0;
+}
