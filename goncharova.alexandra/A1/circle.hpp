1031eb48a87f3b81db4327432001c665f750a231
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-07 18:08:54 +0000

    correction circle.hpp

--- goncharova.alexandra/A1/circle.hpp
+++ goncharova.alexandra/A1/circle.hpp
@@ -1,34 +1,18 @@
 #ifndef circle_hpp
-
 #define circle_hpp
-
-
 
 #include "shape.hpp"
 
-
-
 class Circle: public Shape
-
 {
-
 public:
-
   point_t center_;
   double radius_;
-
   Circle(const point_t &center, double radius);
-
   double getArea() const;
-
   rectangle_t getFrameRect() const;
-
   void move(const point_t &newPoint);
-
   void move(double dx, double dy);
-
 };
-
-
 
 #endif
