9af3f3c9581c23d453e36bfb8925b00d1fd384a0
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-07 18:13:25 +0000

    correction shape.hpp

--- goncharova.alexandra/A1/shape.hpp
+++ goncharova.alexandra/A1/shape.hpp
@@ -1,28 +1,16 @@
 #ifndef shape_hpp
-
 #define shape_hpp
-
 
 #include "base-types.hpp"
 
-
 class Shape
-
 {
-
 public:
-
   virtual ~Shape() = default;
-
   virtual double getArea() const = 0;
-
   virtual rectangle_t getFrameRect() const = 0;
-
   virtual void move(const point_t &newPoint) = 0;
-
   virtual void move(double dx, double dy) = 0;
-
 };
-
 
 #endif
