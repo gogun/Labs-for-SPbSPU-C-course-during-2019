8edc2a531649b3d4fbf06cde6a0cbb9ee9839563
philipp.trofimov <philipp.trofimov2000@mail.ru>
2019-04-08 02:56:38 +0300

    fix circle/rectangle/shape.hpp

--- trofimov.philipp/common/circle.hpp
+++ trofimov.philipp/common/circle.hpp
@@ -4,21 +4,21 @@
 
 namespace trofimov
 {
-  class Circle : public Shape
-  {
-  public:
-    Circle(const point_t &pos, double r);
+ class Circle : public Shape
+ {
+ public:
+   Circle(const point_t &pos, double r);
 
-	double getArea() const override;
-	rectangle_t getFrameRect() const override;
-	void move(const point_t &point) override;
-	void move(double dx, double dy) override;
-	void getInfo() const override;
-	void scale(double cScaling) override;
+   double getArea() const override;
+   rectangle_t getFrameRect() const override;
+   void move(const point_t &point) override;
+   void move(double dx, double dy) override;
+   void getInfo() const override;
+   void scale(double cScaling) override;
 
-  private:
-    point_t center_;
-	double radius_;
-  };
+ private:
+   point_t center_;
+   double radius_;
+ };
 }
 #endif
--- trofimov.philipp/common/rectangle.hpp
+++ trofimov.philipp/common/rectangle.hpp
@@ -4,19 +4,19 @@
 
 namespace trofimov
 {
-  class Rectangle : public Shape
-  {
-  public:
-    Rectangle(const point_t &pos, double w, double h);
+ class Rectangle : public Shape
+ {
+ public:
+   Rectangle(const point_t &pos, double w, double h);
 
-	double getArea() const override;
-	rectangle_t getFrameRect() const override;
-	void move(const point_t &point) override;
-	void move(double dx, double dy) override;
-	void getInfo() const override;
-	void scale(double cScaling) override;
-  private:
-	rectangle_t rectangle_;
-  };
+   double getArea() const override;
+   rectangle_t getFrameRect() const override;
+   void move(const point_t &point) override;
+   void move(double dx, double dy) override;
+   void getInfo() const override;
+   void scale(double cScaling) override;
+ private:
+   rectangle_t rectangle_;
+ };
 }
 #endif
