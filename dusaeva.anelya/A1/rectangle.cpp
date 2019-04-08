346412a9340fd197c17e3f1b2205484dc284176e
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-08 15:08:06 +0300

    изменен порядок включения заголовочных файлов

--- dusaeva.anelya/A1/rectangle.cpp
+++ dusaeva.anelya/A1/rectangle.cpp
@@ -1,7 +1,7 @@
+#include "rectangle.hpp"
+#include "base-types.hpp"
 #include <iostream>
 #include <cassert>
-#include "base-types.hpp"
-#include "rectangle.hpp"
 
 Rectangle::Rectangle(const point_t &centre, double width, double height) :
   position_(centre),
@@ -9,6 +9,16 @@ Rectangle::Rectangle(const point_t &centre, double width, double height) :
   height_(height)
 {
   assert(width >= 0 || height >= 0);
+}
+
+double Rectangle::getArea() const
+{
+  return height_ * width_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rectangle_t {width_, height_,{position_.x, position_.y}};
 }
 
 void Rectangle::move(double dx, double dy)
@@ -20,16 +30,6 @@ void Rectangle::move(double dx, double dy)
 void Rectangle::move(const point_t &param)
 {
   position_ = param;
-}
-
-double Rectangle::getArea() const
-{
-  return height_ * width_;
-}
-
-rectangle_t Rectangle::getFrameRect() const
-{
-  return rectangle_t {width_, height_,{position_.x, position_.y}};
 }
 
 void Rectangle::printInfo() const
