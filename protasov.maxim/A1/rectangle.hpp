03d65fdf4838446ef3173de3df6a8b9b2f196ad0
Maxim Protasov <protasovmax@gmail.com>
2019-04-08 17:06:45 +0000

    Fixed code following coding guidelines

--- protasov.maxim/A1/rectangle.hpp
+++ protasov.maxim/A1/rectangle.hpp
@@ -7,16 +7,16 @@
 class Rectangle : public Shape
 {
 public:
-    Rectangle(const point_t &, const double &, const double &);
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(const point_t &) override;
-    void move(const double &, const double &) override;
+  Rectangle(const point_t &, const double &, const double &);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t &) override;
+  void move(const double &, const double &) override;
 
 private:
-    point_t center_;
-    double width_;
-    double height_;
+  point_t center_;
+  double width_;
+  double height_;
 };
 
 #endif
