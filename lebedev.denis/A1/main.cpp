2e1231e89b9e11aaba7ad47e3b9536a89b6b3b57
MaLebDen <lebedev.dm00@mail.ru>
2019-04-08 21:20:20 +0300

    Сделал другую демонстрацию полиморфизма

--- lebedev.denis/A1/main.cpp
+++ lebedev.denis/A1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "circle.hpp"
 #include "rectangle.hpp"
+#include <cassert>
 
 void showRectangle_t(rectangle_t data)
 {
@@ -10,23 +11,24 @@ void showRectangle_t(rectangle_t data)
   std::cout<<"Y = "<<data.pos.y<<")"<<"\n";
 }
 
-void showAll(const Shape & shape)
+void showAll(const Shape * shape)
 {
-  std::cout<<"Area = "<<shape.getArea()<<"\n";
-  showRectangle_t(shape.getFrameRect());
+  assert(shape != nullptr);
+  std::cout<<"Area = "<<shape->getArea()<<"\n";
+  showRectangle_t(shape->getFrameRect());
 }
 
 int main()
 {
   Rectangle r1({ 5.00, 10.00 }, 3.00, 4.00);
   std::cout<<"Rectangle."<<"\n";
-  showAll(r1);
+  showAll(&r1);
   r1.move({ .x = 7.00, .y = 8.00 });
   std::cout<<"After move"<<"\n";
   showAll(r1);
   Circle c1({ 1.00, 3.00 }, 3.00);
   std::cout<<"Circle."<<"\n";
-  showAll(c1);
+  showAll(&c1);
   c1.move(3.00, -1.5);
   std::cout<<"After move"<<"\n";
   showAll(c1);
