e3b339ff931f7a632ea1034714ba7ef3d4da78f6
Ilia Malinin <malinin.ilja@gmail.com>
2019-04-08 21:07:16 +0000

    Changed Shape pointer name

--- malinin.ilia/A1/main.cpp
+++ malinin.ilia/A1/main.cpp
@@ -4,35 +4,33 @@
 
 int main()
 {
-  Shape *ptr;
-
   Circle circle(20.0, {2.0, -3.0});
   Rectangle rectangle(19.0, 7.0, {-15.0, -17.0});
 
-  ptr = &rectangle;
+  Shape * figure = &rectangle;
 
   std::cout << "\nRECTANGLE\n";
-  ptr->showInfo();
+  figure->showInfo();
   std::cout << "\nMove (to new position using point) X: -7.3, Y: 3.7\n";
-  ptr->move({-7.3, 3.7});
-  ptr->showCords();
+  figure->move({-7.3, 3.7});
+  figure->showCords();
   std::cout << "\nMove (using coordinates) X: -3.2, Y: 5.9\n";
-  ptr->move(-3.2, 5.9);
-  ptr->showCords();
-  ptr->showFrameRect();
+  figure->move(-3.2, 5.9);
+  figure->showCords();
+  figure->showFrameRect();
 
   std::cout << "\n==================================================\n";
 
-  ptr = &circle;
+  figure = &circle;
   std::cout << "\nCIRCLE\n";
-  ptr->showInfo();
+  figure->showInfo();
   std::cout << "\nMove (to new position using point) X: 5.1, Y: -2.2\n";
-  ptr->move({5.1, -2.2});
-  ptr->showCords();
+  figure->move({5.1, -2.2});
+  figure->showCords();
   std::cout << "\nMove (using coordinates) X: -1.0, Y: 2.0\n";
-  ptr->move(-1.0, 2.0);
-  ptr->showCords();
-  ptr->showFrameRect();
+  figure->move(-1.0, 2.0);
+  figure->showCords();
+  figure->showFrameRect();
 
   return(0);
 }
