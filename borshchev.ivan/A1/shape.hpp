48d53a27b7ec956e89adf66f128423879a133c25
Ivan Borshchev <ivan.borshchev@gmail.com>
2019-04-08 23:48:32 +0300

    Исправил отступы в main.cpp и shape.hpp

--- borshchev.ivan/A1/shape.hpp
+++ borshchev.ivan/A1/shape.hpp
@@ -6,11 +6,11 @@ class Shape
 {
 public:
   virtual ~Shape() = default;
-  virtual	double getArea() const = 0;
+  virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(double newX, double newY);
   virtual void move(const point_t & point);
   virtual void dataOutput() const;
-};
+}
 
 #endif
