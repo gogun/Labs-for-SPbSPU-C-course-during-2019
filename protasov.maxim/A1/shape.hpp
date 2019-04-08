11dd76f657cdeec88e24a273d5b3f9ab28e1be22
Maxim Protasov <protasovmax@gmail.com>
2019-04-08 17:07:08 +0000

    Fixed code following coding guidelines

--- protasov.maxim/A1/shape.hpp
+++ protasov.maxim/A1/shape.hpp
@@ -6,11 +6,11 @@
 class Shape
 {
 public:
-    virtual ~Shape() = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t &) = 0;
-    virtual void move(const double &, const double &) = 0;
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t &) = 0;
+  virtual void move(const double &, const double &) = 0;
 };
 
 #endif
