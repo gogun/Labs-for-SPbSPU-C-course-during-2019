d05102cbe097ca631671e6ab7f5f1c512695db80
Baboshina Elizaveta <blizababoshina200@gmail.com>
2019-04-08 14:16:36 +0000

    Added polymorphism

--- baboshina.elizaveta/A1/main.cpp
+++ baboshina.elizaveta/A1/main.cpp
@@ -2,25 +2,26 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
+void test(Shape &shape)
+{
+  shape.getInfo();
+  std::cout << "The area of shape is: " << shape.getArea() << std::endl;
+  std::cout << "Width of frame rectangle is: " << shape.getFrameRect().width << std::endl;
+  std::cout << "Height of frame rectangle is: " << shape.getFrameRect().height << std::endl;
+  shape.move({ 1,1 });
+  shape.getInfo();
+  shape.move(4, 5);
+  shape.getInfo();
+}
+
 int main()
 {
   Circle circle({ 3,4 }, 6);
-  circle.getInfo();
-  std::cout << "The area of circle is: " << circle.getArea() << std::endl;
-  std::cout << "Width of frame rectangle is: " << circle.getFrameRect().width << std::endl;
-  std::cout << "Height of frame rectangle is: " << circle.getFrameRect().height << std::endl;
-  circle.move({ 1,1 });
-  circle.getInfo();
-  circle.move(4, 5);
-  circle.getInfo();
+  std::cout << "\n---Information about circle---" << std::endl;
+  test(circle);
+
   Rectangle rectangle({ 5,5 }, 20, 5);
-  rectangle.getInfo();
-  std::cout << "The area of rectangle is: " << rectangle.getArea() << std::endl;
-  std::cout << "Width of frame rectangle is: " << rectangle.getFrameRect().width << std::endl;
-  std::cout << "Height of frame rectangle is: " << rectangle.getFrameRect().height << std::endl;
-  rectangle.move({ 6,4 });
-  rectangle.getInfo();
-  rectangle.move(-1, 1);
-  rectangle.getInfo();
+  std::cout << "\n---Information about rectangle---" << std::endl;
+  test(rectangle);
   return 0;
 }
