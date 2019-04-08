3328e35968e928bc563d1854731acc66686bcb9b
dimka_9910 <dimka99-10@mail.ru>
2019-04-08 16:45:27 +0300

    исправлен порядок вызова методов

--- neupokoev.dmitry/A1/main.cpp
+++ neupokoev.dmitry/A1/main.cpp
@@ -11,17 +11,17 @@ int main()
   Circle myCircle({5.0, 4.0}, 6.0);
   Shape* myShape = &myCircle;
   myShape->writeParameters();
-  myShape->move(5.0, 6.0);
-  myShape->writeParameters();
   myShape->move({7.0, -7.0});
+  myShape->writeParameters();
+  myShape->move(5.0, 6.0);
   myShape->writeParameters();
 
   Rectangle myRectangle({{9.0, 10.0}, 5.0, 4.0});
   myShape = &myRectangle;
   myShape->writeParameters();
-  myShape->move(5.0, 6.0);
-  myShape->writeParameters();
   myShape->move({7.0, -7.0});
+  myShape->writeParameters();
+  myShape->move(5.0, 6.0);
   myShape->writeParameters();
 
   return 0;
