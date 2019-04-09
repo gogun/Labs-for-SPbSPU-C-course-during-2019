c086ddd52fc9c1204af71cf319f8ba25100dca92
adlovlac <adlovlac@gmail.com>
2019-04-09 17:04:59 +0300

    Added spaces in cycles

--- prohorova.aleksandra/A2/main.cpp
+++ prohorova.aleksandra/A2/main.cpp
@@ -11,28 +11,28 @@ int main()
   prohorova::Shape* correctFigures[] = {&circle1, &circle2, &rectangle1, &rectangle2};
   int size = sizeof(correctFigures)/sizeof(prohorova::Shape);
   std::cout << "Starting values: \n";
-  for(int i = 0;i < size;i++)
+  for(int i = 0; i < size; i++)
   {
     correctFigures[i]->show();
   }
   const double dx = 10;
   const double dy = 20;
   std::cout << "\nMoving all figures for dx = " << dx << ", dy = " << dy << ":\n";
-  for(int i = 0;i<size;i++)
+  for(int i = 0; i < size; i++)
   {
     correctFigures[i]->move(dx,dy);
     correctFigures[i]->show();
   }
   const prohorova::point_t newPos{0, 0};
   std::cout << "\nMoving all figures to the point: (" << newPos.x << ", " <<  newPos.y << ")"  << "\n";
-  for(int i = 0;i<size;i++)
+  for(int i = 0; i < size; i++)
   {
     correctFigures[i]->move(newPos);
     correctFigures[i]->show();
   }
   const double factor = 1.5;
   std::cout << "\n Scaling all figures by factor " << factor << ":\n";
-  for(int i = 0;i<size;i++)
+  for(int i = 0; i < size; i++)
   {
     correctFigures[i]->scale(factor);
     correctFigures[i]->show();
