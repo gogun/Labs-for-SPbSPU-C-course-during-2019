3060c3a3f7a28365169692b41b590c855e805dee
smirnov.pavel.1 <smpashts@gmail.com>
2019-04-09 21:27:30 +0300

    Убраны пробелы, выделявшие оператор ->

--- smirnov.pavel/A1/main.cpp
+++ smirnov.pavel/A1/main.cpp
@@ -8,25 +8,25 @@ int main()
   Rectangle rect(20, 10, {5, 7});
   Shape *basePointer = &rect;
   std::cout << "Before first moving\n";
-  basePointer -> getInfo();
-  basePointer -> move({ 4, 9 });
+  basePointer->getInfo();
+  basePointer->move({ 4, 9 });
   std::cout << "After first moving\n";
-  basePointer -> getInfo();
-  basePointer -> move(10, 5);
+  basePointer->getInfo();
+  basePointer->move(10, 5);
   std::cout << "After second moving\n";
-  basePointer -> getInfo();
+  basePointer->getInfo();
 
   std::cout << "WORK WITH CIRCLE:\n";
   Circle circ(6, { 10, 5 });
   basePointer = &circ;
   std::cout << "Before first moving\n";
-  basePointer -> getInfo();
-  basePointer -> move({15, 20});
+  basePointer->getInfo();
+  basePointer->move({15, 20});
   std::cout << "After first moving\n";
-  basePointer -> getInfo();
-  basePointer -> move(-7, 12);
+  basePointer->getInfo();
+  basePointer->move(-7, 12);
   std::cout << "After second moving\n";
-  basePointer -> getInfo();
+  basePointer->getInfo();
 
   return 0;
 }
