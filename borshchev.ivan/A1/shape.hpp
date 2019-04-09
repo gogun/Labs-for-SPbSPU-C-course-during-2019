9388e7e334805574e85efebff5579483ab585a2d
Ivan Borshchev <ivan.borshchev@gmail.com>
2019-04-09 03:22:24 +0300

    Исправил виртуальные методы в shape.hpp

--- borshchev.ivan/A1/shape.hpp
+++ borshchev.ivan/A1/shape.hpp
@@ -8,9 +8,9 @@ public:
   virtual ~Shape() = default;
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
-  virtual void move(double newX, double newY);
-  virtual void move(const point_t & point);
-  virtual void dataOutput() const;
+  virtual void move(double newX, double newY) = 0;
+  virtual void move(const point_t & point) = 0;
+  virtual void dataOutput() const = 0;
 };
 
 #endif
