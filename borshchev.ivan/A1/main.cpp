85759896928a9411368b650edb28ebc6348921bd
Ivan Borshchev <ivan.borshchev@gmail.com>
2019-04-09 03:53:52 +0300

    Убрал лишние пробелы в main.cpp

--- borshchev.ivan/A1/main.cpp
+++ borshchev.ivan/A1/main.cpp
@@ -5,19 +5,19 @@
 
 int main()
 {
-  Rectangle rectangle({ 5,5 }, 4, 6);
+  Rectangle rectangle({5, 5}, 4, 6);
   Shape *shape = &rectangle;
   shape->dataOutput();
   std::cout << "Rectangle's AREA: " << shape->getArea() << '\n';
   shape->move(8, 9);
-  shape->move({ 2,0 });
+  shape->move({2, 0});
   shape->dataOutput();
-  Circle circle({ 5,5 }, 6);
+  Circle circle({5, 5}, 6);
   shape = &circle;
   shape->dataOutput();
   std::cout << "Circle's AREA: " << shape->getArea() << '\n';
   shape->move(4, 9);
-  shape->move({ -3, 7 });
+  shape->move({-3, 7});
   shape->dataOutput();
 
   return 0;
