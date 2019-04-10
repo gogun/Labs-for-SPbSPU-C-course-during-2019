607fb860d1930467eddc400d76e12f205f57b49f
ArtyomZemlyak <zemlyak.aa@edu.spbstu.ru>
2019-04-10 12:04:03 +0000

    Переменные приведены к одному виду.

--- zemlyak.artyom/A2/test-main.cpp
+++ zemlyak.artyom/A2/test-main.cpp
@@ -13,40 +13,40 @@ const double error = 0.0001;
 BOOST_AUTO_TEST_CASE(circleAfterMoveParameters)
 {
   zemlyak::Circle circle_test({1, 2}, 3);
-  const double initialArea = circle_test.getArea();
-  const zemlyak::rectangle_t initialFrameRect = circle_test.getFrameRect();
+  const double initial_area = circle_test.getArea();
+  const zemlyak::rectangle_t initial_frame_rect = circle_test.getFrameRect();
 
   circle_test.move(10, 15);
-  double currentArea = circle_test.getArea();
-  zemlyak::rectangle_t currentFrameRect = circle_test.getFrameRect();
+  double current_area = circle_test.getArea();
+  zemlyak::rectangle_t current_frame_rect = circle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height, current_frame_rect.height, error);
 
   circle_test.move({0, 1});
-  currentArea = circle_test.getArea();
-  currentFrameRect = circle_test.getFrameRect();
+  current_area = circle_test.getArea();
+  current_frame_rect = circle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height, current_frame_rect.height, error);
 }
 
 BOOST_AUTO_TEST_CASE(circleAfterScaleParameters)
 {
   zemlyak::Circle circle_test({1, 2}, 3);
-  const double initialArea = circle_test.getArea();
-  const zemlyak::rectangle_t initialFrameRect = circle_test.getFrameRect();
+  const double initial_area = circle_test.getArea();
+  const zemlyak::rectangle_t initial_frame_rect = circle_test.getFrameRect();
 
   const int factor = 2;
   circle_test.scale(factor);
-  double currentArea = circle_test.getArea();
-  zemlyak::rectangle_t currentFrameRect = circle_test.getFrameRect();
+  double current_area = circle_test.getArea();
+  zemlyak::rectangle_t current_frame_rect = circle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea * factor * factor, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width * factor, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height * factor, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area * factor * factor, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width * factor, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height * factor, current_frame_rect.height, error);
 }
 
 BOOST_AUTO_TEST_CASE(circleThrowException)
@@ -60,40 +60,40 @@ BOOST_AUTO_TEST_CASE(circleThrowException)
 BOOST_AUTO_TEST_CASE(rectangleAfterMoveParameters)
 {
   zemlyak::Rectangle rectangle_test({1, 2}, 3, 5);
-  const double initialArea = rectangle_test.getArea();
-  const zemlyak::rectangle_t initialFrameRect = rectangle_test.getFrameRect();
+  const double initial_area = rectangle_test.getArea();
+  const zemlyak::rectangle_t initial_frame_rect = rectangle_test.getFrameRect();
 
   rectangle_test.move(10, 15);
-  double currentArea = rectangle_test.getArea();
-  zemlyak::rectangle_t currentFrameRect = rectangle_test.getFrameRect();
+  double current_area = rectangle_test.getArea();
+  zemlyak::rectangle_t current_frame_rect = rectangle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height, current_frame_rect.height, error);
 
   rectangle_test.move({0, 1});
-  currentArea = rectangle_test.getArea();
-  currentFrameRect = rectangle_test.getFrameRect();
+  current_area = rectangle_test.getArea();
+  current_frame_rect = rectangle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height, current_frame_rect.height, error);
 }
 
 BOOST_AUTO_TEST_CASE(rectangleAfterScaleParameters)
 {
   zemlyak::Rectangle rectangle_test({1, 2}, 3, 5);
-  const double initialArea = rectangle_test.getArea();
-  const zemlyak::rectangle_t initialFrameRect = rectangle_test.getFrameRect();
+  const double initial_area = rectangle_test.getArea();
+  const zemlyak::rectangle_t initial_frame_rect = rectangle_test.getFrameRect();
 
   const int factor = 2;
   rectangle_test.scale(factor);
-  double currentArea = rectangle_test.getArea();
-  zemlyak::rectangle_t currentFrameRect = rectangle_test.getFrameRect();
+  double current_area = rectangle_test.getArea();
+  zemlyak::rectangle_t current_frame_rect = rectangle_test.getFrameRect();
 
-  BOOST_CHECK_CLOSE(initialArea * factor * factor, currentArea, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.width * factor, currentFrameRect.width, error);
-  BOOST_CHECK_CLOSE(initialFrameRect.height * factor, currentFrameRect.height, error);
+  BOOST_CHECK_CLOSE(initial_area * factor * factor, current_area, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.width * factor, current_frame_rect.width, error);
+  BOOST_CHECK_CLOSE(initial_frame_rect.height * factor, current_frame_rect.height, error);
 }
 
 BOOST_AUTO_TEST_CASE(rectangleThrowException)
