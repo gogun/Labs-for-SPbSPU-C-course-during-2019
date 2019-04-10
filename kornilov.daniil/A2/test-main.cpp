265993f9fda39da64f06513f46522ba880411c53
Kornilov Daniil Fedorovich <daniil-kornilov@yandex.ru>
2019-04-10 13:11:32 +0000

    BOOST_CHECK_EQUAL replaced by BOOST_CHECK_CLOSE, added const CHECKERROR, added const modifier to variables.

--- kornilov.daniil/A2/test-main.cpp
+++ kornilov.daniil/A2/test-main.cpp
@@ -4,80 +4,82 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
+const double CHECKERROR = 0.0001;
+
 BOOST_AUTO_TEST_SUITE(TestRectangle)
 
 BOOST_AUTO_TEST_CASE(WidthPermanencyAfterMovingdXdY)
 {
-  double width = 10;
+  const double width = 10.1;
   kornilov::Rectangle rectangle({{0, 0}, width, 11});
   rectangle.move(10, 10);
-  BOOST_CHECK_EQUAL(width, rectangle.getFrameRect().width);
+  BOOST_CHECK_CLOSE(width, rectangle.getFrameRect().width, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(WidthPermanencyAfterMovingToPoint)
 {
-  double width = 10;
+  const double width = 10.1;
   kornilov::Rectangle rectangle({{0, 0}, width, 11});
   rectangle.move({10, 10});
-  BOOST_CHECK_EQUAL(width, rectangle.getFrameRect().width);
+  BOOST_CHECK_CLOSE(width, rectangle.getFrameRect().width, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(HeightPermanencyAfterMovingdXdY)
 {
-  double height = 11;
+  const double height = 11.1;
   kornilov::Rectangle rectangle({{0, 0}, 10, height});
   rectangle.move(10, 10);
-  BOOST_CHECK_EQUAL(height, rectangle.getFrameRect().height);
+  BOOST_CHECK_CLOSE(height, rectangle.getFrameRect().height, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(HeightPermanencyAfterMovingToPoint)
 {
-  double height = 11;
+  const double height = 11.1;
   kornilov::Rectangle rectangle({{0, 0}, 10, height});
   rectangle.move({10, 10});
-  BOOST_CHECK_EQUAL(height, rectangle.getFrameRect().height);
+  BOOST_CHECK_CLOSE(height, rectangle.getFrameRect().height, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(AreaPermanencyAfterMovingdXdY)
 {
-  kornilov::Rectangle rectangle({{0, 0}, 10, 11});
-  double rectArea = rectangle.getArea();
+  kornilov::Rectangle rectangle({{0, 0}, 10.1, 11.1});
+  const double rectArea = rectangle.getArea();
   rectangle.move(10, 10);
-  BOOST_CHECK_EQUAL(rectArea, rectangle.getArea());
+  BOOST_CHECK_CLOSE(rectArea, rectangle.getArea(), CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(AreaPermanencyAfterMovingToPoint)
 {
-  kornilov::Rectangle rectangle({{0, 0}, 10, 11});
-  double rectArea = rectangle.getArea();
+  kornilov::Rectangle rectangle({{0, 0}, 10.1, 11.1});
+  const double rectArea = rectangle.getArea();
   rectangle.move({10, 10});
-  BOOST_CHECK_EQUAL(rectArea, rectangle.getArea());
+  BOOST_CHECK_CLOSE(rectArea, rectangle.getArea(), CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(SquareChangingAfterScaling)
 {
-  kornilov::Rectangle rectangle({{0, 0}, 10, 11});
-  double rectArea = rectangle.getArea();
-  double coef = 2;
+  kornilov::Rectangle rectangle({{0, 0}, 10.1, 11.1});
+  const double rectArea = rectangle.getArea();
+  const double coef = 2.1;
   rectangle.scale(coef);
-  BOOST_CHECK_EQUAL(coef * coef, rectangle.getArea() / rectArea);
+  BOOST_CHECK_CLOSE(coef * coef, rectangle.getArea() / rectArea, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(InvalidWidth)
 {
-  double width = 0;
+  const double width = -0.1;
   BOOST_CHECK_THROW(kornilov::Rectangle rectangle({{0, 0}, width, 11}), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(InvalidHeight)
 {
-  double height = 0;
+  const double height = -0.1;
   BOOST_CHECK_THROW(kornilov::Rectangle rectangle({{0, 0}, 10, height}), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(InvalidCoefficient)
 {
-  double coef = 0;
+  const double coef = -0.1;
   kornilov::Rectangle rectangle({{0, 0}, 10, 11});
   BOOST_CHECK_THROW(rectangle.scale(coef), std::invalid_argument);
 }
@@ -88,55 +90,55 @@ BOOST_AUTO_TEST_SUITE(TestCircle)
 
 BOOST_AUTO_TEST_CASE(RadiusPermanencyAfterMovingdXdY)
 {
-  double radius = 10;
+  const double radius = 10.1;
   kornilov::Circle circle({0, 0}, radius);
   circle.move(10, 10);
-  BOOST_CHECK_EQUAL(radius, circle.getFrameRect().height / 2);
+  BOOST_CHECK_CLOSE(radius, circle.getFrameRect().height / 2, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(RadiusPermanencyAfterMovingToPoint)
 {
-  double radius = 10;
+  const double radius = 10.1;
   kornilov::Circle circle({0, 0}, radius);
   circle.move({10, 10});
-  BOOST_CHECK_EQUAL(radius, circle.getFrameRect().height / 2);
+  BOOST_CHECK_CLOSE(radius, circle.getFrameRect().height / 2, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(AreaPermanencyAfterMovingdXdY)
 {
-  kornilov::Circle circle({0, 0}, 10);
-  double circArea = circle.getArea();
+  kornilov::Circle circle({0, 0}, 10.1);
+  const double circArea = circle.getArea();
   circle.move(10, 10);
-  BOOST_CHECK_EQUAL(circArea, circle.getArea());
+  BOOST_CHECK_CLOSE(circArea, circle.getArea(), CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(AreaPermanencyAfterMovingToPoint)
 {
-  kornilov::Circle circle({0, 0}, 10);
-  double circArea = circle.getArea();
+  kornilov::Circle circle({0, 0}, 10.1);
+  const double circArea = circle.getArea();
   circle.move({10, 10});
-  BOOST_CHECK_EQUAL(circArea, circle.getArea());
+  BOOST_CHECK_CLOSE(circArea, circle.getArea(), CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(SquareChangingAfterScaling)
 {
-  kornilov::Circle circle({0, 0}, 10);
-  double circArea = circle.getArea();
-  double coef = 2;
+  kornilov::Circle circle({0, 0}, 10.1);
+  const double circArea = circle.getArea();
+  const double coef = 2.1;
   circle.scale(coef);
-  BOOST_CHECK_EQUAL(coef * coef, circle.getArea() / circArea);
+  BOOST_CHECK_CLOSE(coef * coef, circle.getArea() / circArea, CHECKERROR);
 }
 
 BOOST_AUTO_TEST_CASE(InvalidRadius)
 {
-  double radius = 0;
+  const double radius = -0.1;
   BOOST_CHECK_THROW(kornilov::Circle circle({0, 0}, radius), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(InvalidCoefficient)
 {
   kornilov::Circle circle({0, 0}, 10);
-  double coef = 0;
+  const double coef = -0.1;
   BOOST_CHECK_THROW(circle.scale(coef), std::invalid_argument);
 }
 
