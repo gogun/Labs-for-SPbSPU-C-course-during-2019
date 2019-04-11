f0e35fb0b7511fec20c1696947ff277a1ea0db56
Tsygarova.in <aazz007008@gmail.com>
2019-04-11 19:35:16 +0300

    Add A2

---
+++ tsygarova.irina/A2/test-main.cpp
@@ -0,0 +1,95 @@
+#define BOOST_TEST_MODULE A2
+
+#include <stdexcept>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include "circle.hpp"
+#include "rectangle.hpp"
+
+BOOST_AUTO_TEST_SUITE(A2TestSuite)
+
+const double Epsilon = 0.01;
+
+BOOST_AUTO_TEST_CASE(RectangleConstancyAfterMoving)
+{
+  tsygarova::Rectangle testRectangle(4, 2, {1, 3});
+  const tsygarova::rectangle_t InitFrame = testRectangle.getFrameRect();
+  const double InitArea = testRectangle.getArea();
+
+  testRectangle.move(4, 7);
+  tsygarova::rectangle_t CurrentFrame = testRectangle.getFrameRect();
+  double CurrentArea = testRectangle.getArea();
+  BOOST_CHECK_CLOSE(InitFrame.width, CurrentFrame.width, Epsilon);
+  BOOST_CHECK_CLOSE(InitFrame.height, CurrentFrame.height, Epsilon);
+  BOOST_CHECK_CLOSE(InitArea, CurrentArea, Epsilon);
+
+  testRectangle.move({8, 6});
+  CurrentFrame = testRectangle.getFrameRect();
+  CurrentArea = testRectangle.getArea();
+  BOOST_CHECK_CLOSE(InitFrame.width, CurrentFrame.width, Epsilon);
+  BOOST_CHECK_CLOSE(InitFrame.height, CurrentFrame.height, Epsilon);
+  BOOST_CHECK_CLOSE(InitArea, CurrentArea, Epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(CircleConstancyAfterMoving)
+{
+  tsygarova::Circle testCircle(6, {2, 3});
+  const tsygarova::rectangle_t InitFrame = testCircle.getFrameRect();
+  const double InitArea = testCircle.getArea();
+
+  testCircle.move(5, 9);
+  tsygarova::rectangle_t CurrentFrame = testCircle.getFrameRect();
+  double CurrentArea = testCircle.getArea();
+  BOOST_CHECK_CLOSE(InitFrame.width, CurrentFrame.width, Epsilon);
+  BOOST_CHECK_CLOSE(InitFrame.height, CurrentFrame.height, Epsilon);
+  BOOST_CHECK_CLOSE(InitArea, CurrentArea, Epsilon);
+
+  testCircle.move({9, 3});
+  CurrentFrame = testCircle.getFrameRect();
+  CurrentArea = testCircle.getArea();
+  BOOST_CHECK_CLOSE(InitFrame.width, CurrentFrame.width, Epsilon);
+  BOOST_CHECK_CLOSE(InitFrame.height, CurrentFrame.height, Epsilon);
+  BOOST_CHECK_CLOSE(InitArea, CurrentArea, Epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(RectangleScaling)
+{
+  tsygarova::Rectangle testRectangle(4, 2, {1, 3});
+  const double InitArea = testRectangle.getArea();
+  const double ScaleCoefficient = 4;
+
+  testRectangle.scale(ScaleCoefficient);
+  double CurrentArea = testRectangle.getArea();
+  BOOST_CHECK_CLOSE(InitArea * ScaleCoefficient * ScaleCoefficient, CurrentArea, Epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(CircleScaling)
+{
+  tsygarova::Circle testCircle(6, {2, 3});
+  const double InitArea = testCircle.getArea();
+  const double ScaleCoefficient = 2;
+
+  testRCircle.scale(ScaleCoefficient);
+  double CurrentArea = testCircle.getArea();
+  BOOST_CHECK_CLOSE(InitArea * ScaleCoefficient * ScaleCoefficient, CurrentArea, Epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(InvalidParametersRectangle)
+{
+  BOOST_CHECK_THROW(tsygarova::Rectangle({-6, 3, {1, 3}})), std::invalid_argument);
+  BOOST_CHECK_THROW(tsygarova::Rectangle({6, -3, {1, 3}})), std::invalid_argument);
+  BOOST_CHECK_THROW(tsygarova::Rectangle({-6, -3, {1, 3}})), std::invalid_argument);
+    
+  tsygarova::Rectangle testRectangle({6, 3, {1, 3}});
+  BOOST_CHECK_THROW(testRectangle.scale(0), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(InvalidParametersCircle)
+{
+  BOOST_CHECK_THROW(tsygarova::Circle(-7, {3, 4})), std::invalid_argument);
+    
+  tsygarova::Circle testCircle(6, 3, {1, 3});
+  BOOST_CHECK_THROW(testCircle.scale(0), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ tsygarova.irina/common/rectangle.cpp
@@ -0,0 +1,53 @@
+#include "rectangle.hpp"
+
+#include <iostream>
+#include <stdexcept>
+
+tsygarova::Rectangle::Rectangle(const rectangle_t & rect):
+  rect_(rect)
+{
+  if ((rect_.height < 0.0) || (rect_.width < 0.0))
+  {
+    throw std::invalid_argument("Error. Invalid height or width of rectangle.\n");
+  }
+}
+
+double tsygarova::Rectangle::getArea() const
+{
+  return (rect_.width * rect_.height);
+}
+
+tsygarova::rectangle_t tsygarova::Rectangle::getFrameRect() const
+{
+  return rect_;
+}
+
+void tsygarova::Rectangle::move(const point_t & point)
+{
+  rect_.pos = point;
+}
+
+void tsygarova::Rectangle::move(const double dx, const double dy)
+{
+  rect_.pos.x += dx;
+  rect_.pos.y += dy;
+}
+
+void tsygarova::Rectangle::print() const
+{
+  std::cout << "Area of rectangle: " << getArea() << std::endl;
+  std::cout << "Width of frame rectangle: " << rect_.width << std::endl;
+  std::cout << "Height of frame rectangle: " << rect_.height << std::endl;
+  std::cout << "Center point of frame rectangle: (" << rect_.pos.x
+            << "; " << rect_.pos.y << ")" << std::endl;
+}
+
+void tsygarova::Rectangle::scale(const double coefficient)
+{
+  if (coefficient <= 0)
+  {
+    throw std::invalid_argument("Error.Invalid coefficient of scale.\n");
+  }
+  rect_.height *= coefficient;
+  rect_.width *= coefficient;
+}
