a434136012b2e09fabc5d98b70cbe1c7d222cf63
Шеве Никита Максимович <nikk-nikk@bk.ru>
2019-04-11 19:25:07 +0000

    Update main.cpp

--- sheve.nikita/A1/main.cpp
+++ sheve.nikita/A1/main.cpp
@@ -21,20 +21,7 @@ int main()
   std::cout << "Moving rectangle to point x=-27, y=52" << std::endl;
   rectp->move({ -27, 52 });
   printParams(rect);
-  std::cout << "Moving rectangle by dx = 10, dy = -30" << std::endl;
-  rectp->move(10, -30);
-  printParams(rect);
-
-  std::cout << "Creating Circle x = 20, y = 30, r = 13" << std::endl;
-  Circle circ({ 20, 30 }, 13);
-  Shape *circp = &circ;
-  Rectangle rect({ 20, 10 }, 15, 50);
-  Shape *rectp = &rect;
-  printParams(rect);
-  std::cout << "Moving rectangle to point x=-27, y=52" << std::endl;
-  rectp->move({ -27, 52 });
-  printParams(rect);
-  std::cout << "Moving rectangle by dx=10, dy=-30" << std::endl;
+  std::cout << "Moving rectangle by dx=10, dy=30" << std::endl;
   rectp->move(10, -30);
   printParams(rect);
 
@@ -42,10 +29,10 @@ int main()
   Circle circ({ 20, 30 }, 13);
   Shape *circp = &circ;
   printParams(circ);
-  std::cout << "Moving circle to point x = 32, y = -6" << std::endl;
+  std::cout << "Moving circle to point x=32, y=-6" << std::endl;
   circp->move({ 32, -6 });
   printParams(circ);
-  std::cout << "Moving circle by dx = -12, dy = 9" << std::endl;
+  std::cout << "Moving circle by dx=-12, dy=9" << std::endl;
   circp->move(-12, 9);
   std::cout << "Moving circle by dx=-12, dy=9" << std::endl;
   circp->move(-12, 9);
@@ -58,10 +45,10 @@ int main()
   Triangle tria({ 12, -12 },{ 13, 5 },{ 1, 23 });
   Shape *triap = &tria;
   printParams(tria);
-  std::cout << "Moving triangle to point x = -7, y = 4" << std::endl;
+  std::cout << "Moving triangle to point x=-7, y=4" << std::endl;
   triap->move({ -7, 4 });
   printParams(tria);
-  std::cout << "Moving triangle by dx = 10, dy = 20" << std::endl;
+  std::cout << "Moving triangle by dx=10, dy=20" << std::endl;
   triap->move(10, 20);
   std::cout << "Moving triangle by dx=10, dy=20" << std::endl;
   triap->move(10, 20);
