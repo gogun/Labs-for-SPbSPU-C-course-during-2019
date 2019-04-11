62a9e3aa0400e33feb747232ce2a3ae3bb8b0975
Шеве Никита Максимович <nikk-nikk@bk.ru>
2019-04-11 17:45:08 +0000

    Убрал лишние пробелы

--- sheve.nikita/A1/circle.hpp
+++ sheve.nikita/A1/circle.hpp
@@ -1,6 +1,5 @@
 #ifndef CIRCLE_HPP
 #define CIRCLE_HPP
-
 #include "shape.hpp"
 
 class Circle : public Shape
@@ -9,8 +8,8 @@ public:
   Circle (const point_t &p, double r);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
-  void move (const point_t &p) override;
-  void move (double dx, double dy) override;
+  void move(const point_t &p) override;
+  void move(double dx, double dy) override;
 
 private:
   point_t pos;
