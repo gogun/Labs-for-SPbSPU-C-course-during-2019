3fb4afd34a2c4e69c3356fca025cb4dc4eea38c2
Dmitrii Lutsenko <lutsenko.dyu@edu.spbstu.ru>
2019-04-12 22:30:28 +0300

    Добавлена переменная inaccuracy

--- lutsenko.dmitrii/A2/test-main.cpp
+++ lutsenko.dmitrii/A2/test-main.cpp
@@ -4,6 +4,8 @@
 #include "circle.hpp"
 #include "rectangle.hpp"
 
+double inaccuracy = 0.001;
+
 BOOST_AUTO_TEST_SUITE(testShape)
 
 BOOST_AUTO_TEST_CASE(unalteredRectangleAfterMove1)
@@ -12,9 +14,9 @@ BOOST_AUTO_TEST_CASE(unalteredRectangleAfterMove1)
   lutsenko::rectangle_t rect_before = rect.getFrameRect();
   double area_before = rect.getArea();
   rect.move({100.0, 200.0});
-  BOOST_CHECK_CLOSE_FRACTION(area_before, rect.getArea(), 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, rect.getFrameRect().height, 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, rect.getFrameRect().width, 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before, rect.getArea(), inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, rect.getFrameRect().height, inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, rect.getFrameRect().width, inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(unalteredRectangleAfterMove2)
@@ -23,9 +25,9 @@ BOOST_AUTO_TEST_CASE(unalteredRectangleAfterMove2)
   lutsenko::rectangle_t rect_before = rect.getFrameRect();
   double area_before = rect.getArea();
   rect.move(10.0, 20.0);
-  BOOST_CHECK_CLOSE_FRACTION(area_before, rect.getArea(), 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, rect.getFrameRect().height, 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, rect.getFrameRect().width, 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before, rect.getArea(), inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, rect.getFrameRect().height, inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, rect.getFrameRect().width, inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(unalteredCircleAfterMove1)
@@ -34,9 +36,9 @@ BOOST_AUTO_TEST_CASE(unalteredCircleAfterMove1)
   lutsenko::rectangle_t rect_before = circle.getFrameRect();
   double area_before = circle.getArea();
   circle.move({5.0, 7.0});
-  BOOST_CHECK_CLOSE_FRACTION(area_before, circle.getArea(), 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, circle.getFrameRect().height, 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, circle.getFrameRect().width, 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before, circle.getArea(), inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, circle.getFrameRect().height, inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, circle.getFrameRect().width, inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(unalteredCircleAfterMove2)
@@ -45,9 +47,9 @@ BOOST_AUTO_TEST_CASE(unalteredCircleAfterMove2)
   lutsenko::rectangle_t rect_before = circle.getFrameRect();
   double area_before = circle.getArea();
   circle.move({10.0, 10.0});
-  BOOST_CHECK_CLOSE_FRACTION(area_before, circle.getArea(), 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, circle.getFrameRect().height, 0.001);
-  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, circle.getFrameRect().width, 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before, circle.getArea(), inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.height, circle.getFrameRect().height, inaccuracy);
+  BOOST_CHECK_CLOSE_FRACTION(rect_before.width, circle.getFrameRect().width, inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(quadraticChangeRectangleArea)
@@ -56,7 +58,7 @@ BOOST_AUTO_TEST_CASE(quadraticChangeRectangleArea)
   double area_before = rect.getArea();
   double coefficient = 3;
   rect.scale(coefficient);
-  BOOST_CHECK_CLOSE_FRACTION(area_before * coefficient * coefficient, rect.getArea(), 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before * coefficient * coefficient, rect.getArea(), inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(quadraticChangeCircleArea)
@@ -65,7 +67,7 @@ BOOST_AUTO_TEST_CASE(quadraticChangeCircleArea)
   double area_before = circle.getArea();
   double coefficient = 3;
   circle.scale(coefficient);
-  BOOST_CHECK_CLOSE_FRACTION(area_before * coefficient * coefficient, circle.getArea(), 0.001);
+  BOOST_CHECK_CLOSE_FRACTION(area_before * coefficient * coefficient, circle.getArea(), inaccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(corectRectParametrs)
