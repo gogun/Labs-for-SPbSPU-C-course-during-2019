3a44161a20ff2d28700b014f354047d008303a21
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-12 23:43:04 +0300

    Added triangles support Now program works even with triangles (getting area, frame rectangle)

---
+++ mesnyankin.nikita/A1/triangle.hpp
@@ -0,0 +1,30 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "shape.hpp"
+
+class Triangle : public Shape {
+
+public:
+  Triangle(const point_t &a, const point_t &b, const point_t &c);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(double x, double y) override;
+  void move(const point_t &point) override;
+  point_t getA() const;
+  point_t getB() const;
+  point_t getC() const;
+  double getAB() const;
+  double getBC() const;
+  double getCA() const;
+  point_t getPos() const;
+
+private:
+  point_t a_;
+  point_t b_;
+  point_t c_;
+  point_t pos_;
+
+};
+
+#endif
