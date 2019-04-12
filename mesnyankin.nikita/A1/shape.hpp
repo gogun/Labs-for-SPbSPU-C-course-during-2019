c4f6dbe921028167aa1dba482e94fb724b7fe7c1
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-12 23:18:43 +0300

    Pass-by-const reference in void move(point_t) Do not create a copy of argument and works faster

--- mesnyankin.nikita/A1/shape.hpp
+++ mesnyankin.nikita/A1/shape.hpp
@@ -9,7 +9,7 @@ class Shape {
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(double, double) = 0;
-    virtual void move(point_t) = 0;
+    virtual void move(const point_t &point) = 0;
 };
 
 #endif
