eef3cd55e11d5fbbb3231f2ffbb67d0d77830569
Krinkina_Vladislava <vladakrinkina09@gmail.com>
2019-04-12 08:27:21 +0300

    shape_circle и shape_rectangle изменены на shape

--- krinkina.vladislava/A1/main.cpp
+++ krinkina.vladislava/A1/main.cpp
@@ -2,37 +2,36 @@
 #include "circle.hpp"
 #include "rectangle.hpp"
 
-
 int main()
 {
-  Circle circle{ {6, 5}, 13 };
-  Shape *shape_circle = &circle;
+  Circle circle{{6, 5}, 13};
+  Shape * shape = &circle;
 
   std::cout << "THE PARAMETERS OF THE CIRCLE\n";
-  shape_circle->printInfo();
+  shape->printInfo();
 
-  shape_circle->move(0, 1);
+  shape->move(0, 1);
   std::cout << "the parameters of the circle after move dx = 0, dy = 1 :\n";
-  shape_circle->printInfo();
+  shape->printInfo();
 
-  shape_circle->move({ -3, -2 });
+  shape->move({-3, -2});
   std::cout << "the parameters of the circle after move centre to the point (-3,2) :\n";
-  shape_circle->printInfo();
+  shape->printInfo();
   std::cout << '\n';
 
-  Rectangle rect{ {1, 5}, 15, 10 };
-  Shape *shape_rectangle = &rect;
+  Rectangle rect{{1, 5}, 15, 10};
+  shape = &rect;
 
   std::cout << "THE PARAMETERS OF THE RACTANGLE \n";
-  shape_rectangle->printInfo();
+  shape->printInfo();
 
-  shape_rectangle->move(-1, -5);
+  shape->move(-1, -5);
   std::cout << "the parameters of the rectangle after move dx = -1, dy = -5:\n";
-  shape_rectangle->printInfo();
+  shape->printInfo();
 
-  shape_rectangle->move({ 3, 1 });
+  shape->move({3, 1});
   std::cout << "the parameters of the rectangle after move centre to the point (3,1) :\n";
-  shape_rectangle->printInfo();
+  shape->printInfo();
 
   return 0;
 }
