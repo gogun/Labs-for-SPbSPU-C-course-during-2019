e55e058e5df1ce046dd11223e397a50905b1dc86
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-12 23:25:48 +0300

    Added pass-by-const reference, constructor; dependencies are minimized Deleted one extra #include and added new constructor. Moreover, added pass-by-reference in constructor and void move

--- mesnyankin.nikita/A1/circle.hpp
+++ mesnyankin.nikita/A1/circle.hpp
@@ -1,19 +1,18 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
-#include "base-types.hpp"
 #include "shape.hpp"
 
 class Circle : public Shape {
   public:
-    Circle(double, point_t);
+    Circle(double radius, const point_t &pos);
+    Circle(double radius, double posx, double posy);
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(double, double) override;
-    void move(point_t) override;
+    void move(double x, double y) override;
+    void move(const point_t &point) override;
     double getRadius();
     point_t getPos();
-    void showData();
   private:
     double radius_;
     point_t pos_;
