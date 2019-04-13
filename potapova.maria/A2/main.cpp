1f3a31c00490c7f74ee59bc14a1b2f2940abb0d0
Maria Potapova <potapova.ma@mail.ru>
2019-04-14 01:12:54 +0300

    my first commit with A2

---
+++ potapova.maria/A2/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+int main()
+{
+  potapova::Circle circle({5,7},3);
+  potapova::Shape *shape = &circle;
+  std::cout << "Area: " << shape->getArea() << std::endl;
+  shape->move(3,4);
+  potapova::rectangle_t rect = shape->getFrameRect();
+  std::cout << "FrameRect: " << '\n'
+    << "  width - " << rect.width << '\n'
+    << "  height - " << rect.height << '\n'
+    << "  center - " << rect.pos.x << " , " << rect.pos.y << std::endl;
+
+  shape->move({1,3});
+  potapova::rectangle_t rect_ = shape->getFrameRect();
+  std::cout << "New center: " << rect_.pos.x<< " , " << rect_.pos.y << std::endl;
+
+  potapova::Rectangle rectangle({{9,2}, 3, 8});
+  shape = &rectangle;
+  std::cout << "Area: " << shape->getArea() << std::endl;
+  shape->move(7,1);
+  potapova::rectangle_t rectan = shape->getFrameRect();
+  std::cout << "FrameRect: " << '\n'
+    << "  height - " << rectan.height << '\n'
+    << "  width - " << rectan.width << '\n'
+    << "  center - " << rectan.pos.x << " , " << rectan.pos.y <<std::endl;
+
+  shape->move({4,2});
+  potapova::rectangle_t rectan_ = shape->getFrameRect();
+  std::cout << "New center - " << rectan_.pos.x << " , " << rectan_.pos.y <<std::endl;
+  return 0;
+}
