170bcda23a054aa68eb73b7347b180bee4be4aa2
Arthur <almond.pitch@mail.ru>
2019-04-14 14:46:50 +0300

    Added Triangle; assert() in main.cpp & edited program testing(cosmetically);

--- mkrtchyan.arthur/A1/main.cpp
+++ mkrtchyan.arthur/A1/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <cassert>
 #include "rectangle.hpp"
 #include "circle.hpp"
+#include "triangle.hpp"
 
 void printShapeInfo(const Shape * pointer)
 {
-  std::cout<< "Area: " << pointer->getArea() << "\n";
-  std::cout<< "Centre: " << "( " << pointer->getFrameRect().pos.x << "," << pointer->getFrameRect().pos.y << " )\n";
+  assert(pointer);
+  std::cout<< "Shape's area: " << pointer->getArea() << "\n";
+  std::cout<< "Frame rectangle's centre: " << "( " << pointer->getFrameRect().pos.x << "," << pointer->getFrameRect().pos.y << " )\n";
   std::cout<< "Frame rectangle's width - " << pointer->getFrameRect().width << " & height - " << pointer->getFrameRect().height << "\n";
 }
 
@@ -15,10 +18,10 @@ int main()
   Shape * shapePointer = &circleSample;
   std::cout << "--Circle:--\n";
   printShapeInfo(shapePointer);
-  std::cout<< "--Move to a point (1,2)--\n";
+  std::cout<< "--Move shape to a point (1,2)--\n";
   shapePointer->move({1,2});
   printShapeInfo(shapePointer);
-  std::cout<< "--Shift x by 3 & y by 4--\n";
+  std::cout<< "--Shift shape's x by 3 & y by 4--\n";
   shapePointer->move(3,4);
   printShapeInfo(shapePointer);
 
@@ -28,11 +31,24 @@ int main()
   shapePointer = &rectangleSample;
   std::cout << "--Rectangle:--\n";
   printShapeInfo(shapePointer);
-  std::cout<< "--Move to a point (5,6)--\n";
+  std::cout<< "--Move shape to a point (5,6)--\n";
   shapePointer->move({5,6});
   printShapeInfo(shapePointer);
-  std::cout<< "--Shift x by 7 & y by 8--\n";
+  std::cout<< "--Shift shape's x by 7 & y by 8--\n";
   shapePointer->move(7,8);
+  printShapeInfo(shapePointer);
+
+  std::cout<< "\n\n";
+
+  Triangle triangleSample ({5,5},{16,8},{19,15});
+  shapePointer = &triangleSample;
+  std::cout << "--Triangle:--\n";
+  printShapeInfo(shapePointer);
+  std::cout<< "--Move shape to a point (9,10)--\n";
+  shapePointer->move({9,10});
+  printShapeInfo(shapePointer);
+  std::cout<< "--Shift shape's x by 11 & y by 12--\n";
+  shapePointer->move(11,12);
   printShapeInfo(shapePointer);
 
   return 0;
---
+++ mkrtchyan.arthur/A1/triangle.cpp
@@ -0,0 +1,57 @@
+#include "triangle.hpp"
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+
+Triangle::Triangle(point_t point1, point_t point2, point_t point3):
+  point1_(point1),
+  point2_(point2),
+  point3_(point3),
+  pos_({(point1.x + point2.x + point3.x)/3,(point1.y + point2.y + point3.y)/3})
+{
+  const double a = sqrt((point1_.x - point2_.x) * (point1_.x - point2_.x) + (point1_.y - point2_.y) * (point1_.y - point2_.y));
+  const double b = sqrt((point2_.x - point3_.x) * (point2_.x - point3_.x) + (point2_.y - point3_.y) * (point2_.y - point3_.y));
+  const double c = sqrt((point3_.x - point1_.x) * (point3_.x - point1_.x) + (point3_.y - point1_.y) * (point3_.y - point1_.y));
+  assert((a>0) && (b>0) && (c>0) && ((a + b)>c) && ((c + b)>a) && ((a + c)>b));
+}
+
+double Triangle::getArea() const
+{
+  const double a = sqrt((point1_.x - point2_.x) * (point1_.x - point2_.x) + (point1_.y - point2_.y) * (point1_.y - point2_.y));
+  const double b = sqrt((point2_.x - point3_.x) * (point2_.x - point3_.x) + (point2_.y - point3_.y) * (point2_.y - point3_.y));
+  const double c = sqrt((point3_.x - point1_.x) * (point3_.x - point1_.x) + (point3_.y - point1_.y) * (point3_.y - point1_.y));
+  const double p = 0.5 * (a + b + c);
+  return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+rectangle_t Triangle::getFrameRect() const
+{
+  const double xMax = std::max({point1_.x,point2_.x,point3_.x}, [](double number1, double number2) { return number1 <= number2; });
+  const double xMin = std::min({point1_.x,point2_.x,point3_.x}, [](double number1, double number2) { return number1 <= number2; });
+  const double yMax = std::max({point1_.y,point2_.y,point3_.y}, [](double number1, double number2) { return number1 <= number2; });
+  const double yMin = std::min({point1_.y,point2_.y,point3_.y}, [](double number1, double number2) { return number1 <= number2; });
+  return { xMax - xMin, yMax - yMin, {xMax - (xMax - xMin)/2, yMax - (yMax - yMin)/2} };
+}
+
+void Triangle::move(point_t point)
+{
+  point1_.x -= (pos_.x - point.x);
+  point1_.y -= (pos_.y - point.y);
+  point2_.x -= (pos_.x - point.x);
+  point2_.y -= (pos_.y - point.y);
+  point3_.x -= (pos_.x - point.x);
+  point3_.y -= (pos_.y - point.y);
+  pos_ = point;
+}
+
+void Triangle::move(double dx, double dy)
+{
+  point1_.x += dx;
+  point1_.y += dy;
+  point2_.x += dx;
+  point2_.y += dy;
+  point3_.x += dx;
+  point3_.y += dy;
+  pos_.x += dx;
+  pos_.y += dy;
+}
