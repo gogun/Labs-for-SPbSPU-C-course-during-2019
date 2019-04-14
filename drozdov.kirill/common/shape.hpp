6e55d80924a5ae0fd0450d551408f96110f11e1a
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-14 15:51:06 +0300

    Добавлено пространство имен для абстрактного класса shape.

--- drozdov.kirill/common/shape.hpp
+++ drozdov.kirill/common/shape.hpp
@@ -3,17 +3,20 @@
 
 #include "base-types.hpp"
 
-class Shape
+namespace drozdov
 {
-  public:
-    virtual ~Shape() = default;
+  class Shape
+  {
+    public:
+      virtual ~Shape() = default;
 
-    virtual void printData() const = 0;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void scale(double factor) = 0;
+      virtual void printData() const = 0;
+      virtual double getArea() const = 0;
+      virtual rectangle_t getFrameRect() const = 0;
+      virtual void scale(double factor) = 0;
 
-    virtual void move (double dx, double dy) = 0;
-    virtual void move (const point_t &dot) = 0;
-};
+      virtual void move (double dx, double dy) = 0;
+      virtual void move (const point_t &dot) = 0;
+  };
+}
 #endif
