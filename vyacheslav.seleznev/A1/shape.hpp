ba85601426b135149d87e6497f68c93a3ee00bd0
Vyacheslav Seleznev <vya4eslav.seleznev@gmail.com>
2019-04-14 20:13:42 +0300

    Whitespace

--- vyacheslav.seleznev/A1/shape.hpp
+++ vyacheslav.seleznev/A1/shape.hpp
@@ -6,24 +6,24 @@
 class Shape
 {
   public:
-  Shape(point_t p): centre_(p)
-  {
-  }
-  virtual ~Shape() = default;
-  virtual double getArea() const = 0;
-  virtual rectangle_t getFrameRect() const = 0;
-  void move(const point_t newPoint)
-  {
-    centre_ = newPoint;
-  }
-  void move(const double dx, const double dy)
-  {
-    centre_.x += dx;
-    centre_.y += dy;
-  }
+    Shape(point_t p): centre_(p)
+    {
+    }
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    void move(const point_t newPoint)
+    {
+      centre_ = newPoint;
+    }
+    void move(const double dx, const double dy)
+    {
+      centre_.x += dx;
+      centre_.y += dy;
+    }
 
   protected:
-  point_t centre_;
+    point_t centre_;
 };
 
 #endif
