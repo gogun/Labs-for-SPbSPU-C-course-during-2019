2270f80de2f6e3ff3b148712db526eb611fadc6f
Wiserax.Kar.av <eskercorps@gmail.com>
2019-04-14 05:08:49 +0300

    Initial commit

---
+++ karpenko.alexey/A2/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+const double scaleRate1 = 3.42;
+const double scaleRate2 = 2.93;
+
+void printRectangle(const karpenko::Shape & shape)
+{
+  const karpenko::rectangle_t frameRect = shape.getFrameRect();
+  std::cout << "Width is " << frameRect.width;
+  std::cout << "; Height is " << frameRect.height << ";\n";
+  std::cout << "Center on [" << frameRect.pos.x << ";";
+  std::cout << frameRect.pos.y << "];\n";
+  std::cout << "Area of rectangle is " << shape.getArea() << ";\n\n";
+}
+
+void printCircle(const karpenko::Shape & shape)
+{
+  karpenko::rectangle_t frameRect = shape.getFrameRect();
+  std::cout << "Width of frame circle rectangle is " << frameRect.width;
+  std::cout << "; Height of frame circle rectangle is " << frameRect.height << ";\n";
+  std::cout << "Center on [" << frameRect.pos.x << ";" << frameRect.pos.y << "];\n";
+  std::cout << "Area of circle is " << shape.getArea() << ";\n\n";
+}
+
+int main()
+{
+  karpenko::Rectangle rectangle({1, 4, {3, 4}});
+
+  std::cout << "Rectangle demonstration started.\n\n";
+  printRectangle(rectangle);
+  std::cout << "Moving to coordinates...\n";
+  rectangle.move(5, 6);
+  printRectangle(rectangle);
+  std::cout << "Moving to point...\n";
+  rectangle.move({3, 3});
+  printRectangle(rectangle);
+
+  std::cout << "Applies scaling with coeficcent = "
+      << scaleRate1 << "\n";
+  rectangle.scale(scaleRate1);
+  printRectangle(rectangle);
+  std::cout << "End program for Rectangle;\n\n";
+
+
+  karpenko::Circle circle({-5, {1, 2}});
+
+  std::cout << "Circle demonstration started.\n";
+  printCircle(circle);
+  std::cout << "Moving to coordinates...\n";
+  circle.move(5, 6);
+  printCircle(circle);
+  std::cout << "Moving to point...\n";
+  circle.move({3, 3});
+  printCircle(circle);
+
+  std::cout << "Applies scaling with coeficcent = "
+      << scaleRate2 << "\n";
+  circle.scale(scaleRate2);
+  printCircle(circle);
+  std::cout << "End program for Circle;\n"; 
+  system("pause");
+  return 0;
+}
---
+++ karpenko.alexey/A2/test-main.cpp
@@ -0,0 +1,81 @@
+#define BOOST_AUTO_MODULE A2_TEST
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <stdexcept>
+
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+
+const double LAPSE = 0.01;
+
+BOOST_AUTO_TEST_SUITE(testSuiteShapes)
+
+BOOST_AUTO_TEST_CASE(afterMoveTestingRectangle)
+{
+  karpenko::Rectangle rectangle({3, 6, {2, 7}});
+  const karpenko::rectangle_t originalRect = rectangle.getFrameRect();
+  const double originalSquare = rectangle.getArea();
+
+  rectangle.move(5, 6);
+  const karpenko::rectangle_t afterMoveData = rectangle.getFrameRect();
+  BOOST_CHECK_CLOSE(rectangle.getArea, originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
+
+  rectangle.move({3, 2});
+  const karpenko::rectangle_t afterMoveData = rectangle.getFrameRect();
+  BOOST_CHECK_CLOSE(rectangle.getArea, originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
+}
+
+BOOST_AUTO_TEST_CASE(afterMoveTestingCircle)
+{
+  karpenko::Circle circle({6, {2, 7}});
+  const karpenko::Circle originalFrame = circle.getFrameRect();
+  const double originalSquare = circle.getArea();
+
+  rectangle.move(4, 2);
+  const karpenko::rectangle_t afterMoveData = circle.getFrameRect();
+  BOOST_CHECK_CLOSE(circle.getArea, originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
+
+  rectangle.move({3, 7});
+  const karpenko::rectangle_t afterMoveData = circle.getFrameRect();
+  BOOST_CHECK_CLOSE(circle.getArea, originalSquare);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width);
+}
+
+BOOST_AUTO_TEST_CASE(quadroMetamaorphoseRectangleTesting)
+{
+  karpenko::Rectangle rectangle({3, 6, {2, 7}});
+  const double originalSquare = rectangle.getArea();
+
+  const double scaleRatio = 3.3;
+  rectangle.scale(scaleRatio);
+  BOOST_CHECK_CLOSE(rectangle.getArea, originalSquare * scaleRatio * scaleRatio, LAPSE)
+}
+
+BOOST_AUTO_TEST_CASE(quadroMetamaorphoseCircleTesting)
+{
+  karpenko::Circle circle({6, {2, 7}});
+  const double originalSquare = circle.getArea();
+
+  const double scaleRatio = 3.3;
+  rectangle.scale(scaleRatio);
+  BOOST_CHECK_CLOSE(circle.getArea, originalSquare * scaleRatio * scaleRatio, LAPSE)
+}
+
+BOOST_AUTO_TEST_CASE(argumentTesting)
+{
+  BOOST_CHECK_THROW(karpenko::Rectangle({-2, 7, {2, 2}}), std::invalid_argument);
+  BOOST_CHECK_THROW(karpenko::Rectangle({2, -7, {2, 2}}), std::invalid_argument);
+
+  BOOST_CHECK_THROW(karpenko::Circle(-5, {2, 6}}, std::invalid_argument);
+
+
+BOOST_AUTO_TEST_SUITE_END
