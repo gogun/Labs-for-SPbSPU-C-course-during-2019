766f56818f068dd07787c3e4b1826bfc17b643c3
drozdov.kk <drozdovkirkon@gmail.com>
2019-04-14 15:47:12 +0300

    Добавлен тест для проверки неизменности ширины и высоты, площади фигур при перемещении.

---
+++ drozdov.kirill/A2/test-main.cpp
@@ -0,0 +1,49 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+const double calculationError = 0.01;
+
+BOOST_AUTO_TEST_SUITE(A2TestRectangle)
+
+  BOOST_AUTO_TEST_CASE(testCorrectnessAfterMove)
+  {
+    drozdov::Rectangle rectangle(5.0, 3.0, {4.3, 1.0});
+    const rectangle_t beforeMoveRect = rectangle.getFrameRect();
+    const double areaBeforeMove = rectangle.getArea();
+
+    rectangle.move(2.4, 5.6);
+
+    const rectangle_t afterMoveRect = rectangle.getFrameRect();
+    const double areaAfterMove = rectangle.getArea();
+
+    BOOST_CHECK_CLOSE(beforeMoveRect.width, afterMoveRect.width, calculationError);
+    BOOST_CHECK_CLOSE(beforeMoveRect.height, afterMoveRect.height, calculationError);
+    BOOST_CHECK_CLOSE(areaBeforeMove, areaAfterMove, calculationError);
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(A2TestCircle)
+
+  BOOST_AUTO_TEST_CASE(testCorrectnessAfterMove)
+  {
+    drozdov::Circle circle(5.0, {4.3, 1.0});
+    const double radiusBeforeMove = circle.getRadius();
+    const double areaBeforeMove = circle.getArea();
+
+    circle.move(2.4, 5.6);
+
+    const double radiusAfterMove = circle.getRadius();
+    const double areaAfterMove = circle.getArea();
+
+    BOOST_CHECK_CLOSE(radiusBeforeMove, radiusAfterMove, calculationError);
+    BOOST_CHECK_CLOSE(radiusBeforeMove, radiusAfterMove, calculationError);
+    BOOST_CHECK_CLOSE(areaBeforeMove, areaAfterMove, calculationError);
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
