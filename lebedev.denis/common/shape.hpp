d69b7373d931f8e89ef2abc42ff652902d79a6c8
Lebedev Denis <lebedev.dm00@mail.ru>
2019-04-15 17:29:19 +0000

    Правила оформления

--- lebedev.denis/common/shape.hpp
+++ lebedev.denis/common/shape.hpp
@@ -7,12 +7,12 @@ namespace lebedev
   class Shape
   {
     public:
-virtual ~Shape() = default;
-virtual double getArea() const = 0;
-virtual rectangle_t getFrameRect() const = 0;
-virtual void move(const double x, const double y) = 0;
-virtual void move(const point_t &new_dot) = 0;
-virtual void scale(const double multiplier) = 0;
+      virtual ~Shape() = default;
+      virtual double getArea() const = 0;
+      virtual rectangle_t getFrameRect() const = 0;
+      virtual void move(const double x, const double y) = 0;
+      virtual void move(const point_t &new_dot) = 0;
+      virtual void scale(const double multiplier) = 0;
   };
 }
 #endif
