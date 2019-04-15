8724863f98b3748643f3eddee6a38b1e4b9b2d1e
Baboshina Elizaveta <blizababoshina200@gmail.com>
2019-04-15 19:28:33 +0000

    Changed polymorphism

--- baboshina.elizaveta/A1/main.cpp
+++ baboshina.elizaveta/A1/main.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <cassert>
 #include "rectangle.hpp"
 #include "circle.hpp"
+#include "triangle.hpp"
 
-void test(Shape &shape)
+void test(Shape * shape)
 {
-  shape.getInfo();
-  std::cout << "The area of shape is: " << shape.getArea() << std::endl;
-  std::cout << "Width of frame rectangle is: " << shape.getFrameRect().width << std::endl;
-  std::cout << "Height of frame rectangle is: " << shape.getFrameRect().height << std::endl;
-  shape.move({ 1,1 });
-  shape.getInfo();
-  shape.move(4, 5);
-  shape.getInfo();
+  assert(shape != nullptr);
+  shape->getInfo();
+  std::cout << "The area of shape is: " << shape->getArea() << std::endl;
+  std::cout << "Width of frame rectangle is: " << shape->getFrameRect().width << std::endl;
+  std::cout << "Height of frame rectangle is: " << shape->getFrameRect().height << std::endl;
+  shape->move({1, 1});
+  shape->getInfo();
+  shape->move(4, 5);
+  shape->getInfo();
 }
 
 int main()
 {
-  Circle circle({ 3,4 }, 6);
+  Circle circle({3, 4}, 6);
   std::cout << "\n---Information about circle---" << std::endl;
-  test(circle);
+  test(&circle);
 
-  Rectangle rectangle({ 5,5 }, 20, 5);
+  Rectangle rectangle({5, 5}, 20, 5);
   std::cout << "\n---Information about rectangle---" << std::endl;
-  test(rectangle);
+  test(&rectangle);
+
+  Triangle triangle({1, 1}, {3, 4}, {-4, 4});
+  std::cout << "\n---Information about triangle---" << std::endl;
+  test(&triangle);
+  
   return 0;
 }
