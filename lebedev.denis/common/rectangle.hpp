8a9278c958d6427a6b1d578a979e420047670637
Lebedev Denis <lebedev.dm00@mail.ru>
2019-04-15 17:32:46 +0000

    Правила оформления

--- lebedev.denis/common/rectangle.hpp
+++ lebedev.denis/common/rectangle.hpp
@@ -7,18 +7,18 @@ namespace lebedev
   class Rectangle : public Shape
   {
     public:
-	  Rectangle(double height, double widht, const point_t &pos);
+      Rectangle(double height, double widht, const point_t &pos);
 
-	  double getArea() const override;
-	  rectangle_t getFrameRect() const override;
-	  void move(const double x, const double y) override;
-	  void move(const point_t &new_point) override;
-	  void scale(const double multiplier) override;
+      double getArea() const override;
+      rectangle_t getFrameRect() const override;
+      void move(const double x, const double y) override;
+      void move(const point_t &new_point) override;
+      void scale(const double multiplier) override;
 
     private:
-	  double m_height;
-	  double m_width;
-	  point_t m_pos;
+      double m_height;
+      double m_width;
+      point_t m_pos;
   };
 }
 #endif
