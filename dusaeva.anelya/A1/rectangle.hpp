d79c231579dacb5aee5c5a091a403ce3203c2869
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-17 01:02:23 +0300

    незначительные улучшения

--- dusaeva.anelya/A1/rectangle.hpp
+++ dusaeva.anelya/A1/rectangle.hpp
@@ -1,11 +1,13 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
+
 #include "shape.hpp"
 
 class Rectangle : public Shape
 {
 public:
   Rectangle(const point_t &, double, double);
+  
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(double, double) override;
