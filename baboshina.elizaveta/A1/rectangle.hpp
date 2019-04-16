0f1a4f7eb003ec88971a29a4422c77e33e3dde7a
Baboshina Elizaveta <blizababoshina200@gmail.com>
2019-04-16 19:59:23 +0000

    Update rectangle.hpp

--- baboshina.elizaveta/A1/rectangle.hpp
+++ baboshina.elizaveta/A1/rectangle.hpp
@@ -6,7 +6,7 @@
 class Rectangle : public Shape
 {
 public:
-  Rectangle(const point_t pos, const double width, const double height);
+  Rectangle(const double width, const double height, const point_t pos);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(const point_t point) override;
