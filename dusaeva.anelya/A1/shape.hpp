37256ada523b6c45a235af0c35ff1aee212d033a
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-17 01:05:21 +0300

    незначительные улучшения

--- dusaeva.anelya/A1/shape.hpp
+++ dusaeva.anelya/A1/shape.hpp
@@ -1,14 +1,16 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
+
 #include "base-types.hpp"
 
 class Shape
 {
 public:
   virtual ~Shape() = default;
+ 
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
-  virtual void  move(double, double ) = 0;
+  virtual void  move(double, double) = 0;
   virtual void  move(const point_t &) = 0;
   virtual void printInfo() const = 0;
 };
