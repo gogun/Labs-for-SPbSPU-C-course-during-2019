c73d32078fc6fcb4a4b847f36191ec5e6f2afb01
Wiserax.Kar.av <eskercorps@gmail.com>
2019-04-16 20:04:53 +0300

    Removed ident after BOOST_AUTO_TEST_SUITE

--- karpenko.alexey/A2/test-main.cpp
+++ karpenko.alexey/A2/test-main.cpp
@@ -10,81 +10,81 @@
 
 BOOST_AUTO_TEST_SUITE(testSuiteShapes)
 
-  const double LAPSE = 0.01;
+const double LAPSE = 0.01;
 
-  BOOST_AUTO_TEST_CASE(afterMoveTestingRectangle)
-  {
-    karpenko::Rectangle rectangle({3.5, 6.3, {2.0, 7.0}});
-    const karpenko::rectangle_t originalRect = rectangle.getFrameRect();
-    const double originalSquare = rectangle.getArea();
+BOOST_AUTO_TEST_CASE(afterMoveTestingRectangle)
+{
+  karpenko::Rectangle rectangle({3.5, 6.3, {2.0, 7.0}});
+  const karpenko::rectangle_t originalRect = rectangle.getFrameRect();
+  const double originalSquare = rectangle.getArea();
 
-    rectangle.move(5.0, 6.0);
-    karpenko::rectangle_t afterMoveData = rectangle.getFrameRect();
-    BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
+  rectangle.move(5.0, 6.0);
+  karpenko::rectangle_t afterMoveData = rectangle.getFrameRect();
+  BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
 
-    rectangle.move({3.0, 2.0});
-    afterMoveData = rectangle.getFrameRect();
-    BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
-  }
+  rectangle.move({3.0, 2.0});
+  afterMoveData = rectangle.getFrameRect();
+  BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalRect.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalRect.width, LAPSE);
+}
 
-  BOOST_AUTO_TEST_CASE(afterMoveTestingCircle)
-  {
-    karpenko::Circle circle({6.5, {2.0, 7.0}});
-    const karpenko::rectangle_t originalFrame = circle.getFrameRect();
-    const double originalSquare = circle.getArea();
+BOOST_AUTO_TEST_CASE(afterMoveTestingCircle)
+{
+  karpenko::Circle circle({6.5, {2.0, 7.0}});
+  const karpenko::rectangle_t originalFrame = circle.getFrameRect();
+  const double originalSquare = circle.getArea();
 
-    circle.move(4.0, 2.4);
-    karpenko::rectangle_t afterMoveData = circle.getFrameRect();
-    BOOST_CHECK_CLOSE(circle.getArea(), originalSquare, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.height, originalFrame.height, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.width, originalFrame.width, LAPSE);
+  circle.move(4.0, 2.4);
+  karpenko::rectangle_t afterMoveData = circle.getFrameRect();
+  BOOST_CHECK_CLOSE(circle.getArea(), originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalFrame.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalFrame.width, LAPSE);
 
-    circle.move({3.0, 7.0});
-    afterMoveData = circle.getFrameRect();
-    BOOST_CHECK_CLOSE(circle.getArea(), originalSquare, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.height, originalFrame.height, LAPSE);
-    BOOST_CHECK_CLOSE(afterMoveData.width, originalFrame.width, LAPSE);
-  }
+  circle.move({3.0, 7.0});
+  afterMoveData = circle.getFrameRect();
+  BOOST_CHECK_CLOSE(circle.getArea(), originalSquare, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.height, originalFrame.height, LAPSE);
+  BOOST_CHECK_CLOSE(afterMoveData.width, originalFrame.width, LAPSE);
+}
 
-  BOOST_AUTO_TEST_CASE(quadroMetamaorphoseRectangleTesting)
-  {
-    karpenko::Rectangle rectangle({3.5, 6.3, {2.0, 7.0}});
-    const double originalSquare = rectangle.getArea();
+BOOST_AUTO_TEST_CASE(quadroMetamaorphoseRectangleTesting)
+{
+  karpenko::Rectangle rectangle({3.5, 6.3, {2.0, 7.0}});
+  const double originalSquare = rectangle.getArea();
 
-    const double scaleRatio = 3.3;
-    rectangle.scale(scaleRatio);
-    BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare * scaleRatio * scaleRatio, LAPSE);
-  }
+  const double scaleRatio = 3.3;
+  rectangle.scale(scaleRatio);
+  BOOST_CHECK_CLOSE(rectangle.getArea(), originalSquare * scaleRatio * scaleRatio, LAPSE);
+}
 
-  BOOST_AUTO_TEST_CASE(quadroMetamaorphoseCircleTesting)
-  {
-    karpenko::Circle circle({6.5, {2.0, 7.0}});
-    const double originalSquare = circle.getArea();
+BOOST_AUTO_TEST_CASE(quadroMetamaorphoseCircleTesting)
+{
+  karpenko::Circle circle({6.5, {2.0, 7.0}});
+  const double originalSquare = circle.getArea();
 
-    const double scaleRatio = 3.3;
-    circle.scale(scaleRatio);
-    BOOST_CHECK_CLOSE(circle.getArea(), originalSquare * scaleRatio * scaleRatio, LAPSE);
-  }
+  const double scaleRatio = 3.3;
+  circle.scale(scaleRatio);
+  BOOST_CHECK_CLOSE(circle.getArea(), originalSquare * scaleRatio * scaleRatio, LAPSE);
+}
 
-  BOOST_AUTO_TEST_CASE(argumentTestingRectangle)
-  {
-    BOOST_CHECK_THROW(karpenko::Rectangle({-2.0, 7.0, {2.0, 2.0}}), std::invalid_argument);
-    BOOST_CHECK_THROW(karpenko::Rectangle({2.0, -7.0, {2.0, 2.0}}), std::invalid_argument);
+BOOST_AUTO_TEST_CASE(argumentTestingRectangle)
+{
+  BOOST_CHECK_THROW(karpenko::Rectangle({-2.0, 7.0, {2.0, 2.0}}), std::invalid_argument);
+  BOOST_CHECK_THROW(karpenko::Rectangle({2.0, -7.0, {2.0, 2.0}}), std::invalid_argument);
 
-    karpenko::Rectangle rectangle({2.0, 3.0, {2.0, 2.0}});
-    BOOST_CHECK_THROW(rectangle.scale(-2), std::invalid_argument);
-  }
+  karpenko::Rectangle rectangle({2.0, 3.0, {2.0, 2.0}});
+  BOOST_CHECK_THROW(rectangle.scale(-2), std::invalid_argument);
+}
 
-  BOOST_AUTO_TEST_CASE(argumentTestingCircle)
-  {
-    BOOST_CHECK_THROW(karpenko::Circle(-5.0, {2.0, 6.0}), std::invalid_argument);
+BOOST_AUTO_TEST_CASE(argumentTestingCircle)
+{
+  BOOST_CHECK_THROW(karpenko::Circle(-5.0, {2.0, 6.0}), std::invalid_argument);
 
-    karpenko::Circle circle({6.5, {2.0, 7.0}});
-    BOOST_CHECK_THROW(circle.scale(-1), std::invalid_argument);
-  }
+  karpenko::Circle circle({6.5, {2.0, 7.0}});
+  BOOST_CHECK_THROW(circle.scale(-1), std::invalid_argument);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
