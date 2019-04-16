75014c8b3d320e2e92d35f2a343d068fd04b03a0
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-16 16:16:37 +0000

    Fixed the design

--- goncharova.alexandra/A1/circle.hpp
+++ goncharova.alexandra/A1/circle.hpp
@@ -5,15 +5,17 @@
 
 class Circle : public Shape
 {
-  public:
-  point_t center_;
-  double radius_;
+public:
   Circle(const point_t &center, double radius);
   double getArea() const;
   rectangle_t getFrameRect() const;
   void move(const point_t &newPoint);
   void move(double dx, double dy);
   void inform() const;
+
+private:
+  point_t center_;
+  double radius_;
 };
 
 #endif
