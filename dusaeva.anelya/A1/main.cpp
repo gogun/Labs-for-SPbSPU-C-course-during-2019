082d549fc3525d118b5e2def1159cbc96e99f6de
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-17 00:56:43 +0300

    заменены endl

--- dusaeva.anelya/A1/main.cpp
+++ dusaeva.anelya/A1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "rectangle.hpp"
 #include "circle.hpp"
 #include "triangle.hpp"
@@ -9,42 +10,40 @@ int main()
   Rectangle objRect({5.0, 6.0}, 7.0, 1.0);
   Shape *shape = &objRect;
 
-  std::cout << "Rectangle:" << std::endl;
+  std::cout << "Rectangle:\n";
   shape->printInfo();
   shape->move(5.1, 6.2);
-  std::cout << "Coordinate shift:" << std::endl;
+  std::cout << "Coordinate shift:\n";
   shape->printInfo();
   shape->move({0.0, 3.0});
-  std::cout << "Point shift:" << std::endl;
+  std::cout << "Point shift:\n";
   shape->printInfo();
-  std::cout << std::endl;
 
   //Circle
   Circle objCirc({1.0, 5.0}, 6.0);
   shape = &objCirc;
 
-  std::cout << "Circle:" << std::endl;
+  std::cout << "Circle:\n";
   shape->printInfo();
   shape->move(1.0, 2.0);
-  std::cout << "Coordinate shift:" << std::endl;
+  std::cout << "Coordinate shift:\n";
   shape->printInfo();
   shape->move({7.8, -9.0});
-  std::cout << "Point shift:" << std::endl;
+  std::cout << "Point shift:\n";
   shape->printInfo();
-  std::cout << std::endl;
+
   //Triangle
   Triangle objTriangle({4.0, 10.0}, {15.0, 6.0}, {1.0, 1.0});
   shape = &objTriangle;
 
-  std::cout << "Triangle:" << std::endl;
+  std::cout << "Triangle:\n";
   shape->printInfo();
   shape->move(5.4, 7.6);
-  std::cout << "Coordinate shift:" << std::endl;
+  std::cout << "Coordinate shift:\n";
   shape->printInfo();
   shape->move({1.0, 3.0});
-  std::cout << "Point shift:" << std::endl;
+  std::cout << "Point shift:\n";
   shape->printInfo();
-  std::cout << std::endl;
 
   return 0;
 }
