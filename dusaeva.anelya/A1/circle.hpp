6caa08eb79bb99936e22cb66cf29f6137a3eed8e
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-17 01:01:29 +0300

    добавлены строки для более удобного чтения

--- dusaeva.anelya/A1/circle.hpp
+++ dusaeva.anelya/A1/circle.hpp
@@ -1,11 +1,13 @@
 #ifndef CIRCLE_HPP
 #define CIRCLE_HPP
+
 #include "shape.hpp"
 
 class Circle : public Shape
 {
 public:
   Circle(const point_t &, double);
+  
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(double, double) override;
