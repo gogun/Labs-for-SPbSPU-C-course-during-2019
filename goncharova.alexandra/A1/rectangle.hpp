d02ae4222705b1f0f9759a76952962890c629419
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-16 16:19:56 +0000

    Fixed the design

--- goncharova.alexandra/A1/rectangle.hpp
+++ goncharova.alexandra/A1/rectangle.hpp
@@ -6,15 +6,16 @@
 class Rectangle : public Shape
 {
 public:
-  point_t center_;
-  double width_;
-  double height_;
   Rectangle(const point_t &center, double width, double height);
   double getArea() const;
   rectangle_t getFrameRect() const;
   void move(const point_t &newPoint);
   void move(double dx, double dy);
   void inform() const;
+private:
+  point_t center_;
+  double width_;
+  double height_;
 };
 
 #endif
