f16f90ca900193e66668298ff4fe4c041762d756
kuxov.grigoriy <kuxov@outlook.com>
2019-04-17 20:44:53 +0000

    поддержка треугольников

--- kuxov.grigoriy/A1/main.cpp
+++ kuxov.grigoriy/A1/main.cpp
@@ -2,6 +2,7 @@
 
 #include "rectangle.hpp"
 #include "circle.hpp"
+#include "triangle.hpp"
 
 int main()
 {
@@ -26,6 +27,18 @@ int main()
   shapePtr->print();
   std::cout << "2) Moving to the point (" << 8 << "; " << 8 << ")\n";
   shapePtr->move({ 8, 8 });
+  shapePtr->print();
+  
+  Triangle triangle({ 2.0, 2.0 }, { 7.0, 7.0 }, { 3.0, 3.0 });
+  shapePtr = &triangle;
+  std::cout << "\nFOR TRIANGLE\n";
+  shapePtr->print();
+  std::cout << "\n1) Moving along 0x " << 4 << " and 0y " << 4 << std::endl;
+  shapePtr->move(4.0, 4.0);
+  shapePtr->print();
+  std::cout << "\n2) Moving to the point (" << 5 << ";" << 5 << ")\n";
+  const point_t nPosTrian = { 5.0, 5.0 };
+  shapePtr->move(nPosTrian);
   shapePtr->print();
 
   return 0;
