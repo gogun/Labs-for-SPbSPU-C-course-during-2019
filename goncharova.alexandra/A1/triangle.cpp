17a63405ed61b1a0923e0a562e3a106cc0ee26e7
Alexandra Goncharova <alexandra.andronova.99@mail.ru>
2019-04-17 19:28:44 +0000

    Changed assert, changed properties and parametrs, fixed the design

--- goncharova.alexandra/A1/triangle.cpp
+++ goncharova.alexandra/A1/triangle.cpp
@@ -3,28 +3,28 @@
 #include <cmath>
 #include <cassert>
 
-Triangle::Triangle(const point_t &center, const point_t &pointA, const point_t &pointB, const point_t &pointC):
+Triangle::Triangle(const point_t &center, const point_t &pointA, const point_t &pointB) :
   center_(center),
   pointA_(pointA),
   pointB_(pointB),
-  pointC_(pointC),
-
-  a_(sqrt(pow((pointA.x - pointB.x), 2) + pow((pointA.y - pointB.y), 2))),
-  b_(sqrt(pow((pointB.x - pointC.x), 2) + pow((pointB.y - pointC.y), 2))),
-  c_(sqrt(pow((pointC.x - pointA.x), 2) + pow((pointC.y - pointA.y), 2)))
-
+  pointC_.x(3*center_.x - pointB_.x - pointA_.x),
+  pointC_.y(3*center_.y - pointB_.y - pointA_.y)
 {
-  assert((a_ > 0.0) && (b_ > 0.0) && (c_ > 0.0) && (a_ + b_ > c_) && (a_ + c_ > b_) && (b_ + c_ > a_));
+  assert(fabs((pointB.x - pointA.x)*(center_.y - pointA_.y) - (center_.x - pointA.x)*(pointB.y - pointA.y)) > pow(10, -8));
 }
 
 double Triangle::getArea() const
 {
+  double a_ = (sqrt(pow((pointA.x - pointB.x), 2) + pow((pointA.y - pointB.y), 2)));
+  double b_ = (sqrt(pow((pointB.x - pointC.x), 2) + pow((pointB.y - pointC.y), 2)));
+  double c_ = (sqrt(pow((pointC.x - pointA.x), 2) + pow((pointC.y - pointA.y), 2)));
+  
   double p = (a_ + b_ + c_) / 2;
   double square = sqrt(p * (p - a_) * (p - b_) * (p - c_));
   return square;
 }
 
-rectangle_t Triangle::getFrameRect()const
+rectangle_t Triangle::getFrameRect() const
 {
   double width = (std::max(pointA_.x, std::max(pointB_.x, pointC_.x)))
       - (std::min(pointA_.x, std::min(pointB_.x, pointC_.x)));
@@ -35,7 +35,7 @@ rectangle_t Triangle::getFrameRect()const
   trCenter.x = ((std::min(pointA_.x, std::min(pointB_.x, pointC_.x)) + width / 2));
   trCenter.y = (((std::min(pointA_.y, std::min(pointB_.y, pointC_.y)))) + height / 2);
 
-  return{trCenter, width, height};
+  return {trCenter, width, height};
 }
 
 void Triangle::move(const double dx, const double dy)
