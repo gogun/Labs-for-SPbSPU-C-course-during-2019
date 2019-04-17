54f194e9a2b0f440ad0f06b6c8240de6c3480409
kuxov.grigoriy <kuxov@outlook.com>
2019-04-17 23:04:53 +0000

    Убрал const,

--- kuxov.grigoriy/A1/main.cpp
+++ kuxov.grigoriy/A1/main.cpp
@@ -6,27 +6,27 @@
 
 int main()
 {
-  Rectangle rectangle({ 3, 3, {1, 1} });
+  Rectangle rectangle({ 3.0, 3.0, {1.0, 1.0} });
   Shape * shapePtr = &rectangle;
   std::cout << "FOR RECTANGLE\n";
   shapePtr->print();
   std::cout << "1) Moving along 0x " << 7 << " and 0y " << 7 << std::endl;
-  shapePtr->move(7, 7);
+  shapePtr->move(7.0, 7.0);
   shapePtr->print();
   std::cout << "2) Moving to the point (" << 9 << "; " << 9 << ")\n";
-  shapePtr->move({ 9, 9 });
+  shapePtr->move({ 9.0, 9.0 });
   shapePtr->print();
   std::cout << std::endl;
 
-  Circle circle(5, { 3, 3 });
+  Circle circle(5.0, { 3.0, 3.0 });
   shapePtr = &circle;
   std::cout << "FOR CIRCLE\n";
   shapePtr->print();
   std::cout << "1) Moving along 0x " << 9 << " and 0y " << 9 << std::endl;
-  shapePtr->move(9, 9);
+  shapePtr->move(9.0, 9.0);
   shapePtr->print();
   std::cout << "2) Moving to the point (" << 8 << "; " << 8 << ")\n";
-  shapePtr->move({ 8, 8 });
+  shapePtr->move({ 8.0, 8.0 });
   shapePtr->print();
 
   Triangle triangle({ 2.0, 2.0 }, { 7.0, 7.0 }, { 3.0, 3.0 });
@@ -37,7 +37,7 @@ int main()
   shapePtr->move(4.0, 4.0);
   shapePtr->print();
   std::cout << "\n2) Moving to the point (" << 5 << ";" << 5 << ")\n";
-  const point_t nPosTrian = { 5.0, 5.0 };
+  point_t nPosTrian = { 5.0, 5.0 };
   shapePtr->move(nPosTrian);
   shapePtr->print();
 
