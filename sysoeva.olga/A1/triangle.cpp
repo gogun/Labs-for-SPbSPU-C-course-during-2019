0241deb6087ca61199b575f55f44a13f37afb74c
olyaasysa <olyaasysa@gmail.com>
2019-04-18 00:04:18 +0300

    Added new functions to reorganize code

--- sysoeva.olga/A1/triangle.cpp
+++ sysoeva.olga/A1/triangle.cpp
@@ -7,60 +7,61 @@
 Triangle::Triangle(point_t vertex1, point_t vertex2, point_t vertex3):
   vertex_1(vertex1),
   vertex_2(vertex2),
-  vertex_3(vertex3)
+  vertex_3(vertex3),
+  center_(getCenter())
 {
-  assert((vertex_1.x - vertex_2.x)/(vertex_3.y - vertex_2.y) != (vertex_1.y - vertex_2.y)/(vertex_3.x - vertex_2.x));
-  center_ = getCenter();
+  assert((vertex_1.x - vertex_2.x) / (vertex_3.y - vertex_2.y) != (vertex_1.y - vertex_2.y) / (vertex_3.x - vertex_2.x));
 }
 
 point_t Triangle::getCenter() const
 {
-  return {(vertex_1.x + vertex_2.x + vertex_3.x)/3, (vertex_1.y + vertex_2.y + vertex_3.y)/3};
+  return {(vertex_1.x + vertex_2.x + vertex_3.x) / 3, (vertex_1.y + vertex_2.y + vertex_3.y) / 3};
+}
+
+double Triangle::getVector(const point_t & vertex1, const point_t & vertex2) const
+{
+  return sqrt(pow(vertex2.x - vertex1.x, 2) + pow(vertex2.y - vertex1.y, 2));
 }
 
 double Triangle::getArea() const
 {
-  double side_a = sqrt(pow(vertex_2.x - vertex_1.x, 2) + pow(vertex_2.y - vertex_1.y, 2));
-  double side_b = sqrt(pow(vertex_3.x - vertex_2.x, 2) + pow(vertex_3.y - vertex_2.y, 2));
-  double side_c = sqrt(pow(vertex_1.x - vertex_3.x, 2) + pow(vertex_1.y - vertex_3.y, 2));
+  double side_a = getVector(vertex_1, vertex_2);
+  double side_b = getVector(vertex_2, vertex_3);
+  double side_c = getVector(vertex_3, vertex_1);
   double p = 0.5 * (side_a + side_b + side_c);
   return sqrt(p * (p - side_a) * (p - side_b) * (p - side_c));
 }
 
 rectangle_t Triangle::getFrameRect() const
 {
-  double width = std::max(std::max(vertex_1.x, vertex_2.x), vertex_3.x)
-    - std::min(std::min(vertex_1.x, vertex_2.x), vertex_3.x);
-  double height = std::max(std::max(vertex_1.y, vertex_2.y), vertex_3.y)
-    - std::min(std::min(vertex_1.y, vertex_2.y), vertex_3.y);
-  point_t center = {std::min(std::min(vertex_1.x, vertex_2.x), vertex_3.x) + (width/2),
-                   std::min(std::min(vertex_1.y, vertex_2.y), vertex_3.y) + (height/2)};
+  double width = std::max({vertex_1.x, vertex_2.x, vertex_3.x}) - std::min({vertex_1.x, vertex_2.x, vertex_3.x});
+  double height = std::max({vertex_1.y, vertex_2.y, vertex_3.y}) - std::min({vertex_1.y, vertex_2.y, vertex_3.y});
+  point_t center = {std::min({vertex_1.x, vertex_2.x, vertex_3.x}) + (width/2),
+                    std::min({vertex_1.y, vertex_2.y, vertex_3.y}) + (height/2)};
   return {width, height, center};
+}
+
+void Triangle::changeVertex(double dx, double dy)
+{
+  point_t * mas[] = {&vertex_1, &vertex_2, &vertex_3};
+  for (size_t i = 0; i < sizeof(mas) / sizeof(mas[0]); i++)
+  {
+    * mas[i] = {mas[i]->x + dx, mas[i]->y + dy};
+  }
 }
 
 void Triangle::move(const point_t & point)
 {
-  double dx = center_.x - point.x;
-  double dy = center_.y - point.y;
+  double dx = point.x - center_.x;
+  double dy = point.y - center_.y ;
   center_ = point;
-  vertex_1.x -= dx;
-  vertex_2.x -= dx;
-  vertex_3.x -= dx;
-  vertex_1.y -= dy;
-  vertex_2.y -= dy;
-  vertex_3.y -= dy;
+  changeVertex(dx, dy);
 }
 
 void Triangle::move(double dx, double dy)
 {
-  center_.x += dx;
-  center_.y += dy;
-  vertex_1.x += dx;
-  vertex_2.x += dx;
-  vertex_3.x += dx;
-  vertex_1.y += dy;
-  vertex_2.y += dy;
-  vertex_3.y += dy;
+  center_ = {center_.x + dx, center_.y + dy};
+  changeVertex(dx, dy);
 }
 
 void Triangle::showCoord()
@@ -73,7 +74,5 @@ void Triangle::showCoord()
 
 void Triangle::printCenter()
 {
-  double x = center_.x;
-  double y = center_.y;
-  std::cout << "center (" << x << "," << y << ")" << '\n';
+  std::cout << "center (" << center_.x << "," << center_.y << ")" << '\n';
 }
--- sysoeva.olga/A1/triangle.hpp
+++ sysoeva.olga/A1/triangle.hpp
@@ -9,6 +9,7 @@ public:
   Triangle(point_t vertex1, point_t vertex2, point_t vertex3);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
+  void changeVertex(double dx, double dy);
   void move(const point_t & point) override;
   void move(double dx, double dy) override;
   void showCoord();
@@ -19,6 +20,7 @@ private:
   point_t vertex_3;
   point_t center_;
   point_t getCenter() const;
+  double getVector(const point_t & vertex1, const point_t & vertex2) const;
 };
 
 #endif
