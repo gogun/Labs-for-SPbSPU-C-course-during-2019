b8df7f5c5513c272af8ea4b4a2a05815db14853f
Denis <denisalexandrov99@yandex.ru>
2019-04-17 20:31:12 +0300

    modified: test-main.cpp
    The style of variable names in test-main now coincides with the style in other files.

--- alexandrov.denis/A2/test-main.cpp
+++ alexandrov.denis/A2/test-main.cpp
@@ -13,40 +13,40 @@ BOOST_AUTO_TEST_SUITE(testRectangle)
 
   BOOST_AUTO_TEST_CASE(testRectImmutabilityValues)
   {
-    alexandrov::Rectangle rectangle1({{2.5, 1.3}, 2, 4});
-    const alexandrov::rectangle_t init_rectangle = rectangle1.getFrameRect();
-    const double init_area = rectangle1.getArea();
+    alexandrov::Rectangle rectangle1({{2.5, 1.3}, 2.0, 4.0});
+    const alexandrov::rectangle_t initRectangle = rectangle1.getFrameRect();
+    const double area = rectangle1.getArea();
 
     rectangle1.move(5.2, 9.1);
-    BOOST_CHECK_CLOSE(init_rectangle.width, rectangle1.getFrameRect().width, ACCURACY);
-    BOOST_CHECK_CLOSE(init_rectangle.height, rectangle1.getFrameRect().height, ACCURACY);
-    BOOST_CHECK_CLOSE(init_area, rectangle1.getArea(), ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.width, rectangle1.getFrameRect().width, ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.height, rectangle1.getFrameRect().height, ACCURACY);
+    BOOST_CHECK_CLOSE(area, rectangle1.getArea(), ACCURACY);
 
     rectangle1.move({12.1, 18.2});
-    BOOST_CHECK_CLOSE(init_rectangle.width, rectangle1.getFrameRect().width, ACCURACY);
-    BOOST_CHECK_CLOSE(init_rectangle.height, rectangle1.getFrameRect().height, ACCURACY);
-    BOOST_CHECK_CLOSE(init_area, rectangle1.getArea(), ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.width, rectangle1.getFrameRect().width, ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.height, rectangle1.getFrameRect().height, ACCURACY);
+    BOOST_CHECK_CLOSE(area, rectangle1.getArea(), ACCURACY);
   }
 
   BOOST_AUTO_TEST_CASE(testRectAreaChange)
   {
-    alexandrov::Rectangle rectangle1({{2.2, 5.5}, 2, 4});
-    const double init_area = rectangle1.getArea();
-    const double scale_factor = 4.5;
-    rectangle1.scale(scale_factor);
-    BOOST_CHECK_CLOSE(init_area * scale_factor * scale_factor, rectangle1.getArea(), ACCURACY);
+    alexandrov::Rectangle rectangle1({{2.2, 5.5}, 2.0, 4.0});
+    const double area = rectangle1.getArea();
+    const double factor = 4.5;
+    rectangle1.scale(factor);
+    BOOST_CHECK_CLOSE(area * factor * factor, rectangle1.getArea(), ACCURACY);
   }
 
   BOOST_AUTO_TEST_CASE(testRectInit)
   {
-    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2, 4}, -1, 1}), std::invalid_argument);
-    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2, 4}, 1, -1}), std::invalid_argument);
-    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2, 4}, 0, 1}), std::invalid_argument);
-    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2, 4}, 1, 0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2.0, 4.0}, -1.0, 1.0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2.0, 4.0}, 1.0, -1.0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2.0, 4.0}, 0.0, 1.0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Rectangle rectangle1({{2.0, 4.0}, 1.0, 0.0}), std::invalid_argument);
 
-    alexandrov::Rectangle rectangle1({{5, 5}, 2, 3});
-    BOOST_CHECK_THROW(rectangle1.scale(-2), std::invalid_argument);
-    BOOST_CHECK_THROW(rectangle1.scale(0), std::invalid_argument);
+    alexandrov::Rectangle rectangle1({{5.0, 5.0}, 2.0, 3.0});
+    BOOST_CHECK_THROW(rectangle1.scale(-2.0), std::invalid_argument);
+    BOOST_CHECK_THROW(rectangle1.scale(0.0), std::invalid_argument);
   }
 
 BOOST_AUTO_TEST_SUITE_END();
@@ -55,38 +55,38 @@ BOOST_AUTO_TEST_SUITE(testCircle)
 
   BOOST_AUTO_TEST_CASE(testCircImmutabilityValues)
   {
-    alexandrov::Circle circle1({2.5, 1.3}, 3);
-    const alexandrov::rectangle_t init_rectangle = circle1.getFrameRect();
-    const double init_area = circle1.getArea();
+    alexandrov::Circle circle1({2.5, 1.3}, 3.0);
+    const alexandrov::rectangle_t initRectangle = circle1.getFrameRect();
+    const double area = circle1.getArea();
 
     circle1.move(5.2, 9.1);
-    BOOST_CHECK_CLOSE(init_rectangle.width, circle1.getFrameRect().width, ACCURACY);
-    BOOST_CHECK_CLOSE(init_rectangle.height, circle1.getFrameRect().height, ACCURACY);
-    BOOST_CHECK_CLOSE(init_area, circle1.getArea(), ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.width, circle1.getFrameRect().width, ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.height, circle1.getFrameRect().height, ACCURACY);
+    BOOST_CHECK_CLOSE(area, circle1.getArea(), ACCURACY);
 
     circle1.move({12.1, 18.2});
-    BOOST_CHECK_CLOSE(init_rectangle.width, circle1.getFrameRect().width, ACCURACY);
-    BOOST_CHECK_CLOSE(init_rectangle.height, circle1.getFrameRect().height, ACCURACY);
-    BOOST_CHECK_CLOSE(init_area, circle1.getArea(), ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.width, circle1.getFrameRect().width, ACCURACY);
+    BOOST_CHECK_CLOSE(initRectangle.height, circle1.getFrameRect().height, ACCURACY);
+    BOOST_CHECK_CLOSE(area, circle1.getArea(), ACCURACY);
   }
 
   BOOST_AUTO_TEST_CASE(testCircAreaChange)
   {
-    alexandrov::Circle circle1({{2.2, 5.5}, 2});
-    const double init_area = circle1.getArea();
-    const double scale_factor = 4.5;
-    circle1.scale(scale_factor);
-    BOOST_CHECK_CLOSE(init_area * scale_factor * scale_factor, circle1.getArea(), ACCURACY);
+    alexandrov::Circle circle1({{2.2, 5.5}, 2.0});
+    const double area = circle1.getArea();
+    const double factor = 4.5;
+    circle1.scale(factor);
+    BOOST_CHECK_CLOSE(area * factor * factor, circle1.getArea(), ACCURACY);
   }
 
   BOOST_AUTO_TEST_CASE(testCircInit)
   {
-    BOOST_CHECK_THROW(alexandrov::Circle circle1({{2, 4}, -1}), std::invalid_argument);
-    BOOST_CHECK_THROW(alexandrov::Circle circle1({{2, 4}, 0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Circle circle1({{2.0, 4.0}, -1.0}), std::invalid_argument);
+    BOOST_CHECK_THROW(alexandrov::Circle circle1({{2.0, 4.0}, 0.0}), std::invalid_argument);
 
-    alexandrov::Circle circle1({{5, 5}, 2});
-    BOOST_CHECK_THROW(circle1.scale(-2), std::invalid_argument);
-    BOOST_CHECK_THROW(circle1.scale(0), std::invalid_argument);
+    alexandrov::Circle circle1({{5.0, 5.0}, 2.0});
+    BOOST_CHECK_THROW(circle1.scale(-2.0), std::invalid_argument);
+    BOOST_CHECK_THROW(circle1.scale(0.0), std::invalid_argument);
   }
 
 BOOST_AUTO_TEST_SUITE_END();
