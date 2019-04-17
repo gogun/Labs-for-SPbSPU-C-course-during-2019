42e0b38b39649acdb06573f31506c4e49fe78491
kuxov.grigoriy <kuxov@outlook.com>
2019-04-17 20:55:04 +0000

    Добавил отступы

--- kuxov.grigoriy/A1/triangle.hpp
+++ kuxov.grigoriy/A1/triangle.hpp
@@ -12,12 +12,12 @@ public:
   void print() const override;
   void move(double dx, double dy) override;
   void move(const point_t & nPos) override;
-
+  
 private:
   point_t pos_;
   point_t vA_;
   point_t vB_;
   point_t vC_;
 };
-
+  
 #endif
