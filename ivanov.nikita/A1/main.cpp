46d39ae265fbe9d861efd75dc17c973ebf594775
Nikita_Ivanov <ivanov.nickita.a@gmail.com>
2019-04-19 02:32:49 +0300

    A1
    Внятная демонстрация полиморфизма

--- ivanov.nikita/A1/main.cpp
+++ ivanov.nikita/A1/main.cpp
@@ -5,27 +5,27 @@
 int main()
 {
   Rectangle rectangle({5, 7, {5, 4}});
-  Shape &figure1 = rectangle;
+  Shape *figure = &rectangle;
   std::cout << "\nRECTANGLE:\n";
-  figure1.draw();
+  figure->draw();
   std::cout << "Moving rectangle Ox -3 and Oy 5\n";
-  figure1.move(-3, 5);
-  figure1.draw();
+  figure->move(-3, 5);
+  figure->draw();
   std::cout << "Moving rectangle to the point (9, 4)\n";
-  figure1.move({9, 4});
-  figure1.draw();
+  figure->move({9, 4});
+  figure->draw();
   std::cout << "\n";
-  
+
   Circle circle(7, {1, 5});
-  Shape &figure2 = circle;
+  figure = &circle;
   std::cout << "CIRCLE:\n";
-  figure2.draw();
+  figure->draw();
   std::cout << "Moving circle Ox +4 and Oy +3\n";
-  figure2.move(4, 3);
-  figure2.draw();
+  figure->move(4, 3);
+  figure->draw();
   std::cout << "Moving circle to the point (-10, 7)\n";
-  figure2.move({-10, 7});
-  figure2.draw();
+  figure->move({-10, 7});
+  figure->draw();
   std::cout << "\n";
 
   return 0;
