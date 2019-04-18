8ad5051cf7e8d3d935cf6909a693602c46f3e52f
folsea <88ched@gmail.com>
2019-04-18 23:58:33 +0300

    added ending of rectangle tests and little changes

--- che.eduard/A2/test-main.cpp
+++ che.eduard/A2/test-main.cpp
@@ -13,18 +13,18 @@ BOOST_AUTO_TEST_CASE(Circle_Immutability_After_Moving)
   che::Circle testCircle({ 2.0, 3.5 }, 1.5);
   const che::rectangle_t RectBeforeMoving = testCircle.getFrameRect();
   const double AreaBeforeMoving = testCircle.getArea();
-  circle.move(1.5, 3.0);
+  testCircle.move(1.5, 3.0);
   che::rectangle_t RectAfterMoving
-  BOOST_CHECK_EQUAL(RectBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
-  BOOST_CHECK_EQUAL(RectBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
+  BOOST_CHECK_CLOSE(RectBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
+  BOOST_CHECK_CLOSE(RectBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
   BOOST_CHECK_CLOSE(AreaBeforeMoving, testCircle.getArea(), ACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(Circle_Scale_Tetsting)
 {
-  che:Circle testCircle({ 2.0, 3.5 }, 1.5);
+  che::Circle testCircle({ 2.0, 3.5 }, 1.5);
   const double AreaBeforeScaling = testCircle.getArea();
-  const double circle.coef = 1.8;
+  circle.coef(1.8);
   testCircle.scale(circle.coef);
   BOOST_CHECK_CLOSE(testCircle.getArea(), AreaBeforeScaling * circle.coef * circle.coef, ACCURACY);
 }
@@ -33,9 +33,9 @@ BOOST_AUTO_TEST_CASE(Circle_Invalid_Values)
 {
   BOOST_CHECK_THROW(che::Circle({ 2.0,3.5 }, -1.5), std::invalid_argument);
   che::Circle testCircle1({ 3.0, 3.0 }, 2.0);
-  BOOST_CHECK_THROW(wrongCircle1.scale(-3.0), std::invalid_argument);
+  BOOST_CHECK_THROW(testCircle1.scale(-3.0), std::invalid_argument);
   che::Circle testCircle2({ 2.0, 2.0 }, 3.0);
-  BOOST_CHECK_THROW(wrongCircle1.scale(-2.0), std::invalid_argument);
+  BOOST_CHECK_THROW(testCircle1.scale(-2.0), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -65,4 +65,6 @@ BOOST_AUTO_TEST_CASE(Rectangle_Invalid_Values)
   che::Rectangle testRectangle({ 2.0, 2.0 }, 5.0, 10.0);
   BOOST_CHECK_THROW(testRectangle.scale(0), std::invalid_argument);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
 
