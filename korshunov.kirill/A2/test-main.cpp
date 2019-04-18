aaa0e25f1c330f225cd180d45ff989eddfde713e
kir0108 <kirill.corschunow@yandex.ru>
2019-04-18 14:52:21 +0300

    A2

---
+++ korshunov.kirill/A2/test-main.cpp
@@ -0,0 +1,86 @@
+#define BOOST_TEST_MODULE A2
+
+#include "circle.hpp"
+#include "rectangle.hpp"
+#include <stdexcept>
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(A2_test_suite)
+
+  const double Value = 0.01;
+
+  BOOST_AUTO_TEST_CASE(circle_invar_after_move)
+  {
+    korshunov::Circle testCircle({ 7, 7 }, 7);
+    const korshunov::rectangle_t testFrameBefore = testCircle.getFrameRect();
+    const double testAreaBefore = testCircle.getArea();
+
+    testCircle.move({ 23, 49 });
+    korshunov::rectangle_t testFrameAfter = testCircle.getFrameRect();
+    double testAreaAfter = testCircle.getArea();
+    BOOST_CHECK_CLOSE(testFrameBefore.width, testFrameAfter.width, Value);
+    BOOST_CHECK_CLOSE(testFrameBefore.height, testFrameAfter.height, Value);
+    BOOST_CHECK_CLOSE(testAreaBefore, testAreaAfter, Value);
+
+    testCircle.move(27, 1);
+    testFrameAfter = testCircle.getFrameRect();
+    testAreaAfter = testCircle.getArea();
+    BOOST_CHECK_CLOSE(testFrameBefore.width, testFrameAfter.width, Value);
+    BOOST_CHECK_CLOSE(testFrameBefore.height, testFrameAfter.height, Value);
+    BOOST_CHECK_CLOSE(testAreaBefore, testAreaAfter, Value);
+  }
+
+  BOOST_AUTO_TEST_CASE(rectangle_invar_after_move)
+  {
+    korshunov::Rectangle testRectangle({ 7, 7 }, 7, 17);
+    const korshunov::rectangle_t testFrameBefore = testRectangle.getFrameRect();
+    const double testAreaBefore = testRectangle.getArea();
+
+    testRectangle.move({ 27, 37 });
+    korshunov::rectangle_t testFrameAfter = testRectangle.getFrameRect();
+    double testAreaAfter = testRectangle.getArea();
+    BOOST_CHECK_CLOSE(testFrameBefore.width, testFrameAfter.width, Value);
+    BOOST_CHECK_CLOSE(testFrameBefore.height, testFrameAfter.height, Value);
+    BOOST_CHECK_CLOSE(testAreaBefore, testAreaAfter, Value);
+
+    testRectangle.move(23, 13);
+    testFrameAfter = testRectangle.getFrameRect();
+    testAreaAfter = testRectangle.getArea();
+    BOOST_CHECK_CLOSE(testFrameBefore.width, testFrameAfter.width, Value);
+    BOOST_CHECK_CLOSE(testFrameBefore.height, testFrameAfter.height, Value);
+    BOOST_CHECK_CLOSE(testAreaBefore, testAreaAfter, Value);
+  }
+
+  BOOST_AUTO_TEST_CASE(quadr_change_area_circle)
+  {
+    korshunov::Circle testCircle({ 7, 7 }, 7);
+    const double testAreaBefore = testCircle.getArea();
+    const double coefScale = 7;
+
+    testCircle.scale(coefScale);
+    BOOST_CHECK_CLOSE(testCircle.getArea(), coefScale * coefScale * testAreaBefore, Value);
+  }
+
+  BOOST_AUTO_TEST_CASE(quadr_change_area_rectangle)
+  {
+    korshunov::Rectangle testRectangle({ 7, 7 }, 7, 3);
+    const double testAreaBefore = testRectangle.getArea();
+    const double coefScale = 7;
+
+    testRectangle.scale(coefScale);
+    BOOST_CHECK_CLOSE(testRectangle.getArea(), coefScale * coefScale * testAreaBefore, Value);
+  }
+
+  BOOST_AUTO_TEST_CASE(throwing_exceptions)
+  {
+    BOOST_CHECK_THROW(korshunov::Circle({ 3, 6 }, -4), std::invalid_argument);
+    BOOST_CHECK_THROW(korshunov::Rectangle({ 8, 5 }, 3, -6), std::invalid_argument);
+
+    korshunov::Rectangle testRectangle({ 7, 3 }, 5, 7);
+    BOOST_CHECK_THROW(testRectangle.scale(-7), std::invalid_argument);
+
+    korshunov::Circle testCircle({ 5, 8 }, 2);
+    BOOST_CHECK_THROW(testCircle.scale(-7), std::invalid_argument);
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
