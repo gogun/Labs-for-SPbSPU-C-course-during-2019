a7da2fe80fba1bcd9266d76d11873d6285c3f65d
folsea <88ched@gmail.com>
2019-04-19 00:25:24 +0300

    fixed tests with circle and added little details

--- che.eduard/A2/test-main.cpp
+++ che.eduard/A2/test-main.cpp
@@ -11,20 +11,25 @@ BOOST_AUTO_TEST_SUITE(Circle_Suite_Testing)
 BOOST_AUTO_TEST_CASE(Circle_Immutability_After_Moving)
 {
   che::Circle testCircle({ 2.0, 3.5 }, 1.5);
-  const che::rectangle_t RectBeforeMoving = testCircle.getFrameRect();
+  const che::rectangle_t CircleBeforeMoving = testCircle.getFrameRect();
   const double AreaBeforeMoving = testCircle.getArea();
   testCircle.move(1.5, 3.0);
-  che::rectangle_t RectAfterMoving
-  BOOST_CHECK_CLOSE(RectBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
-  BOOST_CHECK_CLOSE(RectBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
+  BOOST_CHECK_CLOSE(CircleBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
+  BOOST_CHECK_CLOSE(CircleBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
   BOOST_CHECK_CLOSE(AreaBeforeMoving, testCircle.getArea(), ACCURACY);
+
+  testCircle.move({ 2.5, 3.2 });
+  BOOST_CHECK_CLOSE(CircleBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
+  BOOST_CHECK_CLOSE(CircleBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
+  BOOST_CHECK_CLOSE(AreaBeforeMoving, testCircle.getArea(), ACCURACY);
+
 }
 
 BOOST_AUTO_TEST_CASE(Circle_Scale_Tetsting)
 {
   che::Circle testCircle({ 2.0, 3.5 }, 1.5);
   const double AreaBeforeScaling = testCircle.getArea();
-  circle.coef(1.8);
+  testCircle.coef(1.8);
   testCircle.scale(circle.coef);
   BOOST_CHECK_CLOSE(testCircle.getArea(), AreaBeforeScaling * circle.coef * circle.coef, ACCURACY);
 }
@@ -51,6 +56,7 @@ BOOST_AUTO_TEST_SUITE(Rectangle_Suite_Testing)
   BOOST_CHECK_EQUAL(AreaAfterMoving.width, AreaBeforeMoving.width);
   BOOST_CHECK_CLOSE(testRectangle.getArea(), AreaBeforeMoving, ACCURACY);
 }
+
 BOOST_AUTO_TEST_CASE(Rectangle_Scale_Testing)
 {
   che::Rectangle rec({ 4.0, 9.0 }, 2.0, 5.0);
@@ -59,6 +65,7 @@ BOOST_AUTO_TEST_CASE(Rectangle_Scale_Testing)
   const double AreaAfterScaling = rec.getArea();
   BOOST_CHECK_CLOSE(AreaBeforeScaling * AreaBeforeScaling, AreaAfterScaling, ACCURACY);
 }
+
 BOOST_AUTO_TEST_CASE(Rectangle_Invalid_Values)
 {
   BOOST_CHECK_THROW(che::Rectangle({ 2.0, 4.0 }, -3.0, 5.0), std::invalid_argument);
