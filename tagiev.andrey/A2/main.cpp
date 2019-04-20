a22017c3af230220d28967267f324cb895de19fd
atagiev <andrej-tagiev@yandex.ru>
2019-04-20 16:14:12 +0300

    добавил namespace в main.cpp

--- tagiev.andrey/A2/main.cpp
+++ tagiev.andrey/A2/main.cpp
@@ -5,11 +5,11 @@
 #include "rectangle.hpp"
 #include "triangle.hpp"
 
-void printInfo(const Shape *shape)
+void printInfo(const tagiev::Shape *shape)
 {
   assert(shape != nullptr);
 
-  const rectangle_t frame = shape->getFrameRect();
+  const tagiev::rectangle_t frame = shape->getFrameRect();
 
   std::cout << "Area of shape: " << shape->getArea() << '\n';
   std::cout << "Width of frame: " << frame.width << '\n';
@@ -19,28 +19,28 @@ void printInfo(const Shape *shape)
 
 int main()
 {
-  Rectangle rectangle({123.0, 100.0}, 5.0, 3.0);
+  tagiev::Rectangle rectangle({123.0, 100.0}, 5.0, 3.0);
 
   printInfo(&rectangle);
-  const point_t newCenter1 = {1.0, 2.0};
+  const tagiev::point_t newCenter1 = {1.0, 2.0};
   rectangle.move(newCenter1);
   rectangle.move(10.0, 20.0);
   rectangle.scale(2.0);
   printInfo(&rectangle);
 
-  Circle circle({11.1, 12.2}, 4.4);
+  tagiev::Circle circle({11.1, 12.2}, 4.4);
 
   printInfo(&circle);
-  const point_t newCenter2 = {10.0, 20.0};
+  const tagiev::point_t newCenter2 = {10.0, 20.0};
   circle.move(newCenter2);
   circle.move(1.0, 2.0);
   circle.scale(2.0);
   printInfo(&circle);
 
-  Triangle triangle({0.0, 0.0}, {0.0, 4.0}, {3.0, 4.0});
+  tagiev::Triangle triangle({0.0, 0.0}, {0.0, 4.0}, {3.0, 4.0});
 
   printInfo(&triangle);
-  const point_t newCenter3 = {100.0, 200.0};
+  const tagiev::point_t newCenter3 = {100.0, 200.0};
   triangle.move(newCenter3);
   triangle.move(0.1, 0.2);
   triangle.scale(2.0);
