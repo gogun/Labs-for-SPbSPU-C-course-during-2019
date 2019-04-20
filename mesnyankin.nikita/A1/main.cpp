1305d82f1011d88e7e96ae4cf9634bc2cd07b7db
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-20 18:28:06 +0300

    Deleted 2 functions: moveFigure() and showStatus() because they were considered to be copied Added simple polymorphism demonstration with shape pointer All functions have succesfully been tested, see output

--- mesnyankin.nikita/A1/main.cpp
+++ mesnyankin.nikita/A1/main.cpp
@@ -1,43 +1,46 @@
-#include <cassert>
 #include <iostream>
 #include "circle.hpp"
 #include "rectangle.hpp"
 #include "triangle.hpp"
 
-void showStatus(const Shape *shape)
-{
-  assert(shape != nullptr);
-  std::cout << "Current area and frame rectangle are: ";
-  std::cout << shape->getArea() << "\n";
-  rectangle_t frame = shape->getFrameRect();
-  std::cout << frame.width << " | " << frame.height << " | " << frame.pos.x << "," << frame.pos.y << "\n";
-}
-
-void moveFigure(Shape *shape, const double x, const double y)
-{
-  assert(shape != nullptr);
-  shape->move(x,y);
-}
-
 int main()
 {
-  Rectangle rectangle_object(7, 7, {20,20});
+  Rectangle rectangle_object(7, 7, {20, 20});
+  Shape *objPointer = &rectangle_object;
 
-  showStatus(&rectangle_object);
-  moveFigure(&rectangle_object, 3, 3);
-  showStatus(&rectangle_object);
+  std::cout << "Rectangle characteristics: " << "\n";
+  std::cout << "Area: " << objPointer->getArea() << "\n";
+  std::cout << "Frame rectangle values: " << "\n";
+  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
+  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  objPointer->move(1, 1);
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  std::cout << "======================================" << "\n";
 
-  Circle circle_object(5,5,5);
+  Circle circle_object(5, 5, 5);
+  objPointer = &circle_object;
+  std::cout << "Circle characteristics: " << "\n";
+  std::cout << "Area: " << objPointer->getArea() << "\n";
+  std::cout << "Frame rectangle values: " << "\n";
+  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
+  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  objPointer->move(3, 2);
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  std::cout << "======================================" << "\n";
 
-  showStatus(&circle_object);
-  moveFigure(&circle_object, 3, 3);
-  showStatus(&circle_object);
-
-  Triangle triangle_object({5,5}, {3,2}, {4,4});
-
-  showStatus(&triangle_object);
-  moveFigure(&triangle_object, 3, 3);
-  showStatus(&triangle_object);
+  Triangle triangle_object({1, 1}, {1, 1}, {3, 4});
+  objPointer = &triangle_object;
+  std::cout << "Triangle characteristics: " << "\n";
+  std::cout << "Area: " << objPointer->getArea() << "\n";
+  std::cout << "Frame rectangle values: " << "\n";
+  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
+  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  objPointer->move(5, 5);
+  std::cout << "Centre: " << objPointer->getFrameRect().pos.x << "," << objPointer->getFrameRect().pos.y << "\n";
+  std::cout << "======================================" << "\n";
 
   return 0;
 }
