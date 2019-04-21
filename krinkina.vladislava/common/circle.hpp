543a91afc04298112ffbb9573ab8580b41f8b146
Krinkina_Vladislava <vladakrinkina09@gmail.com>
2019-04-21 20:56:16 +0300

    Вынесена общая часть (фигуры, базовый тип) в common

---
+++ krinkina.vladislava/common/circle.hpp
@@ -0,0 +1,28 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+namespace krinkina
+{
+  class Circle : public Shape
+  {
+  public:
+    Circle(const point_t &centre, double radius);
+
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t &centre) override;
+    void move(double dx, double dy) override;
+    void scale(double coefficient) override;
+    void printInfo() const override;
+    double getRadius() const;
+
+  private:
+    point_t centre_;
+    double radius_;
+  };
+} 
+
+#endif //CIRCLE_HPP
+  
