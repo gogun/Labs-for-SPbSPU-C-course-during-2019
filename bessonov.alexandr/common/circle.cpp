5ffb195a9e1cde66d95cb0843631035fe2a33581
bessonov.alexandr <sakryshitel@gmail.com>
2019-04-21 13:45:24 +0300

    Убраны появившиеся лишние отступы

--- bessonov.alexandr/common/circle.cpp
+++ bessonov.alexandr/common/circle.cpp
@@ -4,42 +4,42 @@
 #include <stdexcept>
 #include <cmath>
 
-  bessonov::Circle::Circle(const bessonov::point_t &newCenter, double radius) :
-    center_(newCenter),
-    radius_(radius)
+bessonov::Circle::Circle(const bessonov::point_t &newCenter, double radius) :
+  center_(newCenter),
+  radius_(radius)
+{
+  if (radius <= 0)
   {
-    if (radius <= 0)
-    {
-      throw std::invalid_argument("Problematic radius");
-    }
+    throw std::invalid_argument("Problematic radius");
   }
+}
 
-  void bessonov::Circle::move(double dx, double dy)
+void bessonov::Circle::move(double dx, double dy)
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+void bessonov::Circle::move(const point_t &newCenter)
+{
+  center_ = newCenter;
+}
+
+double bessonov::Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
+
+bessonov::rectangle_t bessonov::Circle::getFrameRect() const
+{
+  return { 2 * radius_, 2 * radius_, center_ };
+}
+
+void bessonov::Circle::scale(double factor)
+{
+  if (factor <= 0)
   {
-    center_.x += dx;
-    center_.y += dy;
+    throw std::invalid_argument("Problematic factor");
   }
-
-  void bessonov::Circle::move(const point_t &newCenter)
-  {
-    center_ = newCenter;
-  }
-
-  double bessonov::Circle::getArea() const
-  {
-    return M_PI * radius_ * radius_;
-  }
-
-  bessonov::rectangle_t bessonov::Circle::getFrameRect() const
-  {
-    return { 2 * radius_, 2 * radius_, center_ };
-  }
-
-  void bessonov::Circle::scale(double factor)
-  {
-    if (factor <= 0)
-    {
-      throw std::invalid_argument("Problematic factor");
-    }
-    radius_ *= factor;
+  radius_ *= factor;
 }
