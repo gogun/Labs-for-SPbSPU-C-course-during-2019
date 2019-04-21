8abb12139a6adc566c2dcf751db7b075cee5b955
Bogdan Lysenko <lysenko.be@gmail.com>
2019-04-21 09:18:10 +0000

    Update test-main.cpp

--- lysenko.bogdan/A2/test-main.cpp
+++ lysenko.bogdan/A2/test-main.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE TEST_A2
+#include <stdexcept>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
-#include <stdexcept>
 #include "circle.hpp"
 #include "rectangle.hpp"
 
