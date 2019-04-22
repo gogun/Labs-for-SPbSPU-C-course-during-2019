98b615cfeb510aa5567c1f2438fae7946b7ad0f8
MaLebDen <lebedev.dm00@mail.ru>
2019-04-22 22:39:20 +0300

    Добавлена демонстрация для нового класса Polygon.

--- lebedev.denis/A2/main.cpp
+++ lebedev.denis/A2/main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include "circle.hpp"
 #include "rectangle.hpp"
+#include "polygon.hpp"
 
 
 void showRectangle_t(lebedev::rectangle_t data)
@@ -19,6 +20,40 @@ void showAll(const lebedev::Shape * shape)
   showRectangle_t(shape->getFrameRect());
 }
 
+void PrintDataPolygon(const lebedev::Shape *shape)
+{
+  if (shape != nullptr)
+  {
+    double unconvex = shape->checkBump();
+    if (unconvex = 0)
+    {
+      double ZeroArea = shape->getArea();
+      if (ZeroArea !=0)
+      {
+        shape->printData();
+        std::std::cout<<"Area = "<<shape->getArea() <<'\n';
+        lebedev::rectangle_t frameRect = shape->getFrameRect();
+        std::cout<<"Width = "<< frameRect.widht<<'\n';
+        std::cout<<"Height = "<<frameRect.height<<'\n';
+        std::cout<<"(X = "<<frameRect.pos.x<<";";
+        std::cout<<"Y = "<<frameRect.pos.y<<")"<<"\n";
+      }
+      else
+      {
+        throw std::invalid_argument("Area must be more then 0");
+      }
+    }
+    else
+    {
+      throw std::invalid_argument("Polygon must be convex!!!");
+    }
+  }
+  else
+  {
+    throw std::invalid_argument("Shape's pointer is null!!!");
+  }
+}
+
 int main()
 {
   lebedev::Rectangle r1(3.00, 4.00, { 5.00, 4.00 });
@@ -27,7 +62,7 @@ int main()
   std::cout<<"After scale"<<"\n";
   r1.scale(3.0);
   showAll(&r1);
-  r1.move({ .x = 7.00, .y = 8.00 });
+  r1.move({.x = 7.00, .y = 8.00 });
   std::cout<<"After move"<<"\n";
   showAll(&r1);
   lebedev::Circle c1(3.00, { 1.00, 3.00 });
@@ -39,6 +74,20 @@ int main()
   c1.move(3.00, -1.5);
   std::cout<<"After move"<<"\n";
   showAll(&c1);
-  
+  lebedev::point_t shape[] = {{4.0, 1.0}, {7.0, 3.0}, {11.0, 3.0}, {6.0, 8.0}, {2.0, 4.0}};
+  size_t qtyVertex = sizeof(shape) / sizeof(shape[0]);
+  lebedev::Polygon poly(qtyVertex, shape);
+  std::cout <<"Polygon."<<'\n';
+  PrintDataPolygon(&poly);
+  std::cout<<"After scale"<<'\n';
+  poly.scale(2.0);
+  PrintDataPolygon(&poly);
+  std::cout<<"After move(dx, dy)"<<"\n";
+  poly.move(3.00, -1.0);
+  PrintDataPolygon(&poly);
+  std::cout<<"After move(Position)"<<"\n";
+  poly.move({.x = 7.00, .y = 8.00 });
+  PrintDataPolygon(&poly);
+
   return 0;
 }
