7bdc51e3284e217544bd5e1363a821a3f852c1cb
olyaasysa <olyaasysa@gmail.com>
2019-04-23 00:55:02 +0300

    Fixed test

--- sysoeva.olga/A2/test-main.cpp
+++ sysoeva.olga/A2/test-main.cpp
@@ -21,8 +21,8 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingTo)
 
   tempRectangle.move({14.3,5.2});
 
-  BOOST_CHECK_CLOSE(initialRectangle.width, tempRecangle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempRectangle.getFrameRecr().height, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.width, tempRectangle.getFrameRect().width, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempRectangle.getFrameRect().height, fault);
   BOOST_CHECK_CLOSE(initialArea, tempRectangle.getArea(), fault);
 }
 
@@ -34,8 +34,8 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingBy)
 
   tempRectangle.move(3.4, 7.5);
 
-  BOOST_CHECK_CLOSE(initialRectangle.width, tempRecangle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempRectangle.getFrameRecr().height, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.width, tempRectangle.getFrameRect().width, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempRectangle.getFrameRect().height, fault);
   BOOST_CHECK_CLOSE(initialArea, tempRectangle.getArea(), fault);
 }
 
@@ -47,20 +47,20 @@ BOOST_AUTO_TEST_CASE(squareChangeOfAreaAfterScale)
 
   tempRectangle.scale(coefficient);
 
-  BOOST_CHECK_CLOSE(initalArea * coefficient * coefficient, tempRectangle.getArea(), fault);
+  BOOST_CHECK_CLOSE(initialArea * coefficient * coefficient, tempRectangle.getArea(), fault);
 }
 
 BOOST_AUTO_TEST_CASE(invalidValues)
 {
-  BOOST_CHECK_THROW(sysoeva::rectangle(0, 6.5, {4.0, 3.0}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::rectangle(5.2, 0, {4.0, 3.0}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::rectangle(-5.2, 6.5, {4.0, 3.0}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::rectangle(5.2, -6.5, {4.0, 3.0}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Rectangle rectangle(0, 6.5, {4.0, 3.0}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Rectangle rectangle(5.2, 0, {4.0, 3.0}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Rectangle rectangle(-5.2, 6.5, {4.0, 3.0}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Rectangle rectangle(5.2, -6.5, {4.0, 3.0}), std::invalid_argument);
 
   sysoeva::Rectangle tempRectangle(10.0, 12.0, {12.5, 9.5});
 
-  BOOST_CHECK_THROW(sysoeva::tempRectangle.scale(0), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::tempRectangle.scale(-2), std::invalid_argument);
+  BOOST_CHECK_THROW(tempRectangle.scale(0), std::invalid_argument);
+  BOOST_CHECK_THROW(tempRectangle.scale(-2), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -76,7 +76,7 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingTo)
   tempCircle.move({4.2,5.2});
 
   BOOST_CHECK_CLOSE(initialRectangle.width, tempCircle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempCircle.getFrameRecr().height, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempCircle.getFrameRect().height, fault);
   BOOST_CHECK_CLOSE(initialArea, tempCircle.getArea(), fault);
 }
 
@@ -89,7 +89,7 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingBy)
   tempCircle.move(3.5, 17.7);
 
   BOOST_CHECK_CLOSE(initialRectangle.width, tempCircle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempCircle.getFrameRecr().height, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempCircle.getFrameRect().height, fault);
   BOOST_CHECK_CLOSE(initialArea, tempCircle.getArea(), fault);
 }
 
@@ -101,18 +101,18 @@ BOOST_AUTO_TEST_CASE(squareChangeOfAreaAfterScale)
 
   tempCircle.scale(coefficient);
 
-  BOOST_CHECK_CLOSE(initalArea * coefficient * coefficient, tempCircle.getArea(), fault);
+  BOOST_CHECK_CLOSE(initialArea * coefficient * coefficient, tempCircle.getArea(), fault);
 }
 
 BOOST_AUTO_TEST_CASE(invalidValues)
 {
-  BOOST_CHECK_THROW(sysoeva::circle(0, {3.2, 4.5}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::circle(-2.5, {3.2, 4.5}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Circle circle(0, {3.2, 4.5}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Circle circle(-2.5, {3.2, 4.5}), std::invalid_argument);
 
   sysoeva::Circle tempCircle(5.7, {6.2, 7.3});
 
-  BOOST_CHECK_THROW(sysoeva::tempCircle.scale(0), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::tempCircle.scale(-5), std::invalid_argument);
+  BOOST_CHECK_THROW(tempCircle.scale(0), std::invalid_argument);
+  BOOST_CHECK_THROW(tempCircle.scale(-5), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -128,7 +128,7 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingTo)
   tempTriangle.move({4.2,5.4});
 
   BOOST_CHECK_CLOSE(initialRectangle.width, tempTriangle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempTriangle.getFrameRecr().height, fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempTriangle.getFrameRect().height, fault);
   BOOST_CHECK_CLOSE(initialArea, tempTriangle.getArea(), fault);
 }
 
@@ -141,8 +141,8 @@ BOOST_AUTO_TEST_CASE(invarianceRectParametersAfterMovingBy)
   tempTriangle.move(3.3, 17.5);
 
   BOOST_CHECK_CLOSE(initialRectangle.width, tempTriangle.getFrameRect().width, fault);
-  BOOST_CHECK_CLOSE(initialRectangle.height, tempTriangle.getFrameRecr().height, fault);
-  BOOST_CHECK_CLOSE(initalArea, tempTriangle.getArea(), fault);
+  BOOST_CHECK_CLOSE(initialRectangle.height, tempTriangle.getFrameRect().height, fault);
+  BOOST_CHECK_CLOSE(initialArea, tempTriangle.getArea(), fault);
 }
 
 BOOST_AUTO_TEST_CASE(squareChangeOfAreaAfterScale)
@@ -153,19 +153,19 @@ BOOST_AUTO_TEST_CASE(squareChangeOfAreaAfterScale)
 
   tempTriangle.scale(coefficient);
 
-  BOOST_CHECK_CLOSE(initalArea * coefficient * coefficient, tempTriangle.getArea(), fault);
+  BOOST_CHECK_CLOSE(initialArea * coefficient * coefficient, tempTriangle.getArea(), fault);
 }
 
 BOOST_AUTO_TEST_CASE(invalidValues)
 {
-  BOOST_CHECK_THROW(sysoeva::triangle({2.3, 6.7}, {7.8, 6.7}, {4.5, 6.7}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::triangle({2.0, 2.0}, {7.0, 7.0}, {7.0, 7.0}), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::triangle({7.8, 6.7}, {7.8, 3.4}, {7.8, 5.6}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Triangle triangle({2.3, 6.7}, {7.8, 6.7}, {4.5, 6.7}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Triangle triangle({2.0, 2.0}, {7.0, 7.0}, {7.0, 7.0}), std::invalid_argument);
+  BOOST_CHECK_THROW(sysoeva::Triangle triangle({7.8, 6.7}, {7.8, 3.4}, {7.8, 5.6}), std::invalid_argument);
 
   sysoeva::Triangle tempTriangle({3.4, 4.5}, {2.3, 7.8}, {14.5, 3.4});
 
-  BOOST_CHECK_THROW(sysoeva::tempTrinagle.scale(0), std::invalid_argument);
-  BOOST_CHECK_THROW(sysoeva::tempTriangle.scale(-1.5), std::invalid_argument);
+  BOOST_CHECK_THROW(tempTrinagle.scale(0), std::invalid_argument);
+  BOOST_CHECK_THROW(tempTriangle.scale(-1.5), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
