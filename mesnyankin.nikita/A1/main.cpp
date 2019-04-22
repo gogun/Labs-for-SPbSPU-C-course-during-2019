1b7c1eb64db129968427e6bc7a26468e1b374c98
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-22 22:41:52 +0300

    Returned previous polymoprhism demostration (returned showStatus() and moveFigure(), but now better) Corrections according to the guidelines

--- mesnyankin.nikita/A1/main.cpp
+++ mesnyankin.nikita/A1/main.cpp
@@ -3,81 +3,43 @@
 #include "rectangle.hpp"
 #include "triangle.hpp"
 
+void moveFigure(Shape * shape, const double &x, const double &y)
+{
+  std::cout << "Before movement: " << shape->getFrameRect().pos.x << " | " << shape->getFrameRect().pos.y << "\n";
+  shape->move(x, y);
+  std::cout << "After movement: " << shape->getFrameRect().pos.x << " | " << shape->getFrameRect().pos.y << "\n";
+}
+
+void showStatus(const Shape * shape)
+{
+  std::cout << "Current shape area is: " << shape->getArea() << "\n";
+  std::cout << "Current shape centre is in: "
+      << shape->getFrameRect().pos.x << "," << shape->getFrameRect().pos.y << "\n";
+}
+
 int main()
 {
   Rectangle rectangle_object(7, 7, {20, 20});
-  Shape *objPointer = &rectangle_object;
 
-  std::cout << "Rectangle characteristics: " << "\n";
-  std::cout << "Area: " << objPointer->getArea() << "\n";
-  std::cout << "Frame rectangle values: " << "\n";
-  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
-  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
-
-  std::cout << "Centre position: " << rectangle_object.getPos().x << ","
-    << rectangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move(1, 1);
-  std::cout << "Centre after displacement: " << rectangle_object.getPos().x << ","
-    << rectangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move({1, 1});
-  std::cout << "Centre after movement: " << rectangle_object.getPos().x << ","
-    << rectangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
+  showStatus(&rectangle_object);
+  moveFigure(&rectangle_object, 5, 5);
+  showStatus(&rectangle_object);
   std::cout << "======================================" << "\n";
 
   Circle circle_object(5, 5, 5);
-  objPointer = &circle_object;
 
-  std::cout << "Circle characteristics: " << "\n";
-  std::cout << "Area: " << objPointer->getArea() << "\n";
-  std::cout << "Frame rectangle values: " << "\n";
-  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
-  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
-  std::cout << "Centre position: " << circle_object.getPos().x << ","
-    << circle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move(6, 3);
-  std::cout << "Centre after displacement: " << circle_object.getPos().x << ","
-    << circle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move({2, 2});
-  std::cout << "Centre after movement: " << circle_object.getPos().x << ","
-    << circle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
+  showStatus(&circle_object);
+  moveFigure(&circle_object, 3, 2);
+  showStatus(&circle_object);
   std::cout << "======================================" << "\n";
 
-  Triangle triangle_object({1, 1}, {2, 3}, {3, 4});
-  objPointer = &triangle_object;
+  Triangle triangle_object({1, 1}, {2, 3}, {3, 2});
 
-  std::cout << "Triangle characteristics: " << "\n";
-  std::cout << "Area: " << objPointer->getArea() << "\n";
-  std::cout << "Frame rectangle values: " << "\n";
-  std::cout << "Width: " << objPointer->getFrameRect().width << "\n";
-  std::cout << "Height: " << objPointer->getFrameRect().height << "\n";
-
-  std::cout << "Centre position: " << triangle_object.getPos().x << ","
-    << triangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move(6, 3);
-  std::cout << "Centre after displacement: " << triangle_object.getPos().x << ","
-    << triangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  objPointer->move({ 2, 2 });
-  std::cout << "Centre after movement: " << triangle_object.getPos().x << ","
-    << triangle_object.getPos().y << "\n";
-  std::cout << "Centre (frame rectangle): " << objPointer->getFrameRect().pos.x << ","
-    << objPointer->getFrameRect().pos.y << "\n";
-  std::cout << "======================================" << "\n";
+  showStatus(&triangle_object);
+  triangle_object.showCoordinates();
+  moveFigure(&triangle_object, 7, 8);
+  showStatus(&triangle_object);
+  triangle_object.showCoordinates();
 
   return 0;
 }
