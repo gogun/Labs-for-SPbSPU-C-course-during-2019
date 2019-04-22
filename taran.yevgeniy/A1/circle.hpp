d54f65d513a7b684e80367761d1521f60fa2368b
Taran <jaktar94@gmail.com>
2019-04-22 22:36:47 +0300

    Исправил на foreach

---
+++ taran.yevgeniy/A1/circle.hpp
@@ -0,0 +1,22 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+class Circle :
+  public Shape
+{
+public:
+  Circle(double radius, const point_t& center);
+  Circle(double radius, double center_x, double center_y);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t& pos) override;
+  void move(double dx, double dy) override;
+  void showParams() const override;
+private:
+  double m_radius;
+  point_t m_center;
+};
+
+#endif
