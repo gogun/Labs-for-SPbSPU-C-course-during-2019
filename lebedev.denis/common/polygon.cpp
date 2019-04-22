413c85e37b724204adb439966f730f54bc24ef23
MaLebDen <lebedev.dm00@mail.ru>
2019-04-22 19:09:21 +0300

    Добавлен метод вычисления центроида полигона. Так же с помощью него добавлена инициализация поля(центроида полигона). Метод помещен в конструктор.

---
+++ lebedev.denis/common/polygon.cpp
@@ -0,0 +1,40 @@
+#include "polygon.hpp"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <cmath>
+
+lebedev::Polygon::Polygon(std::size_t qtyDots, lebedev::point_t *vertex):
+  m_qtyVertex(qtyDots)
+{
+  if (m_qtyVertex <= 2)
+  {
+    throw std::invalid_argument("Quantity of vertex must be more then 2");
+  }
+
+  m_vertex = new point_t[m_qtyVertex];
+  for (std::size_t index = 0; index < m_qtyVertex; ++index)
+  {
+    m_vertex[index] = vertix[index];
+  }
+
+  calcCentroid();
+  }
+
+  lebedev::Polygon::~Polygon()
+  {
+    delete [] m_vertex;
+  }
+
+  void lebedev::calcCentoid()
+  {
+    lebedev::poin_t temp_point = {0.0, 0.0};
+    for (std::size_t index = 0; index < m_qtyVetex; index++)
+    {
+      temp_point.x += m_vertex[index].x;
+      temp_point.y += m_vertex[index].y;
+    }
+    m_centroid = {temp_point.x / m_qtyVetex, temp_point.y / m_qtyVetex};
+  }
