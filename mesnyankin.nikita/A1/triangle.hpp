a672e3b5e796458463ea93eb1acb4347757c3047
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-04-22 22:39:04 +0300

    Added showCoordinates() method, show all points' x and y Corrections according to the guidelines

--- mesnyankin.nikita/A1/triangle.hpp
+++ mesnyankin.nikita/A1/triangle.hpp
@@ -7,16 +7,22 @@ class Triangle: public Shape {
 
 public:
   Triangle(const point_t &a, const point_t &b, const point_t &c);
+
   double getArea() const override;
   rectangle_t getFrameRect() const override;
-  void move(double x, double y) override;
+  void move(const double &x, const double &y) override;
   void move(const point_t &point) override;
+
+  void showCoordinates() const;
+
   point_t getA() const;
   point_t getB() const;
   point_t getC() const;
+
   double getAB() const;
   double getBC() const;
   double getCA() const;
+
   point_t getPos() const;
 
 private:
