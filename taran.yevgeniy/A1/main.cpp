eeba1dee5f0089b41137b35a8eb4f4168b871570
Taran <jaktar94@gmail.com>
2019-04-22 23:32:10 +0300

    Fixed on foreach

--- taran.yevgeniy/A1/main.cpp
+++ taran.yevgeniy/A1/main.cpp
@@ -1,39 +1,24 @@
-#include "rectangle.hpp"
+﻿#include "rectangle.hpp"
 #include "circle.hpp"
 
 int main()
 {
-  Circle mycircle(5.0, {3.0, 3.0}); // sozdat' ob"yekt klassa Circle
+  Circle mycircle(5.0, { 3.0, 3.0 }); // sozdat' ob"yekt klassa Circle
 
   Rectangle myrectangle(mycircle.getFrameRect()); // sozdat' ob"yekt klassa Rectangle
 
-<<<<<<< HEAD
-  Shape * figures[] = {&mycircle, &myrectangle};
-  int size = sizeof(figures)/sizeof(Shape);
-
-  for (Shape * shape : figures)
-  {
-    shape->showParams();
-  }
-
-  for (Shape * shape : figures)
-  {
-    shape->move(5.0, 5.0);
-    shape->showParams();
-=======
   Shape* figures[] = {&mycircle, &myrectangle};
   int size = sizeof(figures) / sizeof(Shape);
 
-  for (int i = 0; i < size; i++)
+  for (Shape* shape : figures)
   {
-    figures[i]->showParams();
+    shape->showParams();
   }
 
-  for (int i = 0; i < size; i++)
+  for (Shape* shape : figures)
   {
-    figures[i]->move(5.0, 5.0);
-    figures[i]->showParams();
->>>>>>> 8405d27ad9ed79ce78febe68465bc0967796a1cb
+    shape->move(5.0, 5.0);
+    shape->showParams();
   }
 
   return 0;
--- taran.yevgeniy/A1/rectangle.cpp
+++ taran.yevgeniy/A1/rectangle.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cassert>
 
-Rectangle::Rectangle(const rectangle_t& figure):
+Rectangle::Rectangle(const rectangle_t& figure) :
   m_figure(figure)
 {
   assert((figure.width > 0) && (figure.height > 0));
@@ -10,11 +10,7 @@ Rectangle::Rectangle(const rectangle_t& figure):
 
 double Rectangle::getArea() const
 {
-<<<<<<< HEAD
-  return m_figure.width * m_figure.height;
-=======
   return m_figure.width* m_figure.height;
->>>>>>> 8405d27ad9ed79ce78febe68465bc0967796a1cb
 }
 
 rectangle_t Rectangle::getFrameRect() const
@@ -36,13 +32,7 @@ void Rectangle::move(double dx, double dy)
 void Rectangle::showParams() const
 {
   std::cout << "Rectangle: center - {" << m_figure.pos.x << ","
-<<<<<<< HEAD
-            << m_figure.pos.y << "}\n" << "Width - " << m_figure.width
-            << '\n' << "Height - " << m_figure.height << '\n'
-            << "Area - " << getArea() << '\n';
-=======
       << m_figure.pos.y << "}\n" << "Width - " << m_figure.width
       << '\n' << "Height - " << m_figure.height << '\n'
       << "Area - " << getArea() << '\n';
->>>>>>> 8405d27ad9ed79ce78febe68465bc0967796a1cb
 }
