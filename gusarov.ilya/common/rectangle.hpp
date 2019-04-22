7d2ff2ef834c21c9b87127b8a4b23dbecb8008c0
Ilya <gusarov2000@gmail.com>
2019-04-22 13:33:33 +0300

    Правила оформления

--- gusarov.ilya/common/rectangle.hpp
+++ gusarov.ilya/common/rectangle.hpp
@@ -7,10 +7,10 @@ namespace gusarov
   class Rectangle: public Shape
   {
   public:
-    Rectangle(const point_t &,const double,const double);
+    Rectangle(const point_t &, const double, const double);
     rectangle_t getFrameRect() const override;
     double getArea() const override;
-    void move(const double,const double) override;
+    void move(const double, const double) override;
     void move(const point_t &) override;
     void printInfo() const override;
     void scale(const double) override;
