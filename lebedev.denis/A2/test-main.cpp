284ce1efa68de00cec2d1908549daa619a22ccb8
MaLebDen <lebedev.dm00@mail.ru>
2019-04-23 21:01:53 +0300

    Исправил ошибки оформления.

--- lebedev.denis/A2/test-main.cpp
+++ lebedev.denis/A2/test-main.cpp
@@ -11,7 +11,7 @@ const double fault = 0.001;
 
 BOOST_AUTO_TEST_CASE(TestCircleAfterMove)
 {
-  lebedev::Circle c1(3.00, { 1.00, 3.00 });
+  lebedev::Circle c1(3.00, {1.00, 3.00 });
   const double AreaBefore = c1.getArea();
   const lebedev::rectangle_t frameBefore = c1.getFrameRect();
   c1.move(2.00, 5.4);
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_CASE(TestCircleAfterMove)
 
 BOOST_AUTO_TEST_CASE(TestCiecleAfterMovePos)
 {
-  lebedev::Circle c1(3.00, { 1.00, 3.00 });
+  lebedev::Circle c1(3.00, {1.00, 3.00 });
   const double AreaBefore = c1.getArea();
   const lebedev::rectangle_t frameBefore = c1.getFrameRect();
   c1.move({3.00, 4.00});
@@ -48,12 +48,12 @@ BOOST_AUTO_TEST_CASE(TestCircleScale)
 
 BOOST_AUTO_TEST_CASE(TestCircleInvalidParametrs)
 {
-  BOOST_CHECK_THROW(lebedev::Circle(-1.5, { 1.00, 3.00 }), std::invalid_argument);
+  BOOST_CHECK_THROW(lebedev::Circle(-1.5, {1.00, 3.00}), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(TestCircleInvalidMultiplier)
 {
-  lebedev::Circle c1(1.5, {1.00, 3.00 });
+  lebedev::Circle c1(1.5, {1.00, 3.00});
   BOOST_CHECK_THROW(c1.scale(-1.5), std::invalid_argument);
 }
 
@@ -102,8 +102,8 @@ BOOST_AUTO_TEST_CASE(TestRectangleScale)
 
 BOOST_AUTO_TEST_CASE(TestRectangleInvalidParametrs)
 {
-  BOOST_CHECK_THROW(lebedev::Rectangle (-3.00, 4.00, { 5.00, 4.00 }), std::invalid_argument);
-  BOOST_CHECK_THROW(lebedev::Rectangle (3.00, -4.00, { 5.00, 4.00 }), std::invalid_argument);
+  BOOST_CHECK_THROW(lebedev::Rectangle (-3.00, 4.00, {5.00, 4.00 }), std::invalid_argument);
+  BOOST_CHECK_THROW(lebedev::Rectangle (3.00, -4.00, {5.00, 4.00 }), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(TestRectangleInvalidCoefficient)
