d68b9940c2c626037e8d4024c370512b97db48b9
Katerina <zaitceva_ek@mail.ru>
2019-04-26 00:50:14 +0300

    A1 fixed#4

--- zaitceva.ekaterina/A1/triangle.hpp
+++ zaitceva.ekaterina/A1/triangle.hpp
@@ -8,7 +8,7 @@ class Triangle : public Shape
 public:
   Triangle(const point_t &point1, const point_t &point2, const point_t &point3);
   point_t getCentre() const;
-  double getSide(const point_t &point1, const point_t &point2);
+  static double getSide(const point_t &point1, const point_t &point2);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(const point_t &point) override;
