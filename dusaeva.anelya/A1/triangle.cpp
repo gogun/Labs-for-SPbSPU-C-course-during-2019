8cbc1b520607b3af20a6f1e916ab13394824c045
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-04-25 22:29:27 +0300

    убраны стороны при инициализации, поправлено условие проверки на вырожденность, добавлен классификатор const к getSide

--- dusaeva.anelya/A1/triangle.cpp
+++ dusaeva.anelya/A1/triangle.cpp
@@ -7,39 +7,38 @@
 Triangle::Triangle(const point_t &pointA, const point_t &pointB, const point_t &pointC) :
   pointA_(pointA),
   pointB_(pointB),
-  pointC_(pointC),
-
-  a_(getSide(pointA, pointB)),
-  b_(getSide(pointB, pointC)),
-  c_(getSide(pointA, pointC))
+  pointC_(pointC)
 {
-  assert((a_ + b_ > c_) && (a_ + c_ > b_) && (b_ + c_ > a_));
+  assert(getArea() > 0.0);
 }
 
 double Triangle::getArea() const
 {
-  double p = (a_ + b_ + c_) / 2;
-  double square = sqrt(p * (p - a_) * (p - b_) * (p - c_));
+  double a = getSide(pointA_, pointB_);
+  double b = getSide(pointB_, pointC_);
+  double c = getSide(pointA_, pointC_);
+  double p = (a + b + c) / 2;
+  double square = sqrt(p * (p - a) * (p - b) * (p - c));
 
   return square;
 }
 
 rectangle_t Triangle::getFrameRect() const
 {
-  point_t min;
-  min.x = std::min(pointA_.x, std::min(pointB_.x, pointC_.x));
-  min.y = std::min(pointA_.y, std::min(pointB_.y, pointC_.y));
+  point_t minSide;
+  minSide.x = std::min(pointA_.x, std::min(pointB_.x, pointC_.x));
+  minSide.y = std::min(pointA_.y, std::min(pointB_.y, pointC_.y));
 
-  point_t max;
-  max.x = std::max(pointA_.x, std::max(pointB_.x, pointC_.x));
-  max.y = std::max(pointA_.y, std::max(pointB_.y, pointC_.y));
+  point_t maxSide;
+  maxSide.x = std::max(pointA_.x, std::max(pointB_.x, pointC_.x));
+  maxSide.y = std::max(pointA_.y, std::max(pointB_.y, pointC_.y));
 
-  double width = max.x - min.x;
-  double height = max.y - min.y;
+  double width = maxSide.x - minSide.x;
+  double height = maxSide.y - minSide.y;
 
   point_t center;
-  center.x = min.x + width / 2;
-  center.y = min.y + height / 2;
+  center.x = minSide.x + width / 2;
+  center.y = minSide.y + height / 2;
 
   return rectangle_t{width, height, center};
 }
@@ -66,16 +65,15 @@ void Triangle::move(const point_t &param)
   move(dx, dy);
 }
 
-void Triangle::printInfo() const{
-
+void Triangle::printInfo() const
+{
   rectangle_t info = getFrameRect();
-  std::cout << "a = " << a_ << ", b = " << b_  << ", c = " << c_ << "\n";
   std::cout << "Area = " << getArea() << "\n";
   std::cout << "Central coordinates of frame: (" << info.pos.x << ", " << info.pos.y << ")\n";
   std::cout << "Frame width = " << info.width <<", frame height = " << info.height << "\n\n";
 }
 
-double Triangle::getSide(const point_t &point1, const point_t &point2)
+double Triangle::getSide(const point_t &point1, const point_t &point2) const
 {
   double side = sqrt(pow((point1.x - point2.x), 2) + pow((point1.y - point2.y), 2));
 
