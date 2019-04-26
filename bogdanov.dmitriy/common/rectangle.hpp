2812a13aff491ab01ef2a83e7d7e4fecf68f000a
Bogdanov Dmitriy <bogdanov3.da@edu.spbstu.ru>
2019-04-26 11:25:56 +0000

    Update rectangle.hpp

--- bogdanov.dmitriy/common/rectangle.hpp
+++ bogdanov.dmitriy/common/rectangle.hpp
@@ -7,18 +7,18 @@
 namespace bogdanov
 {
   class Rectangle: public Shape {
-    public:
-      Rectangle(const point_t &center, double width, double height);
+  public:
+    Rectangle(const point_t &center, double width, double height);
 
-      void move(const point_t &newCenter) override;
-      void move(double dx, double dy) override;
-      double getArea() const override;
-      rectangle_t getFrameRect() const override;
-      void writeInfo() const override;
-      void scale(double scalingFactor) override;
+    void move(const point_t &newCenter) override;
+    void move(double dx, double dy) override;
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void writeInfo() const override;
+    void scale(double scalingFactor) override;
 
-    private:
-      rectangle_t rectangle_;
+  private:
+    rectangle_t rectangle_;
   };
 }  //namespace bogdanov
 
