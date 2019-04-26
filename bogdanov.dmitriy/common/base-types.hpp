2a989c33e836fc7cec714d3ee829e9d5854b65eb
Bogdanov Dmitriy <bogdanov3.da@edu.spbstu.ru>
2019-04-26 11:18:16 +0000

    Update base-types.hpp

--- bogdanov.dmitriy/common/base-types.hpp
+++ bogdanov.dmitriy/common/base-types.hpp
@@ -1,19 +1,18 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-namespace bogdanov {
-
-struct point_t
+namespace bogdanov
 {
-  double x, y;
-};
+  struct point_t
+  {
+    double x, y;
+  };
 
-struct rectangle_t
-{
-  point_t pos;
-  double width, height;
-};
-
+  struct rectangle_t
+  {
+    point_t pos;
+    double width, height;
+  };
 }  //namespace bogdanov
 
 #endif
