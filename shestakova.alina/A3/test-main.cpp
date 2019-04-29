b98c8e5ede3bba8934d19d3920b69a12d5ad879d
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2019-04-29 18:09:39 +0300

    Added extra space

--- shestakova.alina/A3/test-main.cpp
+++ shestakova.alina/A3/test-main.cpp
@@ -75,8 +75,8 @@ BOOST_AUTO_TEST_CASE(compositeShapeIncorrectParameters)
   BOOST_CHECK_THROW(compSh.scale(-3), std::invalid_argument);
 
   BOOST_CHECK_THROW(compSh.addShape(nullptr), std::invalid_argument);
-  BOOST_CHECK_THROW(compSh.deleteShape(2), std::out_of_range);
-  BOOST_CHECK_THROW(compSh.deleteShape(-2), std::out_of_range);
+  BOOST_CHECK_THROW(compSh.deleteShape(10), std::out_of_range);
+  BOOST_CHECK_THROW(compSh.deleteShape(-1), std::out_of_range);
 
   compSh.deleteShape(1);
   BOOST_CHECK_THROW(compSh.deleteShape(1), std::out_of_range);
