11c88ffe613c8059e1c343672d6fe2f8f88a19f1
tural.shahbazly <shahbazly.dev@gmail.com>
2019-04-29 19:01:29 +0300

    Заменены имена переменных на более понятные

--- shahbazly.tural/A2/test-main.cpp
+++ shahbazly.tural/A2/test-main.cpp
@@ -16,46 +16,46 @@ const double ACCURACY = 0.01;
 BOOST_AUTO_TEST_CASE(invariabilityAfterMovingCircle)
 {
   shahbazly::Circle testCircle(2.5, {0, 0});
-  const shahbazly::rectangle_t InitialFrameRect = testCircle.getFrameRect();
-  const double InitialArea = testCircle.getArea();
+  const shahbazly::rectangle_t rectangleFrameBeforeMoving  = testCircle.getFrameRect();
+  const double areaBeforeMoving = testCircle.getArea();
 
   testCircle.move({2.3, 8.6});
-  BOOST_CHECK_CLOSE(InitialFrameRect.width, testCircle.getFrameRect().width, ACCURACY);
-  BOOST_CHECK_CLOSE(InitialFrameRect.height, testCircle.getFrameRect().height, ACCURACY);
-  BOOST_CHECK_CLOSE(InitialArea, testCircle.getArea(), ACCURACY);
+  BOOST_CHECK_CLOSE(rectangleFrameBeforeMoving.width, testCircle.getFrameRect().width, ACCURACY);
+  BOOST_CHECK_CLOSE(rectangleFrameBeforeMoving.height, testCircle.getFrameRect().height, ACCURACY);
+  BOOST_CHECK_CLOSE(areaBeforeMoving, testCircle.getArea(), ACCURACY);
 }
 
 //Тест на неизменность после перемещения прямоугольника
 BOOST_AUTO_TEST_CASE(invariabilityAfterMovingRectangle)
 {
   shahbazly::Rectangle testRectangle({0, 0}, 7.2, 6.4);
-  const shahbazly::rectangle_t InitialFrameRect = testRectangle.getFrameRect();
-  const double InitialArea = testRectangle.getArea();
+  const shahbazly::rectangle_t rectangleFrameBeforeMoving = testRectangle.getFrameRect();
+  const double areaBeforeMoving = testRectangle.getArea();
 
   testRectangle.move({6.9, 9.6});
-  BOOST_CHECK_CLOSE(InitialFrameRect.width, testRectangle.getFrameRect().width, ACCURACY);
-  BOOST_CHECK_CLOSE(InitialFrameRect.height, testRectangle.getFrameRect().height, ACCURACY);
-  BOOST_CHECK_CLOSE(InitialArea, testRectangle.getArea(), ACCURACY);
+  BOOST_CHECK_CLOSE(rectangleFrameBeforeMoving.width, testRectangle.getFrameRect().width, ACCURACY);
+  BOOST_CHECK_CLOSE(rectangleFrameBeforeMoving.height, testRectangle.getFrameRect().height, ACCURACY);
+  BOOST_CHECK_CLOSE(areaBeforeMoving, testRectangle.getArea(), ACCURACY);
 }
 
 //Тест на неизменность площади кргуа после его масштабирования
 BOOST_AUTO_TEST_CASE(areaAfterScalingCircle)
 {
   shahbazly::Circle testCircle(4, {0, 0});
-  const double InitialArea = testCircle.getArea();
+  const double areaBeforeMoving = testCircle.getArea();
   const double scaleFactor = 6.8;
   testCircle.scale(scaleFactor);
-  BOOST_CHECK_CLOSE(testCircle.getArea(), scaleFactor * scaleFactor * InitialArea, ACCURACY);
+  BOOST_CHECK_CLOSE(testCircle.getArea(), scaleFactor * scaleFactor * areaBeforeMoving, ACCURACY);
 }
 
 //Тест на неизменность площади прямоугольника после его масштабирования
 BOOST_AUTO_TEST_CASE(areaAfterScalingRectangle)
 {
   shahbazly::Rectangle testRectangle({0, 0}, 2, 8);
-  const double InitialArea = testRectangle.getArea();
+  const double areaBeforeMoving = testRectangle.getArea();
   const double scaleFactor = 6.8;
   testRectangle.scale(scaleFactor);
-  BOOST_CHECK_CLOSE(testRectangle.getArea(), scaleFactor * scaleFactor * InitialArea, ACCURACY);
+  BOOST_CHECK_CLOSE(testRectangle.getArea(), scaleFactor * scaleFactor * areaBeforeMoving, ACCURACY);
 }
 
 //Тест на неправильные аргументы круга
