f13ae6f4f64ddf907e6a9fbb2f08b59c9e47054b
uzhegov <dimitri.uzhegov@uandex.ru>
2019-04-30 17:42:39 +0300

    add pointer shape, delete other

--- uzhegov.dmitry/A2/main.cpp
+++ uzhegov.dmitry/A2/main.cpp
@@ -5,35 +5,32 @@
 int main()
 {
   uzhegov::Rectangle rec{{0, 0}, 10, 20};
-  uzhegov::Shape *shape_r = &rec;
-  uzhegov::Circle circle{{1, 1}, 2};
-  uzhegov::Shape *shape_c = &circle;
-
+  uzhegov::Shape *shape = &rec;
   std::cout << "PARAMETERS OF THE RECTANGLE:\n";
-  shape_r->printParams();
-  std::cout << "PARAMETERS OF THE CIRCLE:\n";
-  shape_c->printParams();
-
-  shape_r->move(2, 5);
+  shape->printParams();
+  shape->move(2, 5);
   std::cout << "PARAMETERS OF THE RECTANGLE AFTER MOVE dx = 2, dy = 5:\n";
-  shape_r->printParams();
-  shape_c->move(9, 4);
-  std::cout << "PARAMETERS OF THE CIRCLE AFTER MOVE dx = 9, dy = 4:\n";
-  shape_c->printParams();
-
-  shape_r->move({-1, -1});
+  shape->printParams();
+  shape->move({-1, -1});
   std::cout << "PARAMETERS OF THE RECTANGLE AFTER MOVE TO (-1, -1):\n";
-  shape_r->printParams();
-  shape_c->move({-1, -1});
-  std::cout << "PARAMETERS OF THE CIRCLE AFTER MOVE TO (-1, 1):\n";
-  shape_c->printParams();
-
-  shape_r->scale(2.0);
+  shape->printParams();
+  shape->scale(2.0);
   std::cout << "PARAMETERS OF THE RECTANGLE AFTER TWOFOLD INCREASE:\n";
-  shape_r->printParams();
-  shape_c->scale(3.0);
+  shape->printParams();
+      
+  uzhegov::Circle circle{{1, 1}, 2};
+  shape = &circle;
+  std::cout << "PARAMETERS OF THE CIRCLE:\n";
+  shape->printParams();
+  shape->move(9, 4);
+  std::cout << "PARAMETERS OF THE CIRCLE AFTER MOVE dx = 9, dy = 4:\n";
+  shape->printParams();
+  shape->move({-1, -1});
+  std::cout << "PARAMETERS OF THE CIRCLE AFTER MOVE TO (-1, 1):\n";
+  shape->printParams();
+  shape->scale(3.0);
   std::cout << "PARAMETERS OF THE CIRCLE AFTER THREEFOLD INCREASE:\n";
-  shape_c->printParams();
+  shape->printParams();
 
   return 0;
 }
