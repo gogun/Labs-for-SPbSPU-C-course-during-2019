ef3ac2539277aa2b04ac4cd1094b1bf661c53b79
Ivanov Nikita Alexandrovich <ivanov.nickita.a@gmail.com>
2019-04-30 14:25:14 +0000

    Единообразные отступы в фигурных скобках

--- ivanov.nikita/A2/test-main.cpp
+++ ivanov.nikita/A2/test-main.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(rectangleTest)
 
 BOOST_AUTO_TEST_CASE(rectangleConstantParametersAfterMovingByDistance)
 {
-  ivanov::Rectangle testRectangle({3, 6.2, { 1, 3 }});
+  ivanov::Rectangle testRectangle({3, 6.2, {1, 3}});
   const double testRectangleArea = testRectangle.getArea();
   const ivanov::rectangle_t testFrameRect = testRectangle.getFrameRect();
   testRectangle.move(4, 2);
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(rectangleConstantParametersAfterMovingByDistance)
 
 BOOST_AUTO_TEST_CASE(rectangleConstantParametersAfterMovingToPoint)
 {
-  ivanov::Rectangle testRectangle({4.1, 2, { 2, 3.6 }});
+  ivanov::Rectangle testRectangle({4.1, 2, {2, 3.6}});
   const double testRectangleArea = testRectangle.getArea();
   const ivanov::rectangle_t testFrameRect = testRectangle.getFrameRect();
   testRectangle.move({4.2, 7});
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(rectangleConstantParametersAfterMovingToPoint)
 
 BOOST_AUTO_TEST_CASE(boundingRectangleChangeAfterScalingRectangle)
 {
-  ivanov::Rectangle testRectangle({1.9, 8.4, { 2.7, 7 }});
+  ivanov::Rectangle testRectangle({1.9, 8.4, {2.7, 7}});
   const double testArea = testRectangle.getArea();
   const double coefficient = 3.5;
   testRectangle.scale(coefficient);
@@ -42,12 +42,12 @@ BOOST_AUTO_TEST_CASE(boundingRectangleChangeAfterScalingRectangle)
 
 BOOST_AUTO_TEST_CASE(rectangleInvalidValues)
 {
-  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({0, 2.5, { 3, 4.1 }}), std::invalid_argument);
-  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({ 3.1, 0, { 4, 4 }}), std::invalid_argument);
-  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({-1.8, 7, { 2, 4 }}), std::invalid_argument);
-  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({6, -1, { 7, 4.9 }}), std::invalid_argument);
+  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({0, 2.5, {3, 4.1}}), std::invalid_argument);
+  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({3.1, 0, {4, 4}}), std::invalid_argument);
+  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({-1.8, 7, {2, 4}}), std::invalid_argument);
+  BOOST_CHECK_THROW(ivanov::Rectangle testRectangle({6, -1, {7, 4.9}}), std::invalid_argument);
 
-  ivanov::Rectangle testRectangle({2, 1, { 1, 8.2 }});
+  ivanov::Rectangle testRectangle({2, 1, {1, 8.2}});
 
   BOOST_CHECK_THROW(testRectangle.scale(-1), std::invalid_argument);
   BOOST_CHECK_THROW(testRectangle.scale(0), std::invalid_argument);
@@ -58,10 +58,10 @@ BOOST_AUTO_TEST_SUITE(circleTest)
 
 BOOST_AUTO_TEST_CASE(circleConstantParametersAfterMovingCircleToPoint)
 {
-  ivanov::Circle testCircle(4, { 2, 3 });
+  ivanov::Circle testCircle(4, {2, 3});
   const double testCircleArea = testCircle.getArea();
   const ivanov::rectangle_t testFrameRect = testCircle.getFrameRect();
-  testCircle.move({ 1.5, 7 });
+  testCircle.move({1.5, 7});
   BOOST_CHECK_CLOSE(testCircle.getFrameRect().height, testFrameRect.height, tolerance);
   BOOST_CHECK_CLOSE(testCircle.getFrameRect().width, testFrameRect.width, tolerance);
   BOOST_CHECK_CLOSE(testCircle.getArea(), testCircleArea, tolerance);
@@ -69,7 +69,7 @@ BOOST_AUTO_TEST_CASE(circleConstantParametersAfterMovingCircleToPoint)
 
 BOOST_AUTO_TEST_CASE(circleConstantParametersAfterMovingByDistance)
 {
-  ivanov::Circle testCircle(8.2, { 2.4, 3.6 });
+  ivanov::Circle testCircle(8.2, {2.4, 3.6});
   const double testCircleArea = testCircle.getArea();
   const ivanov::rectangle_t testFrameRect = testCircle.getFrameRect();
   testCircle.move(11.4, 2.3);
@@ -80,7 +80,7 @@ BOOST_AUTO_TEST_CASE(circleConstantParametersAfterMovingByDistance)
 
 BOOST_AUTO_TEST_CASE(boundingCircleChangeAfterScaling)
 {
-  ivanov::Circle testCircle(4, { 2.1, 7.7 });
+  ivanov::Circle testCircle(4, {2.1, 7.7});
   const double testArea = testCircle.getArea();
   const double coefficient = 3.5;
   testCircle.scale(coefficient);
@@ -89,9 +89,9 @@ BOOST_AUTO_TEST_CASE(boundingCircleChangeAfterScaling)
 
 BOOST_AUTO_TEST_CASE(circleInvalidValues)
 {
-  BOOST_CHECK_THROW(ivanov::Circle testCircle(0, { 1.2, 4 }), std::invalid_argument);
-  BOOST_CHECK_THROW(ivanov::Circle testCircle(-4, { 4.3, 4.1 }), std::invalid_argument);
-  ivanov::Circle testCircle(6, { 1, 8.2 });
+  BOOST_CHECK_THROW(ivanov::Circle testCircle(0, {1.2, 4}), std::invalid_argument);
+  BOOST_CHECK_THROW(ivanov::Circle testCircle(-4, {4.3, 4.1}), std::invalid_argument);
+  ivanov::Circle testCircle(6, {1, 8.2});
   BOOST_CHECK_THROW(testCircle.scale(0), std::invalid_argument);
   BOOST_CHECK_THROW(testCircle.scale(-1), std::invalid_argument);
 }
