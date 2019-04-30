2c4d04e4a1a4db647f75d2fe2c3d9d084c63897e
Ivanov Nikita Alexandrovich <ivanov.nickita.a@gmail.com>
2019-04-30 14:16:11 +0000

    Единообразные отступы в скобках

--- ivanov.nikita/A2/main.cpp
+++ ivanov.nikita/A2/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-  ivanov::Rectangle rectangle({ 5, 7, {5, 4} });
+  ivanov::Rectangle rectangle({5, 7, {5, 4}});
   ivanov::Shape *figure = &rectangle;
   std::cout << "\nRECTANGLE:\n";
   figure->draw();
@@ -12,14 +12,14 @@ int main()
   figure->move(-3, 5);
   figure->draw();
   std::cout << "Moving rectangle to the point (9, 4)\n";
-  figure->move({ 9, 4 });
+  figure->move({9, 4});
   figure->draw();
   std::cout << "Change the scale of the rectangle with coefficient 1.2\n";
   figure->scale(1.2);
   figure->draw();
   std::cout << "\n";
 
-  ivanov::Circle circle(7, { 1, 5 });
+  ivanov::Circle circle(7, {1, 5});
   figure = &circle;
   std::cout << "CIRCLE:\n";
   figure->draw();
@@ -27,7 +27,7 @@ int main()
   figure->move(4, 3);
   figure->draw();
   std::cout << "Moving circle to the point (-10, 7)\n";
-  figure->move({ -10, 7 });
+  figure->move({-10, 7});
   figure->draw();
   std::cout << "Change the scale of the circle with coefficient 2.2\n";
   figure->scale(2.2);
