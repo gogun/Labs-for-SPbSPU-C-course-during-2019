56de98a3077077b1d3887934d44f4e9cfc361608
rodchenkov.sn <rodchenkov.sn@edu.spbstu.ru>
2019-05-01 12:45:09 +0300

    composite shape fix
    now composite shape is more "vectorish"

--- rodchenkov.simon/common/composite-shape.hpp
+++ rodchenkov.simon/common/composite-shape.hpp
@@ -17,11 +17,11 @@ public:
 
   const CompositeShape& operator = (const CompositeShape&);
   const CompositeShape& operator = (CompositeShape&&) noexcept;
-  Shape&                operator [] (std::size_t) const;
+  Shape& operator [] (std::size_t) const;
 
   double                 getArea()                const noexcept override;
   rectangle_t            getFrameRect()           const noexcept override;
-  std::unique_ptr<Shape> clone()                  const          override;
+  std::unique_ptr<Shape> cloneUnique()            const          override;
   void                   printData(std::ostream&) const          override;
   void                   move(const point_t&)           noexcept override;
   void                   move(double, double)           noexcept override;
@@ -30,12 +30,13 @@ public:
   std::size_t getSize() const noexcept;
   void        add(const Shape&);
   void        remove(std::size_t);
+  void        resize(std::size_t);
 
-  void swap(CompositeShape& r) noexcept;
+  void swap(CompositeShape&) noexcept;
 
 private:
-
   std::size_t count_;
+  std::size_t capacity_;
   ShapeArray  shapes_;
 };
 
