e49c3099b56b638b153022734cd1351944847c35
folsea <88ched@gmail.com>
2019-05-02 00:39:29 +0300

    little changes

--- che.eduard/common/triangle.cpp
+++ che.eduard/common/triangle.cpp
@@ -9,7 +9,8 @@ che::Triangle::Triangle(const point_t& pointA, const point_t& pointB, const poin
   pointB_(pointB),
   pointC_(pointC)
 {
-  if (std::abs((pointB.x - pointA.x) * (pointC.y - pointA.y) - (pointC.x - pointA.x) * (pointB.y - pointA.y)) < 0.0000001)
+  if (std::abs((pointB.x - pointA.x) * (pointC.y - pointA.y)
+    - (pointC.x - pointA.x) * (pointB.y - pointA.y)) < 0.0000001)
   {
 	throw std::invalid_argument("Points can't lie on the same line");
   }
@@ -17,7 +18,8 @@ che::Triangle::Triangle(const point_t& pointA, const point_t& pointB, const poin
 
 double che::Triangle::getArea() const
 {
-  return std::abs((pointB_.x - pointA_.x) * (pointC_.y - pointA_.y) - (pointC_.x - pointA_.x) * (pointB_.y - pointA_.y)) / 2;
+  return std::abs((pointB_.x - pointA_.x) * (pointC_.y - pointA_.y) 
+	- (pointC_.x - pointA_.x) * (pointB_.y - pointA_.y)) / 2;
 }
 
 che::rectangle_t che::Triangle::getFrameRect() const
@@ -46,14 +48,14 @@ void che::Triangle::scale(double coef)
 {
   if (coef <= 0)
   {
-	throw std::invalid_argument("Triangle scale coefficient must be positive");
+	throw std::invalid_argument("Triangle scale coef can't be negativ");
   }
   pointA_ = { centre_.x + coef * (pointA_.x - centre_.x),
-  centre_.y + coef * (pointA_.y - centre_.y) };
+    centre_.y + coef * (pointA_.y - centre_.y) };
   pointB_ = { centre_.x + coef * (pointB_.x - centre_.x),
-  centre_.y + coef * (pointB_.y - centre_.y) };
+    centre_.y + coef * (pointB_.y - centre_.y) };
   pointC_ = { centre_.x + coef * (pointC_.x - centre_.x),
-  centre_.y + coef * (pointC_.y - centre_.y) };
+    centre_.y + coef * (pointC_.y - centre_.y) };
 }
 
 void che::Triangle::showInfo1() const
