31077baddef170b138a00b186908226b1253e722
folsea <88ched@gmail.com>
2019-05-02 01:55:05 +0300

    spaces changes

--- che.eduard/common/triangle.cpp
+++ che.eduard/common/triangle.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 
 che::Triangle::Triangle(const point_t& pointA, const point_t& pointB, const point_t& pointC) :
-  centre_({ (pointA.x + pointB.x + pointC.x) / 3, (pointA.y + pointB.y + pointC.y) / 3 }),
+  centre_({(pointA.x + pointB.x + pointC.x) / 3, (pointA.y + pointB.y + pointC.y) / 3}),
   pointA_(pointA),
   pointB_(pointB),
   pointC_(pointC)
@@ -28,7 +28,8 @@ che::rectangle_t che::Triangle::getFrameRect() const
   double min_x = fmin(fmin(pointA_.x, pointB_.x), pointC_.x);
   double max_y = fmax(fmax(pointA_.y, pointB_.y), pointC_.y);
   double min_y = fmin(fmin(pointA_.y, pointB_.y), pointC_.y);
-  return { max_x - min_x, max_y - min_y, {(max_x + min_x) / 2, (max_y + min_y) / 2} };
+
+  return {max_x - min_x, max_y - min_y, {(max_x + min_x) / 2, (max_y + min_y) / 2}};
 }
 
 void che::Triangle::move(const point_t& centre)
@@ -38,10 +39,10 @@ void che::Triangle::move(const point_t& centre)
 
 void che::Triangle::move(double deltaX, double deltaY)
 {
-  centre_ = { centre_.x + deltaX, centre_.y + deltaY };
-  pointA_ = { pointA_.x + deltaX, pointA_.y + deltaY };
-  pointB_ = { pointB_.x + deltaX, pointB_.y + deltaY };
-  pointC_ = { pointC_.x + deltaX, pointC_.y + deltaY };
+  centre_ = {centre_.x + deltaX, centre_.y + deltaY};
+  pointA_ = {pointA_.x + deltaX, pointA_.y + deltaY};
+  pointB_ = {pointB_.x + deltaX, pointB_.y + deltaY};
+  pointC_ = {pointC_.x + deltaX, pointC_.y + deltaY};
 }
 
 void che::Triangle::scale(double coef)
@@ -65,7 +66,7 @@ void che::Triangle::showInfo1() const
   std::cout << "B: (" << pointB_.x << "; " << pointB_.y << ")" << "\n";
   std::cout << "C: (" << pointC_.x << "; " << pointC_.y << ")" << "\n";
   std::cout << "Area: " << getArea() << "\n";
-  std::cout << "frame rect centre:" << getFrameRect().pos.x << "; " << getFrameRect().pos.y << "\n";
+  std::cout << "frame rect centre:" << getFrameRect().point.x << "; " << getFrameRect().point.y << "\n";
   std::cout << "frame rect width: " << getFrameRect().width << "\n";
   std::cout << "frame rect height: " << getFrameRect().height << "\n";
 }
