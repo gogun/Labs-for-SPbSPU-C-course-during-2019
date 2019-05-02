39186bd863ee93a2a7af65a9c47bdc4531ff4a45
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2019-05-02 12:44:04 +0300

    Small fixes

--- mesnyankin.nikita/A1/shape.hpp
+++ mesnyankin.nikita/A1/shape.hpp
@@ -3,13 +3,14 @@
 
 #include "base-types.hpp"
 
-class Shape {
-  public:
-    virtual ~Shape() = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const double &x, const double &y) = 0;
-    virtual void move(const point_t &point) = 0;
+class Shape
+{
+public:
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(double x, double y) = 0;
+  virtual void move(const point_t &point) = 0;
 };
 
 #endif
