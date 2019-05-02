8e9dc490afedae8ae018cd4c7b2218b98df81133
folsea <88ched@gmail.com>
2019-05-02 15:30:52 +0300

    main changes, namespace added

--- che.eduard/A2/main.cpp
+++ che.eduard/A2/main.cpp
@@ -3,6 +3,8 @@
 #include "rectangle.hpp"
 #include "triangle.hpp"
 
+using namespace che;
+
 void showInfo (Shape & shape1)
 {
   std::cout << "height = " << shape1.getFrameRect().height << std::endl;
@@ -15,21 +17,21 @@ void showInfo (Shape & shape1)
 int main()
 {
   std::cout << "Circle:" << std::endl;
-  che::Circle circle({1,1},3);
+  Circle circle({1,1},3);
   showInfo(circle);
   std::cout << "Scaling by 2: " << std::endl;
   circle.scale(2);
   showInfo(circle);
 
   std::cout << "Rectangle:" << std::endl;
-  che::Rectangle rectangle({2,2,{10,10}});
+  Rectangle rectangle({2,2,{10,10}});
   showInfo(rectangle);
   std::cout << "Scaling by 1.5: " << std::endl;
   rectangle.scale(1.5);
   showInfo(rectangle);
 
   std::cout << "Triangle:" << std::endl;
-  che::Triangle triangle{{4, 8}, {6, 12}, {30, 5}};
+  Triangle triangle{{4, 8}, {6, 12}, {30, 5}};
   Info(triangle);
   std::cout << "Scaling by 3: " << std::endl;
   rectangle.scale(3);
