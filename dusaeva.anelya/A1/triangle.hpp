7d2b2fbe1747d3e4631ed33683b432e55ad462c7
Anely.Dusaeva <ai.dusaeva@edu.spbstu.ru>
2019-05-05 13:39:00 +0300

    добавлен центр треугольника как центр тяжести, площадь считается через точки, а не стороны, убраны лишние функции

--- dusaeva.anelya/A1/triangle.hpp
+++ dusaeva.anelya/A1/triangle.hpp
@@ -12,13 +12,12 @@ public:
   void move (const point_t &) override;
   void move (double,  double) override;
   void printInfo() const override;
-
-  double getSide(const point_t &, const point_t &) const;
-
+  
 private:
   point_t pointA_;
   point_t pointB_;
   point_t pointC_;
+  point_t center_;
 };
 
 #endif
