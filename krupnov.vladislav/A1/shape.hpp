605b6c9217b387bfede4e5c18f84b2a62ddca6ef
VvladislavV <kruuupnovv@mail.ru>
2019-05-06 21:03:13 +0300

    Убран конструктор, метод getCenterInfo(), не имеет смысла применять эти методы. И убраны квалификаторы const в методе move, т.к. аргументы передаются по значению, а не по ссылке (правило 1.10).

--- krupnov.vladislav/A1/shape.hpp
+++ krupnov.vladislav/A1/shape.hpp
@@ -6,13 +6,11 @@
 
 class Shape
 {
-public:
-  Shape() {};
-  virtual ~Shape() = default;
-  virtual double getArea() const = 0;
-  virtual rectangle_t getFrameRect() const = 0;
-  virtual void move(const point_t &p) = 0;
-  virtual void move(const double &x, const double &y) = 0;
-  virtual void getCenterInfo() const = 0;
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t &newPos) = 0;
+    virtual void move(double dx, double dy) = 0;
 };
-#endif
+#endif //SHAPE_HPP
