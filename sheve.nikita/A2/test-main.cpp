1683c4914dc3803c3684e919682bf9bf759b6878
Шеве Никита Максимович <nikk-nikk@bk.ru>
2019-05-06 08:23:36 +0000

    Update test-main.cpp

--- sheve.nikita/A2/test-main.cpp
+++ sheve.nikita/A2/test-main.cpp
@@ -107,7 +107,7 @@ BOOST_AUTO_TEST_SUITE(triangle)
 
 BOOST_AUTO_TEST_CASE(invalid_Initialisation)
 {
-  BOOST_CHECK_THROW(sheve::Triangle({ 1, -12 }, { -13, 2 }, { 1, 3 }), std::invalid_argument);
+  BOOST_CHECK_THROW(sheve::Triangle({1, 0}, {2, 0}, {3, 0}), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(invalid_Scale)
