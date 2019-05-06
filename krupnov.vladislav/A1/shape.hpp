c712cafc17adade837475ca5092dc32d6e245e9f
VvladislavV <kruuupnovv@mail.ru>
2019-05-06 22:51:21 +0300

    Добавлен виртуальный метод для вывода информации о фигуре и добавлено подключение библиотеки, чтобы лишний раз не прописывать в каждом классе соответсвенные заголовки.

--- krupnov.vladislav/A1/shape.hpp
+++ krupnov.vladislav/A1/shape.hpp
@@ -3,14 +3,16 @@
 
 #include "base-types.hpp"
 #include <iostream>
+#include <stdexcept>
 
 class Shape
 {
-  public:
-    virtual ~Shape() = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t &newPos) = 0;
-    virtual void move(double dx, double dy) = 0;
+public:
+  virtual ~Shape() = default;
+  virtual void printInfo() const = 0;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t &newPos) = 0;
+  virtual void move(double dx, double dy) = 0;
 };
 #endif //SHAPE_HPP
