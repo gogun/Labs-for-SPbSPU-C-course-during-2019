4ab9e09f2877478eaaa852cd1a6919458fba2e33
Курбанова Анжела Магомедовна <kurbanovaangela12345@mail.ru>
2019-05-07 20:38:31 +0000

    Update test-rectangle.cpp

--- kurbanova.angela/common/test-rectangle.cpp
+++ kurbanova.angela/common/test-rectangle.cpp
@@ -9,15 +9,15 @@ BOOST_AUTO_TEST_SUITE(testRectangle)
 
 BOOST_AUTO_TEST_CASE(invariabilityWidthAfterMoveInPoint)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double width = test_rec.getFrameRect().width;
-  test_rec.move({ 6, 10 });
+  test_rec.move({6, 10});
   BOOST_CHECK_CLOSE(test_rec.getFrameRect().width, width, INACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(invariabilityWidthAfterMoveDxDy)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double width = test_rec.getFrameRect().width;
   test_rec.move(6, 10);
   BOOST_CHECK_CLOSE(test_rec.getFrameRect().width, width, INACCURACY);
@@ -25,15 +25,15 @@ BOOST_AUTO_TEST_CASE(invariabilityWidthAfterMoveDxDy)
 
 BOOST_AUTO_TEST_CASE(invariabilityHeightAfterMoveInPoint)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double heigt = test_rec.getFrameRect().height;
-  test_rec.move({ 6, 10 });
+  test_rec.move({6, 10});
   BOOST_CHECK_CLOSE(test_rec.getFrameRect().height, heigt, INACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(invariabilityHeightAfterMoveDxDy)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double height = test_rec.getFrameRect().height;
   test_rec.move(6, 10);
   BOOST_CHECK_CLOSE(test_rec.getFrameRect().height, height, INACCURACY);
@@ -41,15 +41,15 @@ BOOST_AUTO_TEST_CASE(invariabilityHeightAfterMoveDxDy)
 
 BOOST_AUTO_TEST_CASE(invariabilityAreAfterMoveInPoint)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double area = test_rec.getArea();
-  test_rec.move({ 6, 10 });
+  test_rec.move({6, 10});
   BOOST_CHECK_CLOSE(test_rec.getArea(), area, INACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(invariabilityAreAfterMoveDxDy)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double area = test_rec.getArea();
   test_rec.move(6, 10);
   BOOST_CHECK_CLOSE(test_rec.getArea(), area, INACCURACY);
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE(invariabilityAreAfterMoveDxDy)
 
 BOOST_AUTO_TEST_CASE(changingAreAfterScaleAnIncrease)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double area = test_rec.getArea();
   const double increase_coef = 2;
   test_rec.scale(increase_coef);
@@ -66,7 +66,7 @@ BOOST_AUTO_TEST_CASE(changingAreAfterScaleAnIncrease)
 
 BOOST_AUTO_TEST_CASE(changingAreAfterScaleAnDecrease)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   const double area = test_rec.getArea();
   const double decrease_coef = 0.5;
   test_rec.scale(decrease_coef);
@@ -75,18 +75,18 @@ BOOST_AUTO_TEST_CASE(changingAreAfterScaleAnDecrease)
 
 BOOST_AUTO_TEST_CASE(scaleTest)
 {
-  kurbanova::Rectangle test_rec({ 5, 5 }, 8, 2);
+  kurbanova::Rectangle test_rec({5, 5}, 8, 2);
   BOOST_CHECK_THROW(test_rec.scale(-2), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(throwExceptionWidth)
 {
-  BOOST_CHECK_THROW(kurbanova::Rectangle test_rec({ 5, 5 }, -8, 2), std::invalid_argument);
+  BOOST_CHECK_THROW(kurbanova::Rectangle test_rec({5, 5}, -8, 2), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(throwExceptionHeight)
 {
-  BOOST_CHECK_THROW(kurbanova::Rectangle test_rec({ 5, 5 }, 8, -2), std::invalid_argument);
+  BOOST_CHECK_THROW(kurbanova::Rectangle test_rec({5, 5}, 8, -2), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
