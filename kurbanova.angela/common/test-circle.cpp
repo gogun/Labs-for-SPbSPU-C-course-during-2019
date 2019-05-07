4106cbbb77f9ca98d9139d598128c2d1bc6706ba
Курбанова Анжела Магомедовна <kurbanovaangela12345@mail.ru>
2019-05-07 20:39:46 +0000

    Update test-circle.cpp

--- kurbanova.angela/common/test-circle.cpp
+++ kurbanova.angela/common/test-circle.cpp
@@ -9,15 +9,15 @@ BOOST_AUTO_TEST_SUITE(testCircle)
 
 BOOST_AUTO_TEST_CASE(invariabilityRadAfterMoveInPoint)
 {
-  kurbanova::Circle test_circle({ 12, -17 }, 4);
+  kurbanova::Circle test_circle({12, -17}, 4);
   const double rad = test_circle.getRad();
-  test_circle.move({ 6, 20 });
+  test_circle.move({6, 20});
   BOOST_CHECK_CLOSE(test_circle.getRad(), rad, INACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(invariabilityRadAfterMoveDxDy)
 {
-  kurbanova::Circle test_circle({ 12, -17 }, 4);
+  kurbanova::Circle test_circle({12, -17}, 4);
   const double rad = test_circle.getRad();
   test_circle.move(5, 5);
   BOOST_CHECK_CLOSE(test_circle.getRad(), rad, INACCURACY);
@@ -25,15 +25,15 @@ BOOST_AUTO_TEST_CASE(invariabilityRadAfterMoveDxDy)
 
 BOOST_AUTO_TEST_CASE(invariabilityAreaAfterMoveInPoint)
 {
-  kurbanova::Circle test_circle({ 12, -17 }, 4);
+  kurbanova::Circle test_circle({12, -17}, 4);
   const double area = test_circle.getArea();
-  test_circle.move({ 6, 20 });
+  test_circle.move({6, 20});
   BOOST_CHECK_CLOSE(test_circle.getArea(), area, INACCURACY);
 }
 
 BOOST_AUTO_TEST_CASE(invariabilityAreaAfterMoveDxDy)
 {
-  kurbanova::Circle test_circle({ 12, -17 }, 4);
+  kurbanova::Circle test_circle({12, -17}, 4);
   const double area = test_circle.getArea();
   test_circle.move(5, 5);
   BOOST_CHECK_CLOSE(test_circle.getArea(), area, INACCURACY);
@@ -41,7 +41,7 @@ BOOST_AUTO_TEST_CASE(invariabilityAreaAfterMoveDxDy)
 
 BOOST_AUTO_TEST_CASE(changingAreaAfterScaleAnIncrease)
 {
-  kurbanova::Circle test_circle({ 10, 10 }, 4);
+  kurbanova::Circle test_circle({10, 10}, 4);
   const double area = test_circle.getArea();
   const double increase_coef = 2;
   test_circle.scale(increase_coef);
@@ -50,7 +50,7 @@ BOOST_AUTO_TEST_CASE(changingAreaAfterScaleAnIncrease)
 
 BOOST_AUTO_TEST_CASE(changingAreaAfterScaleAnDecrease)
 {
-  kurbanova::Circle test_circle({ 10, 10 }, 4);
+  kurbanova::Circle test_circle({10, 10}, 4);
   const double area = test_circle.getArea();
   const double decrease_coef = 0.5;
   test_circle.scale(decrease_coef);
@@ -59,12 +59,12 @@ BOOST_AUTO_TEST_CASE(changingAreaAfterScaleAnDecrease)
 
 BOOST_AUTO_TEST_CASE(throwExceptionRad)
 {
-  BOOST_CHECK_THROW(kurbanova::Circle test_circle({ 10, 10 }, -4), std::invalid_argument);
+  BOOST_CHECK_THROW(kurbanova::Circle test_circle({10, 10}, -4), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(throwExceptionAfterScale)
 {
-  kurbanova::Circle test_circle({ 10, 10 }, 4);
+  kurbanova::Circle test_circle({10, 10}, 4);
   BOOST_CHECK_THROW(test_circle.scale(-3), std::invalid_argument);
 }
 
