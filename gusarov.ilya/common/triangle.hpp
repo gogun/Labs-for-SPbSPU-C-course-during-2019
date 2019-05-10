e0c178f18b79fff1b408a9dd5068ecc87bf1e62f
Ilya <gusarov2000@gmail.com>
2019-05-10 19:15:32 +0300

    еще немного оформления...

--- gusarov.ilya/common/triangle.hpp
+++ gusarov.ilya/common/triangle.hpp
@@ -3,21 +3,19 @@
 
 #include "shape.hpp"
 
-namespace gusarov {
-
-  class Triangle: public Shape {
+namespace gusarov
+{
+  class Triangle: public Shape
+  {
   public:
     Triangle(const point_t &, const point_t &, const point_t &);
-
     point_t getCenter() const;
-
     double getArea() const override;
     rectangle_t getFrameRect() const override;
     void move(const double, const double) override;
     void move(const point_t &) override;
     void scale(const double) override;
     void printInfo() const override;
-
   private:
     point_t pointA_;
     point_t pointB_;
