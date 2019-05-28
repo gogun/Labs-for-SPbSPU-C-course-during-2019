a807571faa09ef6fe517ff11a2294c22e2778bd6
gusarov.ie <gusarov2000@gmail.com>
2019-05-28 14:51:51 +0300

    Правила оформления

--- gusarov.ilya/common/separation.cpp
+++ gusarov.ilya/common/separation.cpp
@@ -24,19 +24,24 @@ gusarov::Matrix<gusarov::Shape> gusarov::section(const gusarov::CompositeShape &
     size_t layerCount = 0;
     size_t layerIndex = 0;
 
-    for (size_t j = 0; j < matrix.getRows(); ++j) {
-      for (size_t k = 0; k < matrix.getColumns(); ++k) {
-        if (matrix[j][k] == nullptr) {
+    for (size_t j = 0; j < matrix.getRows(); ++j)
+    {
+      for (size_t k = 0; k < matrix.getColumns(); ++k)
+      {
+        if (matrix[j][k] == nullptr)
+        {
           layerCount = j;
           layerIndex = k;
           break;
         }
-        if (intersect(compShape[i]->getFrameRect(), matrix[j][k]->getFrameRect())) {
+        if (intersect(compShape[i]->getFrameRect(), matrix[j][k]->getFrameRect()))
+        {
           layerCount = j + 1;
           layerIndex = 0;
           break;
         }
-        if (k == matrix.getColumns() - 1) {
+        if (k == matrix.getColumns() - 1)
+        {
           layerIndex = k + 1;
         }
       }
